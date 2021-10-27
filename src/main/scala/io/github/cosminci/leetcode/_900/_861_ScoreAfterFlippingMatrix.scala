836aa9216b0720febeebf95070e426859e9ef83d
Cosmin Ciobanu <ciobanu@adobe.com>
2021-10-27 10:29:47 +0300

    [861] Score after Flipping Matrix

---
+++ src/main/scala/io/github/cosminci/leetcode/_900/_861_ScoreAfterFlippingMatrix.scala
@@ -0,0 +1,11 @@
+package io.github.cosminci.leetcode._900
+
+object _861_ScoreAfterFlippingMatrix:
+
+  private def matrixScore(grid: Array[Array[Int]]): Int =
+    val (m, n) = (grid.length, grid(0).length)
+
+    (1 until n).foldLeft(m * 1 << n - 1) { (score, col) =>
+      val colScore = (0 until m).count(row => grid(row)(col) == grid(row)(0))
+      score + colScore.max(m - colScore) * (1 << n - 1 - col)
+    }
