6d98ae6a1f2c955fa39445bc9ac2d7308bc7a52c
Cosmin Ciobanu <ciobanu@adobe.com>
2021-10-27 10:53:02 +0300

    [869] Reorder Power of 2

---
+++ src/main/scala/io/github/cosminci/leetcode/_900/_869_ReorderedPowerOfTwo.scala
@@ -0,0 +1,8 @@
+package io.github.cosminci.leetcode._900
+
+object _869_ReorderedPowerOfTwo:
+  def main(args: Array[String]): Unit =
+    println(reorderedPowerOf2(679213508))
+
+  private def reorderedPowerOf2(n: Int): Boolean =
+    (0 until 30).exists(pow => (1 << pow).toString.sorted == n.toString.sorted)
