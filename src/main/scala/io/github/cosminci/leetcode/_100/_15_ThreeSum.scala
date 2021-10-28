db038da5add812a20b8fad3b139b5cef622a1e03
Cosmin Ciobanu <ciobanu@adobe.com>
2021-10-28 10:57:09 +0300

    [15] 3 Sum - functional reimplementation

--- src/main/scala/io/github/cosminci/leetcode/_100/_15_ThreeSum.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_15_ThreeSum.scala
@@ -1,23 +1,23 @@
 package io.github.cosminci.leetcode._100
 
-import scala.collection.mutable
+import scala.collection.Searching.*
+import scala.language.implicitConversions
 
 object _15_ThreeSum:
   def main(args: Array[String]): Unit =
     println(threeSum(Array(-1, 0, 1, 2, -1, -4)))
 
-  private def threeSum(n: Array[Int]): List[List[Int]] =
-    val results = mutable.Set.empty[List[Int]]
-    val nums    = n.sorted
-    (0 until nums.length - 2).foreach { i =>
-      var j = i + 1
-      var k = nums.length - 1
-      while j < k do
-        if nums(i) + nums(j) + nums(k) == 0 then
-          results.addOne(List(nums(i), nums(j), nums(k)))
-          j += 1
-          k -= 1
-        else if nums(i) + nums(j) + nums(k) > 0 then k -= 1
-        else j += 1
-    }
+  private def threeSum(nums: Array[Int]): List[List[Int]] =
+    nums.sortInPlace()
+
+    given Conversion[SearchResult, Option[Int]] =
+      case Found(k) => Some(k)
+      case _        => None
+
+    val results = for
+      i <- 0 until nums.length - 2 if (i == 0 || nums(i - 1) != nums(i))
+      j <- i + 1 until nums.length - 1 if (j == i + 1 || nums(j - 1) != nums(j))
+      k <- nums.search(-(nums(i) + nums(j)), j + 1, nums.length)
+    yield List(nums(i), nums(j), nums(k))
+
     results.toList
