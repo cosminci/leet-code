d6076cd5db5421935385c69e25a4a976f82a539f
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-01 09:18:34 +0200

    [130] Surrounded Regions ADDENDUM * use for comprehensions for traversing the board

--- src/main/scala/io/github/cosminci/leetcode/_200/_130_SurroundedRegions.scala
+++ src/main/scala/io/github/cosminci/leetcode/_200/_130_SurroundedRegions.scala
@@ -7,9 +7,11 @@ import scala.collection.mutable
 object _130_SurroundedRegions:
   private def solve(board: Array[Array[Char]]): Unit =
     val (m, n) = (board.length, board.head.length)
-    val sources =
-      Seq(0, m - 1).flatMap(r => (0 until n).collect { case c if board(r)(c) == 'O' => (r, c) }) ++
-        Seq(0, n - 1).flatMap(c => (0 until m).collect { case r if board(r)(c) == 'O' => (r, c) })
+    val sources = for
+      r <- 0 until m
+      c <- 0 until n
+      if (r == 0 || r == m - 1 || c == 0 || c == n - 1) && board(r)(c) == 'O'
+    yield (r, c)
 
     val toVisit = mutable.Stack.from(sources)
     val visited = mutable.Set.from(sources)
@@ -23,9 +25,8 @@ object _130_SurroundedRegions:
           toVisit.push(n)
       }
 
-    board.indices.foreach { r =>
-      board(r).indices.foreach { c =>
-        if visited.contains((r, c)) then board(r)(c) = 'O'
-        else board(r)(c) = 'X'
-      }
-    }
+    for
+      r <- 0 until m
+      c <- 0 until n
+      if !visited.contains((r, c))
+    do board(r)(c) = 'X'
