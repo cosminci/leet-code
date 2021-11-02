9942b0cadba7c4961f4af583b1629e683619f07b
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-02 16:51:59 +0200

    [2059] Minimum Operations to Convert Number

---
+++ src/main/scala/io/github/cosminci/leetcode/_2100/_2059_MinOpsToConvertNumber.scala
@@ -0,0 +1,21 @@
+package io.github.cosminci.leetcode._2100
+
+import scala.collection.mutable
+
+object _2059_MinOpsToConvertNumber:
+  private def minimumOperations(nums: Array[Int], start: Int, goal: Int): Int =
+    if start == goal then return 0
+
+    val toVisit = mutable.Queue((start, 0))
+    val visited = mutable.Set(start)
+
+    while toVisit.nonEmpty do
+      val (curr, ops) = toVisit.dequeue()
+      nums.flatMap(n => Array(curr + n, curr - n, curr ^ n)).foreach { next =>
+        if next == goal then return ops + 1
+        if next >= 0 && next <= 1000 && !visited.contains(next) then
+          toVisit.enqueue((next, ops + 1))
+          visited.add(next)
+      }
+
+    -1
