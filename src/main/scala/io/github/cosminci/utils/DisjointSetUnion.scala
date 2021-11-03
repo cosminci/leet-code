2015c4831aba9fb7cd5e264022bc28fdd68de04b
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-03 11:24:13 +0200

    [UTILS] Simplify DSU class

--- src/main/scala/io/github/cosminci/utils/DisjointSetUnion.scala
+++ src/main/scala/io/github/cosminci/utils/DisjointSetUnion.scala
@@ -8,15 +8,13 @@ object DisjointSetUnion:
     private val rank: mutable.Map[Int, Int]   = mutable.Map.empty.withDefaultValue(1)
     private val parent: mutable.Map[Int, Int] = mutable.Map.empty.withDefault(n => n)
 
-    def union(n1: Int, n2: Int): Unit =
+    def union(n1: Int, n2: Int): Int =
       val (p1, p2) = (find(n1), find(n2))
       if p1 != p2 then
-        if rank(p1) >= rank(p2) then
-          rank.update(p1, rank(p1) + rank(p2))
-          parent.update(p2, p1)
-        else
-          rank.update(p2, rank(p1) + rank(p2))
-          parent.update(p1, p2)
+        val (lo, hi) = Option.when(rank(p1) >= rank(p2))((p2, p1)).getOrElse((p1, p2))
+        rank.update(hi, rank(lo) + rank(hi))
+        parent.update(lo, hi)
+      parent(p1)
 
     def find(n: Int): Int =
       if parent(n) != n then parent.update(n, find(parent(n)))
