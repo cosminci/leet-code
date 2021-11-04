a08329458f344d91ecb04aa014ba4dfa194468ae
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-04 14:30:12 +0200

    [404] Sum of Left Leaves

---
+++ src/main/scala/io/github/cosminci/leetcode/_500/_404_SumOfLeftLeaves.scala
@@ -0,0 +1,15 @@
+package io.github.cosminci.leetcode._500
+
+import io.github.cosminci.utils.TreeNode
+
+object _404_SumOfLeftLeaves:
+  def main(args: Array[String]): Unit =
+    println(sumOfLeftLeaves(new TreeNode(3, new TreeNode(9))))
+
+  def sumOfLeftLeaves(root: TreeNode): Int =
+    def dfs(node: TreeNode, isLeft: Boolean): Int =
+      if node == null then 0
+      else if node.left == null && node.right == null & isLeft then node.value
+      else dfs(node.left, isLeft = true) + dfs(node.right, isLeft = false)
+
+    dfs(node = root, isLeft = false)
