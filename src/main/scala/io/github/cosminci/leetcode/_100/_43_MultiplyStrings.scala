09f018a77dd1be8a6350f94536d29bfa59adc92b
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-07 09:58:26 +0200

    [43] Multiply String ADDENDUM * reimplementation

--- src/main/scala/io/github/cosminci/leetcode/_100/_43_MultiplyStrings.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_43_MultiplyStrings.scala
@@ -6,27 +6,18 @@ object _43_MultiplyStrings:
     println(multiply("123", "456"))
 
   def multiply(num1: String, num2: String): String =
-    if num1 == "0" || num2 == "0" then return "0"
-    val n1 = num1.map(_ - '0').reverse
-    val n2 = num2.map(_ - '0').reverse
+    val resultDigits = Array.fill(num1.length + num2.length)(0)
 
-    var result = "0" * (num1.length + num2.length)
-    var carry  = 0
-    var index  = 0
-    n1.zipWithIndex.foreach { case (d1, idx1) =>
-      n2.zipWithIndex.foreach { case (d2, idx2) =>
-        index = idx1 + idx2
-        val prev    = (result(index) - '0')
-        val product = d1 * d2 + carry + prev
-        result = updateResult(result, index, product % 10)
-        carry = product / 10
-      }
-      if carry > 0 then
-        result = updateResult(result, index + 1, carry)
-        carry = 0
-    }
+    for
+      i <- num1.length - 1 to 0 by -1
+      j <- num2.length - 1 to 0 by -1
+    do
+      val product = (num1(i) - '0') * (num2(j) - '0')
+      val p1      = i + j
+      val p2      = i + j + 1
+      val sum     = product + resultDigits(p2)
+      resultDigits(p1) += sum / 10
+      resultDigits(p2) = sum % 10
 
-    result.reverse.dropWhile(_ == '0')
-
-  def updateResult(result: String, index: Int, value: Int) =
-    result.updated(index, ('0' + value).toChar)
+    val result = resultDigits.mkString.dropWhile(_ == '0')
+    if result.length == 0 then "0" else result
