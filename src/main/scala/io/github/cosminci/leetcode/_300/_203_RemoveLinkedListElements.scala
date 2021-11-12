fff6a341236c85af8db861d544b140506a5b7660
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-12 11:31:07 +0200

    [203] Remove Linked List Elements ADDENDUM * cosmetic updates

--- src/main/scala/io/github/cosminci/leetcode/_300/_203_RemoveLinkedListElements.scala
+++ src/main/scala/io/github/cosminci/leetcode/_300/_203_RemoveLinkedListElements.scala
@@ -3,12 +3,12 @@ package io.github.cosminci.leetcode._300
 import io.github.cosminci.utils.ListNode
 
 object _203_RemoveLinkedListElements:
-  def removeElements(head: ListNode, `val`: Int): ListNode =
+  def removeElements(head: ListNode, value: Int): ListNode =
     val dummy        = new ListNode(0, head)
     var (prev, curr) = (dummy, head)
 
     while curr != null do
-      if curr.x == `val` then prev.next = curr.next
+      if curr.x == value then prev.next = curr.next
       else prev = prev.next
       curr = curr.next
 
