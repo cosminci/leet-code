31c5bf8caaee82b65839ad36fdc194e76bd468e5
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-14 19:40:45 +0200

    [2070] Most Beautiful Item for each Query

---
+++ src/main/scala/io/github/cosminci/leetcode/_2100/_2070_MostBeautifulItemForEachQuery.scala
@@ -0,0 +1,15 @@
+package io.github.cosminci.leetcode._2100
+
+object _2070_MostBeautifulItemForEachQuery:
+
+  def maximumBeauty(items: Array[Array[Int]], queries: Array[Int]): Array[Int] =
+    val ascPricesWithMaxPrefixBeauty = items
+      .sortBy { case Array(price, beauty) => (price, beauty) }
+      .scanLeft((0, 0)) { case ((prevPrice, maxBeauty), Array(price, beauty)) =>
+        (price, beauty.max(maxBeauty))
+      }
+
+    queries.map { price =>
+      val idx = ascPricesWithMaxPrefixBeauty.search((price, Int.MaxValue)).insertionPoint
+      ascPricesWithMaxPrefixBeauty(idx - 1)._2
+    }
