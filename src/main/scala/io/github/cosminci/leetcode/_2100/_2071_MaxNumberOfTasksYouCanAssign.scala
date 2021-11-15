a95d7224e45b96dc2d7becacfa9758011636b2e5
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-15 10:27:38 +0200

    [2071] Maximum Number of Tasks You Can Assign

---
+++ src/main/scala/io/github/cosminci/leetcode/_2100/_2071_MaxNumberOfTasksYouCanAssign.scala
@@ -0,0 +1,34 @@
+package io.github.cosminci.leetcode._2100
+
+import scala.collection.mutable
+
+object _2071_MaxNumberOfTasksYouCanAssign:
+  def main(args: Array[String]): Unit =
+    println(maxTaskAssign(Array(3, 2, 1), Array(0, 3, 3), 1, 1))
+
+  def maxTaskAssign(tasks: Array[Int], workers: Array[Int], pills: Int, pStr: Int): Int =
+    tasks.sortInPlace()
+    workers.sortInPlace()
+
+    def executeTasks(taskCount: Int): Boolean =
+      var pillsLeft = pills
+      val workforce = mutable.TreeMap.from(workers.takeRight(taskCount).groupBy(identity).view.mapValues(_.length))
+      tasks.take(taskCount).reverse.foreach { tStr =>
+        val (wStr, count) = workforce.last
+        if tStr <= wStr then
+          if count == 1 then workforce.remove(wStr) else workforce.update(wStr, count - 1)
+        else if tStr <= workforce.last._1 + pStr && pillsLeft > 0 then
+          workforce.minAfter(tStr - pStr - 1).foreach { case (wStr, count) =>
+            if count == 1 then workforce.remove(wStr) else workforce.update(wStr, count - 1)
+          }
+          pillsLeft -= 1
+        else return false
+      }
+      true
+
+    var (lo, hi) = (0, tasks.length.min(workers.length))
+    while lo < hi do
+      val mid = lo + hi + 1 >> 1
+      if executeTasks(mid) then lo = mid
+      else hi = mid - 1
+    lo
