86a977041e2e32d34c275bb073f69d7eedf20220
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-16 00:01:06 +0200

    [2065] Maximum Path Quality of a Graph ADDENDUM * cosmetic improvements

--- src/main/scala/io/github/cosminci/leetcode/_2100/_2065_MaxPathQualityOfAGraph.scala
+++ src/main/scala/io/github/cosminci/leetcode/_2100/_2065_MaxPathQualityOfAGraph.scala
@@ -2,22 +2,18 @@ package io.github.cosminci.leetcode._2100
 
 import scala.collection.mutable
 
-object _2065_MaxPathQualityOfAGraph {
-  def maximalPathQuality(values: Array[Int], edges: Array[Array[Int]], maxTime: Int): Int = {
+object _2065_MaxPathQualityOfAGraph:
+  def maximalPathQuality(values: Array[Int], edges: Array[Array[Int]], maxTime: Int): Int =
     val adjList = edges.foldLeft(Map.empty[Int, Seq[(Int, Int)]].withDefaultValue(Seq.empty)) {
       case (adj, Array(n1, n2, cost)) =>
         adj.updated(n1, adj(n1) :+ (n2, cost)).updated(n2, adj(n2) :+ (n1, cost))
     }
 
-    def dfs(node: Int, visited: Set[Int], quality: Int, budget: Int): Int = {
-      val result = Option.when(node == 0)(quality).getOrElse(0)
+    def dfs(node: Int, visited: Set[Int], quality: Int, budget: Int): Int =
       adjList(node).collect {
         case (next, cost) if (cost <= budget) =>
-          val nextQuality = quality + Option.when(visited.contains(next))(0).getOrElse(values(next))
-          dfs(next, visited + next, nextQuality, budget - cost)
-      }.maxOption.getOrElse(0).max(result)
-    }
+          val nextQuality = Option.when(!visited.contains(next))(values(next)).getOrElse(0)
+          dfs(next, visited + next, quality + nextQuality, budget - cost)
+      }.appendedAll(Option.when(node == 0)(quality)).maxOption.getOrElse(0)
 
     dfs(node = 0, visited = Set(0), quality = values.head, budget = maxTime)
-  }
-}
