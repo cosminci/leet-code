1215085db4def40e5173595e5578202ab9843731
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-16 15:59:42 +0200

    [1137] Nth Tribonacci Number ADDENDUM * one-liner

--- src/main/scala/io/github/cosminci/leetcode/_1400/_1137_NthTribonacciNumber.scala
+++ src/main/scala/io/github/cosminci/leetcode/_1400/_1137_NthTribonacciNumber.scala
@@ -1,16 +1,9 @@
 package io.github.cosminci.leetcode._1400
 
-object _1137_NthTribonacciNumber {
-  def main(args: Array[String]): Unit = {
+object _1137_NthTribonacciNumber:
+  def main(args: Array[String]): Unit =
     println(tribonacci(4))
     println(tribonacci(25))
-  }
 
   def tribonacci(n: Int): Int =
-    if (n == 0) 0
-    else
-      (3 to n).foldLeft(Seq(0, 1, 1)) {
-        case (prevThree, _) =>
-          prevThree.tail :+ prevThree.sum
-      }.last
-}
+    if n == 0 then 0 else (3 to n).foldLeft(Seq(0, 1, 1))((prev, _) => prev.tail :+ prev.sum).last
