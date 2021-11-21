f759ab83b7c041facb695447411ded9a77a1aa25
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-21 10:31:42 +0200

    [105] Construct Binary Tree From Inorder & Preorder Traversal

---
+++ src/main/scala/io/github/cosminci/leetcode/_200/_105_ConstructBinaryTreeFromInOrderAndPreOrderTraversal.scala
@@ -0,0 +1,19 @@
+package io.github.cosminci.leetcode._200
+
+import io.github.cosminci.utils.TreeNode
+
+object _105_ConstructBinaryTreeFromInOrderAndPreOrderTraversal:
+  def main(args: Array[String]): Unit =
+    val root = buildTree(Array(3, 9, 20, 15, 7), Array(9, 3, 15, 20, 7))
+    println(root)
+
+  def buildTree(preorder: Array[Int], inorder: Array[Int]): TreeNode =
+    def dfs(preord: Array[Int], inord: Array[Int]): TreeNode =
+      if preord.isEmpty || inord.isEmpty then Option.empty.orNull
+      else {
+        val leftRightDivide     = inord.indexOf(preord.head)
+        val (preLeft, preRight) = preord.tail.splitAt(leftRightDivide)
+        val (inLeft, inRight)   = inord.splitAt(leftRightDivide)
+        new TreeNode(preord.head, dfs(preLeft, inLeft), dfs(preRight, inRight.tail))
+      }
+    dfs(preorder, inorder)
