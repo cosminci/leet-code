e57f8221737d448874797018847d77d53b0ae570
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-22 08:16:42 +0200

    [450] Delete Node in a BST ADDENDUM * fixed impl - how did this ever pass? o.O

--- src/main/scala/io/github/cosminci/leetcode/_500/_450_DeleteNodeInABST.scala
+++ src/main/scala/io/github/cosminci/leetcode/_500/_450_DeleteNodeInABST.scala
@@ -5,12 +5,12 @@ import io.github.cosminci.utils.TreeNode
 object _450_DeleteNodeInABST:
 
   def deleteNode(root: TreeNode, key: Int): TreeNode =
-    if root == null then null
+    if root == null then root
     else if root.value < key then
-      root.left = deleteNode(root.left, key)
+      root.right = deleteNode(root.right, key)
       root
     else if root.value > key then
-      root.right = deleteNode(root.right, key)
+      root.left = deleteNode(root.left, key)
       root
     else if root.left == null then root.right
     else if root.right == null then root.left
