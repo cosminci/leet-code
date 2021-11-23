c8e181457f07162639950c093a288e70d57ce1a3
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-23 13:59:06 +0200

    [952] Largest Component Size by Common Factor

--- src/main/scala/io/github/cosminci/leetcode/_1000/_952_LargestComponentSizeByCommonFactor.scala
+++ src/main/scala/io/github/cosminci/leetcode/_1000/_952_LargestComponentSizeByCommonFactor.scala
@@ -3,15 +3,25 @@ package io.github.cosminci.leetcode._1000
 import io.github.cosminci.utils
 import io.github.cosminci.utils.DisjointSetUnion.DSU
 
+import scala.collection.mutable
+
 object _952_LargestComponentSizeByCommonFactor {
   def largestComponentSize(nums: Array[Int]): Int = {
-    val spf = utils.sieve(nums.max + 1)
-    val uf  = new DSU
-    nums.foreach { n =>
-      utils.primeFactors(n, spf).foreach { f =>
-        uf.union(n, f)
-      }
-    }
-    uf.ranks.values.max
+    val dsu  = new DSU
+
+    val primes = mutable.Map.empty[Int, mutable.ListBuffer[Int]]
+    for {
+      n <- nums
+      p <- primeSet(n)
+    } primes.getOrElseUpdate(p, mutable.ListBuffer.empty).append(n)
+
+    primes.values.foreach(_.reduce(dsu.union))
+    nums.map(dsu.find).groupBy(identity).values.map(_.length).max
   }
+
+  private def primeSet(n: Int): Set[Int] =
+    (2 to math.sqrt(n).toInt).collectFirst {
+      case i if n % i == 0 =>
+        primeSet(n / i) + i
+    }.getOrElse(Set(n))
 }
