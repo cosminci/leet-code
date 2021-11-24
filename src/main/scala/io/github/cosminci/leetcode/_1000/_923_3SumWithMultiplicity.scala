1d3a61e256e4a0ff4a817d6dc1920208a35b490c
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-24 11:07:25 +0200

    [923] 3 Sum with Multiplicity

---
+++ src/main/scala/io/github/cosminci/leetcode/_1000/_923_3SumWithMultiplicity.scala
@@ -0,0 +1,19 @@
+package io.github.cosminci.leetcode._1000
+
+import scala.collection.mutable
+
+object _923_3SumWithMultiplicity:
+  def main(args: Array[String]): Unit =
+    println(threeSumMulti(Array(1, 1, 2, 2, 3, 3, 4, 4, 5, 5), 8))
+
+  def threeSumMulti(arr: Array[Int], target: Int): Int =
+    val counts = mutable.Map.empty[Int, Int]
+    val mod    = 1_000_000_007
+    var result = 0
+    arr.indices.foreach { i =>
+      result = (result + counts.getOrElse(target - arr(i), 0)) % mod
+      (0 until i).foreach { j =>
+        counts.update(arr(i) + arr(j), counts.getOrElse(arr(i) + arr(j), 0) + 1)
+      }
+    }
+    result
