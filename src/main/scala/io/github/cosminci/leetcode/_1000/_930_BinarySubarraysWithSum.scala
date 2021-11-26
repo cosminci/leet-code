4c24f013ecb7cf78ebe3d81f70dcf2688782c02f
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-26 17:58:39 +0200

    [930] Binary Subarrays with Sum

---
+++ src/main/scala/io/github/cosminci/leetcode/_1000/_930_BinarySubarraysWithSum.scala
@@ -0,0 +1,11 @@
+package io.github.cosminci.leetcode._1000
+
+object _930_BinarySubarraysWithSum {
+  def numSubarraysWithSum(nums: Array[Int], goal: Int): Int =
+    nums.foldLeft(0, 0, Map(0 -> 1)) {
+      case ((result, prefixSum, prevSums), n) =>
+        val newPrefixSum = prefixSum + n
+        val newResult = result + prevSums.getOrElse(newPrefixSum - goal, 0)
+        (newResult, newPrefixSum, prevSums.updated(newPrefixSum, prevSums.getOrElse(newPrefixSum, 0) + 1))
+    }._1
+}
