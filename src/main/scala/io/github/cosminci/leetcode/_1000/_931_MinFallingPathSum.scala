dc8ed4f407a95b58a8ceebbec54e0f926f944d3f
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-27 08:27:34 +0200

    [931] Minimum Falling Path Sum

---
+++ src/main/scala/io/github/cosminci/leetcode/_1000/_931_MinFallingPathSum.scala
@@ -0,0 +1,13 @@
+package io.github.cosminci.leetcode._1000
+
+object _931_MinFallingPathSum:
+  def main(args: Array[String]): Unit =
+    println(minFallingPathSum(Array(Array(17, 82), Array(1, -44))))
+
+  def minFallingPathSum(matrix: Array[Array[Int]]): Int =
+    (matrix.length - 2 to 0 by -1)
+      .foldLeft(matrix.last.toSeq) { case (rowBelow, r) =>
+        rowBelow.indices.map { c =>
+          matrix(r)(c) + rowBelow.slice(c - 1, c + 2).min
+        }
+      }.min
