a2029be172d28acc52d8918736df0608519d5980
Cosmin Ciobanu <ciobanu@adobe.com>
2021-11-30 09:47:29 +0200

    [221] Maximal Square ADDENDUM * minor refactoring

--- src/main/scala/io/github/cosminci/leetcode/_300/_221_MaximalSquare.scala
+++ src/main/scala/io/github/cosminci/leetcode/_300/_221_MaximalSquare.scala
@@ -8,14 +8,14 @@ object _221_MaximalSquare:
     matrix.indices.foreach { i =>
       matrix(0).indices.foreach { j =>
         dp(i)(j) = matrix(i)(j) - '0'
-        if dp(i)(j) > max then max = dp(i)(j)
+        max = dp(i)(j).max(max)
       }
     }
     matrix.indices.tail.foreach { i =>
       matrix(0).indices.tail.foreach { j =>
         if matrix(i)(j) == '1' then
-          dp(i)(j) = 1 + math.min(dp(i - 1)(j - 1), math.min(dp(i - 1)(j), dp(i)(j - 1)))
-          if dp(i)(j) > max then max = dp(i)(j)
+          dp(i)(j) = 1 + dp(i - 1)(j - 1).min(dp(i - 1)(j)).min(dp(i)(j - 1))
+          max = dp(i)(j).max(max)
       }
     }
     max * max
