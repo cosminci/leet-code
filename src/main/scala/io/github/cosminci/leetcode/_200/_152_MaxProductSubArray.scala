dec61695c28d883ba7a07ddedc4003221e32be53
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-03 10:24:12 +0200

    [152] Max Product Subarray ADDENDUM * functional rewrite

--- src/main/scala/io/github/cosminci/leetcode/_200/_152_MaxProductSubArray.scala
+++ src/main/scala/io/github/cosminci/leetcode/_200/_152_MaxProductSubArray.scala
@@ -9,18 +9,12 @@ object _152_MaxProductSubArray:
     println(maxProduct(Array(-2, 0, 1)))
 
   def maxProduct(nums: Array[Int]): Int =
-    var result                 = nums.max
-    var (maxMoving, minMoving) = (1, 1)
-    nums.foreach { n =>
-      if n == 0 then
-        maxMoving = 1
-        minMoving = 1
-      else {
-        val newMaxMoving = Array(minMoving * n, maxMoving * n, n).max
-        val newMinMoving = Array(minMoving * n, maxMoving * n, n).min
-        maxMoving = newMaxMoving
-        minMoving = newMinMoving
-        if maxMoving > result then result = maxMoving
+    nums
+      .foldLeft(nums.max, 1, 1) { case ((result, maxMoving, minMoving), n) =>
+        if n == 0 then (result, 1, 1)
+        else
+          val newMaxMoving = (minMoving * n).max(maxMoving * n).max(n)
+          val newMinMoving = (minMoving * n).min(maxMoving * n).min(n)
+          (result.max(newMaxMoving), newMaxMoving, newMinMoving)
       }
-    }
-    result
+      ._1
