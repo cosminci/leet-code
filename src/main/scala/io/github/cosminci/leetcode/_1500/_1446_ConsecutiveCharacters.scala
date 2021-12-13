450189f46d906542b191ff0c5f35961d60c4a0aa
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-13 11:36:34 +0200

    [1446] Consecutive Characters

---
+++ src/main/scala/io/github/cosminci/leetcode/_1500/_1446_ConsecutiveCharacters.scala
@@ -0,0 +1,9 @@
+package io.github.cosminci.leetcode._1500
+
+object _1446_ConsecutiveCharacters {
+  def maxPower(s: String): Int =
+    s.indices.tail.foldLeft(1, 1) {
+      case ((max, curr), i) =>
+        Option.when(s(i) == s(i - 1))(((curr + 1).max(max), curr + 1)).getOrElse((max, 1))
+    }._1
+}
