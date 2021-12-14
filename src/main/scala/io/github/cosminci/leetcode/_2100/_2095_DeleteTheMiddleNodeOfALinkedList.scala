cddf32d69ecdb9462c022cc6d266b37e744d3803
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-15 00:00:33 +0200

    [2095] Delete the Middle Node of a Linked List

---
+++ src/main/scala/io/github/cosminci/leetcode/_2100/_2095_DeleteTheMiddleNodeOfALinkedList.scala
@@ -0,0 +1,21 @@
+package io.github.cosminci.leetcode._2100
+
+import io.github.cosminci.utils.*
+
+object _2095_DeleteTheMiddleNodeOfALinkedList:
+  def main(args: Array[String]): Unit =
+    println(seq(deleteMiddle(linkedList(Seq(1, 3, 4, 7, 1, 2, 6)))))
+    println(seq(deleteMiddle(linkedList(Seq(1, 2, 3, 4)))))
+    println(seq(deleteMiddle(linkedList(Seq(2, 1)))))
+    println(seq(deleteMiddle(linkedList(Seq(2)))))
+
+  def deleteMiddle(head: ListNode): ListNode =
+    @annotation.tailrec
+    def dfs(slow: ListNode, fast: ListNode): ListNode =
+      if fast == null || fast.next == null then slow
+      else dfs(slow.next, fast.next.next)
+
+    val dummyHead = new ListNode(0, head)
+    val prevMid   = dfs(dummyHead, head)
+    prevMid.next = prevMid.next.next
+    dummyHead.next
