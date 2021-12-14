f99a9c78d8f04b2cba758993acce3c134fe9b78b
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-14 23:53:29 +0200

    [876] Middle of the Linked List ADDENDUM * tailrec reimplementation

--- src/main/scala/io/github/cosminci/leetcode/_900/_876_MiddleOfTheLinkedList.scala
+++ src/main/scala/io/github/cosminci/leetcode/_900/_876_MiddleOfTheLinkedList.scala
@@ -4,10 +4,9 @@ import io.github.cosminci.utils.ListNode
 
 object _876_MiddleOfTheLinkedList:
   def middleNode(head: ListNode): ListNode =
-    var (slow, fast) = (head, head)
-    
-    while fast != null && fast.next != null do
-      fast = fast.next.next
-      slow = slow.next
-      
-    slow
+    @annotation.tailrec
+    def dfs(slow: ListNode, fast: ListNode): ListNode =
+      if fast == null || fast.next == null then slow
+      else dfs(slow.next, fast.next.next)
+
+    dfs(head, head)
