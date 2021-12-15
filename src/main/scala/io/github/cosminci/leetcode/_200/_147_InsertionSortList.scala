59e6b2d8666b0ce2fe8dcc018e7d95c0b6d83d41
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-15 12:18:25 +0200

    [147] Insertion Sort List ADDENDUM * minor refactoring

--- src/main/scala/io/github/cosminci/leetcode/_200/_147_InsertionSortList.scala
+++ src/main/scala/io/github/cosminci/leetcode/_200/_147_InsertionSortList.scala
@@ -1,19 +1,19 @@
 package io.github.cosminci.leetcode._200
 
-import io.github.cosminci.utils._
+import io.github.cosminci.utils.*
 
 object _147_InsertionSortList:
   def main(args: Array[String]): Unit =
     println(seq(insertionSortList(linkedList(Seq(4, 5, 1, 3)))))
 
   def insertionSortList(head: ListNode): ListNode =
-    val resultDummyHead   = new ListNode(0)
-    var (insertion, curr) = (resultDummyHead, head)
-    var next: ListNode    = null
+    val dummyHead               = new ListNode(0)
+    var (insertion, curr, next) = (dummyHead, head, head)
+
     while curr != null do
       next = curr.next
 
-      if insertion.x >= curr.x then insertion = resultDummyHead
+      if insertion.x >= curr.x then insertion = dummyHead
 
       while insertion.next != null && insertion.next.x < curr.x do insertion = insertion.next
 
@@ -21,4 +21,4 @@ object _147_InsertionSortList:
       insertion.next = curr
       curr = next
 
-    resultDummyHead.next
+    dummyHead.next
