5e812f7bac2796556100a59efb00b1a31cf202f6
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-26 00:34:18 +0200

    [2109] Adding Spaces to a String

---
+++ src/main/scala/io/github/cosminci/leetcode/_2200/_2109_AddingSpacesToAString.scala
@@ -0,0 +1,13 @@
+package io.github.cosminci.leetcode._2200
+
+object _2109_AddingSpacesToAString:
+  def main(args: Array[String]): Unit =
+    println(addSpaces("spacing", Array(0, 3, 4, 7)))
+
+  def addSpaces(s: String, spaces: Array[Int]): String =
+    val result = new StringBuilder(s.slice(0, spaces.head))
+    (spaces.tail :+ s.length).foldLeft(spaces.head) { case (prevIdx, currIdx) =>
+      result.append(" ").append(s.slice(prevIdx, currIdx))
+      currIdx
+    }
+    result.toString()
