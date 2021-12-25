759e7bb5d5540c5428cb8147b8334084d1477a3f
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-25 11:37:08 +0200

    [227] Basic Calculator II * reimplment w/o stack

--- src/main/scala/io/github/cosminci/leetcode/_300/_227_BasicCalculatorII.scala
+++ src/main/scala/io/github/cosminci/leetcode/_300/_227_BasicCalculatorII.scala
@@ -6,24 +6,21 @@ object _227_BasicCalculatorII:
     println(calculate("3+2*2"))
     println(calculate(" 3 / 2 "))
 
-  def calculate(s: String): Int = {
+  def calculate(s: String): Int =
     @annotation.tailrec
-    def dfs(i: Int, stack: Array[Int], operand: Int, operation: Char): Int =
-      if (i == s.length) stack.sum
-      else {
-        val newOperand = if (s(i).isDigit) operand * 10 + s(i) - '0' else operand
+    def dfs(i: Int, prevVal: Int, prevOp: Char, result: Int): Int =
+      if i >= s.length then result + prevVal
+      else if s(i) == ' ' then dfs(i + 1, prevVal, prevOp, result)
+      else if !s(i).isDigit then dfs(i + 1, prevVal, s(i), result)
+      else
+        val endIdx  = s.indexWhere(c => !c.isDigit, i)
+        val newIdx  = Option.when(endIdx > 0)(endIdx).getOrElse(s.length)
+        val currVal = s.slice(i, newIdx).toInt
+        val (newVal, newResult) = prevOp match
+          case '+' => (currVal, result + prevVal)
+          case '-' => (-currVal, result + prevVal)
+          case '*' => (prevVal * currVal, result)
+          case _   => (prevVal / currVal, result)
+        dfs(newIdx, newVal, prevOp, newResult)
 
-        if (i != s.length - 1 && (s(i).isDigit || s(i) == ' '))
-          dfs(i + 1, stack, newOperand, operation)
-        else if (operation == '+')
-          dfs(i + 1, stack :+ newOperand, 0, s(i))
-        else if (operation == '-')
-          dfs(i + 1, stack :+ -newOperand, 0, s(i))
-        else if (operation == '/')
-          dfs(i + 1, stack.dropRight(1) :+ stack.last / newOperand, 0, s(i))
-        else
-          dfs(i + 1, stack.dropRight(1) :+ stack.last * newOperand, 0, s(i))
-      }
-
-    dfs(i = 0, stack = Array.empty, operand = 0, operation = '+')
-  }
+    dfs(i = 0, prevVal = 0, prevOp = '+', result = 0)
