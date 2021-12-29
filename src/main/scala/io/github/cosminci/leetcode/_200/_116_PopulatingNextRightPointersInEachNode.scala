c7e02841a19e663c2be6b6764edb7de0f506bc9a
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-29 09:55:56 +0200

    [116] Populating Next Right Pointers in Each Node ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_200/_116_PopulatingNextRightPointersInEachNode.scala
+++ src/main/scala/io/github/cosminci/leetcode/_200/_116_PopulatingNextRightPointersInEachNode.scala
@@ -1,41 +1,25 @@
 package io.github.cosminci.leetcode._200
 
-import scala.collection.mutable
-
 object _116_PopulatingNextRightPointersInEachNode:
-  class Node(var _value: Int):
-    var value: Int  = _value
-    var left: Node  = null
-    var right: Node = null
-    var next: Node  = null
 
   def main(args: Array[String]): Unit =
     val root = new Node(1)
     root.left = new Node(2)
     root.right = new Node(3)
-    connectLevelOrderTraversal(root)
+    connect(root)
 
-  def connectLevelOrderTraversal(root: Node): Node =
-    if root == null then return null
+  def connect(root: Node): Node =
+    @annotation.tailrec
+    def dfs(levelNodes: Seq[Node]): Unit =
+      if levelNodes.nonEmpty then
+        (0 until levelNodes.length - 1).foreach(i => levelNodes(i).next = levelNodes(i + 1))
+        dfs(levelNodes.flatMap(n => Seq(n.left, n.right).flatMap(Option.apply)))
 
-    val toVisit = mutable.Queue(root)
-    while toVisit.nonEmpty do
-      val levelNodes = toVisit.dequeueAll(_ => true)
-      levelNodes.indices.foreach { i =>
-        if levelNodes(i).left != null then toVisit.enqueue(levelNodes(i).left)
-        if levelNodes(i).right != null then toVisit.enqueue(levelNodes(i).right)
-        if i < levelNodes.length - 1 then levelNodes(i).next = levelNodes(i + 1)
-      }
-
+    Option(root).foreach(r => dfs(Seq(r)))
     root
 
-  def connectIterative(root: Node): Node =
-    var leftmost = root
-    while leftmost != null && leftmost.left != null do
-      var curr = leftmost
-      while curr != null do
-        curr.left.next = curr.right
-        if curr.next != null then curr.right.next = curr.next.left
-        curr = curr.next
-      leftmost = leftmost.left
-    return root
+  class Node(var _value: Int):
+    var value: Int  = _value
+    var left: Node  = Option.empty.orNull
+    var right: Node = Option.empty.orNull
+    var next: Node  = Option.empty.orNull
