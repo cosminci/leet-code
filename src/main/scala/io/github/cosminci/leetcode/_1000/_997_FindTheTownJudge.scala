9844bb6b6282f9f6ceba34a3f22c38d8b41d8bda
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-03 10:23:11 +0200

    [997] Find the Town Judge ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_1000/_997_FindTheTownJudge.scala
+++ src/main/scala/io/github/cosminci/leetcode/_1000/_997_FindTheTownJudge.scala
@@ -2,13 +2,11 @@ package io.github.cosminci.leetcode._1000
 
 import scala.collection.mutable
 
-object _997_FindTheTownJudge {
-  def findJudge(n: Int, trust: Array[Array[Int]]): Int = {
-    val degree = Array.ofDim[Int](n + 1)
-    trust.foreach { case Array(p1, p2) =>
-      degree(p1) = degree(p1) - 1
-      degree(p2) = degree(p2) + 1
-    }
-    degree.indexWhere(_ == n - 1, from = 1)
-  }
-}
+object _997_FindTheTownJudge:
+  def findJudge(n: Int, trust: Array[Array[Int]]): Int =
+    trust
+      .foldLeft((1 to n).map(_ -> 0).toMap) { case (indegrees, Array(p1, p2)) =>
+        indegrees.updated(p1, indegrees(p1) - 1).updated(p2, indegrees(p2) + 1)
+      }
+      .collectFirst { case (p, indegree) if indegree == n - 1 => p }
+      .getOrElse(-1)
