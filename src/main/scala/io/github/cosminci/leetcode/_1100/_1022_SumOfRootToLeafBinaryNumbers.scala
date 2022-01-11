0dc2ffc1ffb707e78c95141477e9c06b87bee9c6
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-11 09:40:24 +0200

    [1022] Sum of Root to Leaf Binary Numbers

---
+++ src/main/scala/io/github/cosminci/leetcode/_1100/_1022_SumOfRootToLeafBinaryNumbers.scala
@@ -0,0 +1,12 @@
+package io.github.cosminci.leetcode._1100
+
+import io.github.cosminci.utils.TreeNode
+
+object _1022_SumOfRootToLeafBinaryNumbers:
+
+  def sumRootToLeaf(root: TreeNode): Int =
+    def dfs(node: TreeNode, v: Int): Int =
+      if node.left == null && node.right == null then v * 2 + node.value
+      else Seq(Option(node.left), Option(node.right)).flatten.map(dfs(_, v * 2 + node.value)).sum
+
+    dfs(root, v = 0)
