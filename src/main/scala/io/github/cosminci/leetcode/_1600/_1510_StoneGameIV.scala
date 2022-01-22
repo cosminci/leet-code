e6d154fb74e9d4f2df08b53796b5ba551c49a397
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-22 19:35:29 +0200

    [1510] Stone Game IV

---
+++ src/main/scala/io/github/cosminci/leetcode/_1600/_1510_StoneGameIV.scala
@@ -0,0 +1,17 @@
+package io.github.cosminci.leetcode._1600
+
+import scala.collection.mutable
+
+object _1510_StoneGameIV:
+  def main(args: Array[String]): Unit =
+    println(winnerSquareGame(92719))
+
+  def winnerSquareGame(n: Int): Boolean =
+    val mem = mutable.Map.empty[Int, Boolean]
+    def dfs(n: Int): Boolean = mem.getOrElseUpdate(n,
+      n > 0 && Iterator
+        .iterate(math.sqrt(n).toInt)(i => i - 1)
+        .takeWhile(_ > 0)
+        .exists(v => !dfs(n - v * v))
+    )
+    dfs(n)
