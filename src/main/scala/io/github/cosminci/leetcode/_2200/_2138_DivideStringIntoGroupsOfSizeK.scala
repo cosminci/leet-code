3a443a1a63c8a1d61db5d4cd19d21bf8c1fd2b25
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-23 22:57:30 +0200

    [2138] Divide a String Into Groups of Size k

---
+++ src/main/scala/io/github/cosminci/leetcode/_2200/_2138_DivideStringIntoGroupsOfSizeK.scala
@@ -0,0 +1,7 @@
+package io.github.cosminci.leetcode._2200
+
+object _2138_DivideStringIntoGroupsOfSizeK {
+
+  def divideString(s: String, k: Int, fill: Char): Array[String] =
+    s.padTo((s.length + k - 1) / k * k, fill).grouped(k).toArray
+}
