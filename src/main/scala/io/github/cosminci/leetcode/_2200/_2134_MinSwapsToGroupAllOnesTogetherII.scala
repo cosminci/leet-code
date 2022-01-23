3444501cae6e0c08e92c1f7be6846d7df9fc99ec
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-23 13:58:02 +0200

    [2134] Minimum Swaps to Group All 1's Together II ADDENDUM * remove redundant right pointer

--- src/main/scala/io/github/cosminci/leetcode/_2200/_2134_MinSwapsToGroupAllOnesTogetherII.scala
+++ src/main/scala/io/github/cosminci/leetcode/_2200/_2134_MinSwapsToGroupAllOnesTogetherII.scala
@@ -3,14 +3,14 @@ package io.github.cosminci.leetcode._2200
 object _2134_MinSwapsToGroupAllOnesTogetherII:
 
   def minSwaps(nums: Array[Int]): Int =
-    val (total, n)  = (nums.sum, nums.length)
-    val initialOnes = nums.slice(0, total).sum
+    val (windowSize, n)  = (nums.sum, nums.length)
+    val initialOnes = nums.slice(0, windowSize).sum
 
     @annotation.tailrec
-    def dfs(l: Int, r: Int, prevOnes: Int, minZeroes: Int): Int =
-      if r == n * 2 then minZeroes
+    def dfs(l: Int, prevOnes: Int, minZeroes: Int): Int =
+      if l + windowSize == n * 2 then minZeroes
       else
-        val currOnes = prevOnes + nums(r % n) - nums(l % n)
-        dfs(l + 1, r + 1, currOnes, minZeroes.min(total - currOnes))
+        val currOnes = prevOnes + nums((l + windowSize) % n) - nums(l % n)
+        dfs(l + 1, currOnes, minZeroes.min(windowSize - currOnes))
 
-    dfs(l = 0, r = total, prevOnes = initialOnes, minZeroes = total - initialOnes)
+    dfs(l = 0, prevOnes = initialOnes, minZeroes = windowSize - initialOnes)
