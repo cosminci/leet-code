707ca9580525fa6ffc2b0ac1080697ec42220b83
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-23 23:25:16 +0200

    [2140] Solving Questions With Brainpower

---
+++ src/main/scala/io/github/cosminci/leetcode/_2200/_2140_SolvingQuestionsWithBrainpower.scala
@@ -0,0 +1,20 @@
+package io.github.cosminci.leetcode._2200
+
+import scala.collection.mutable
+
+object _2140_SolvingQuestionsWithBrainpower {
+
+  def mostPoints(questions: Array[Array[Int]]): Long = {
+    val mem = mutable.Map.empty[Int, Long]
+
+    def dfs(idx: Int): Long = mem.getOrElseUpdate(idx, {
+      if (idx >= questions.length) 0
+      else {
+        val Array(points, brainpower) = questions(idx)
+        dfs(idx + 1).max(points + dfs(idx + brainpower + 1))
+      }
+    })
+
+    dfs(idx = 0)
+  }
+}
