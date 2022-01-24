7acd142d0fa6055038a845b29b0f8f01582cc338
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-24 09:18:40 +0200

    [2144] Minimum Cost of Buying Candies With Discount

---
+++ src/main/scala/io/github/cosminci/leetcode/_2200/_2144_MinCostOfBuyingCandiesWithDiscount.scala
@@ -0,0 +1,8 @@
+package io.github.cosminci.leetcode._2200
+
+object _2144_MinCostOfBuyingCandiesWithDiscount:
+  def minimumCost(cost: Array[Int]): Int =
+    cost.sortBy(c => -c)
+      .grouped(3)
+      .flatMap(_.take(2))
+      .sum
