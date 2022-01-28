04f13bbfa77e79f5e850ea6ea1a1674f4cd4fe62
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-28 11:14:32 +0200

    [211] Design Add and Search Words Data Structure ADDENDUM * minor refactoring

--- src/main/scala/io/github/cosminci/leetcode/_300/_211_DesignAddAndSearchWordsDataStructure.scala
+++ src/main/scala/io/github/cosminci/leetcode/_300/_211_DesignAddAndSearchWordsDataStructure.scala
@@ -3,34 +3,23 @@ package io.github.cosminci.leetcode._300
 import scala.collection.mutable
 
 object _211_DesignAddAndSearchWordsDataStructure:
-  def main(args: Array[String]): Unit =
-    val dict = new WordDictionary()
-    dict.addWord("ba")
-    dict.addWord("dad")
-    dict.addWord("bax")
-    dict.addWord("bbb")
-    println(dict.search("ba"))
-    println(dict.search("b.b"))
-    println(dict.search(".aq"))
 
   class WordDictionary():
-    private val prefixTreeRoot = Node(children = mutable.Map.empty, isLeaf = false)
+    private val prefixTreeRoot = new Node(children = mutable.Map.empty)
 
-    def addWord(word: String) =
-      var node = prefixTreeRoot
-      word.toCharArray.zipWithIndex.foreach { (char, idx) =>
-        if !node.children.contains(char) then
-          node.children.update(char, Node(children = mutable.Map.empty, isLeaf = idx == word.length - 1))
-        node = node.children(char)
-      }
+    def addWord(word: String): Unit =
+      word.foldLeft(prefixTreeRoot) { (node, char) =>
+        node.children.getOrElseUpdate(char, new Node())
+      }.isLeaf = true
 
     def search(word: String): Boolean =
-      def dfs(node: Node, wordIndex: Int): Boolean =
-        if wordIndex == word.length then return node.isLeaf
-        if word(wordIndex) == '.' then node.children.exists(n => dfs(n._2, wordIndex + 1))
-        else if node.children.contains(word(wordIndex)) then dfs(node.children(word(wordIndex)), wordIndex + 1)
-        else false
+      def dfs(node: Node, idx: Int): Boolean =
+        if idx == word.length then node.isLeaf
+        else if word(idx) == '.' then node.children.values.exists(dfs(_, idx + 1))
+        else node.children.get(word(idx)) match
+          case None           => false
+          case Some(nextNode) => dfs(nextNode, idx + 1)
 
-      dfs(prefixTreeRoot, 0)
+      dfs(prefixTreeRoot, idx = 0)
 
-    case class Node(children: mutable.Map[Char, Node], isLeaf: Boolean)
+    class Node(val children: mutable.Map[Char, Node] = mutable.Map.empty, var isLeaf: Boolean = false)
