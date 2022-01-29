6227c343e2325c6f54f864477d639b87a58b3f0c
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-29 13:11:45 +0200

    [84] Largest Rectangle in Histogram ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_100/_84_LargestRectangleInHistogram.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_84_LargestRectangleInHistogram.scala
@@ -1,34 +1,21 @@
 package io.github.cosminci.leetcode._100
 
-import scala.collection.mutable
-
 object _84_LargestRectangleInHistogram:
   def main(args: Array[String]): Unit =
-    println(largestRectangleArea(Array(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)))
+    println(largestRectangleArea(Array(4, 2, 0, 3, 2, 4, 3, 4)))
 
   def largestRectangleArea(heights: Array[Int]): Int =
-    var maxBlock          = Block(0, 0)
-    val paddedHistogram   = heights.prepended(0).appended(0)
-    val increasingHeights = mutable.Stack.empty[Block]
-    increasingHeights.push(Block(0, 0))
-    (1 until paddedHistogram.length).foreach { idx =>
-      val currentHeight = paddedHistogram(idx)
-      val prevHeight    = paddedHistogram(idx - 1)
-      if currentHeight == prevHeight then
-        increasingHeights.push(Block(currentHeight, increasingHeights.pop().width + 1))
-      else if currentHeight > prevHeight then increasingHeights.push(Block(currentHeight, 1))
-      else {
-        var poppedLength = 0
-        while increasingHeights.head.height > currentHeight do
-          val prevBlock = increasingHeights.pop()
-          if (prevBlock.width + poppedLength) * prevBlock.height > maxBlock.width * maxBlock.height then
-            maxBlock = Block(prevBlock.height, prevBlock.width + poppedLength)
-          poppedLength += prevBlock.width
-        if currentHeight == increasingHeights.head.height then
-          increasingHeights.push(Block(currentHeight, increasingHeights.pop().width + poppedLength + 1))
-        else increasingHeights.push(Block(currentHeight, poppedLength + 1))
-      }
-    }
-    maxBlock.width * maxBlock.height
+    (heights :+ 0).zipWithIndex
+      .foldLeft(Seq.empty[Int], 0) { case ((stack, max), (currHeight, i)) =>
+        @annotation.tailrec
+        def dfs(stack: Seq[Int], max: Int): (Seq[Int], Int) =
+          if stack.isEmpty || heights(stack.last) < currHeight then (stack :+ i, max)
+          else
+            val st :+ j    = stack
+            val prevHigher = heights(j)
+            val width      = i - st.lastOption.map(1 + _).getOrElse(0)
+            dfs(st, max.max(prevHigher * width))
 
-  private case class Block(height: Int, width: Int)
+        dfs(stack, max)
+      }
+      ._2
