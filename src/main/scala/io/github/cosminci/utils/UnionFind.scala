31c7b9a1760fc802ae4611ed37ee5ecc01d03ff9
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-30 23:07:35 +0200

    [UTILS] Minor renaming in UnionFind + add rank accessor API

--- src/main/scala/io/github/cosminci/utils/UnionFind.scala
+++ src/main/scala/io/github/cosminci/utils/UnionFind.scala
@@ -3,19 +3,20 @@ package io.github.cosminci.utils
 import scala.collection.mutable
 
 class UnionFind[T]:
-  private val rank: mutable.Map[T, Int] = mutable.Map.empty.withDefaultValue(1)
-  private val parent: mutable.Map[T, T] = mutable.Map.empty.withDefault(n => n)
+  private val ranks: mutable.Map[T, Int] = mutable.Map.empty.withDefaultValue(1)
+  private val parents: mutable.Map[T, T] = mutable.Map.empty.withDefault(n => n)
 
   def union(n1: T, n2: T): T =
     val (p1, p2) = (find(n1), find(n2))
     if p1 != p2 then
-      val (lo, hi) = Option.when(rank(p1) >= rank(p2))((p2, p1)).getOrElse((p1, p2))
-      rank.update(hi, rank(lo) + rank(hi))
-      parent.update(lo, hi)
-    parent(p1)
+      val (lo, hi) = Option.when(ranks(p1) >= ranks(p2))((p2, p1)).getOrElse((p1, p2))
+      ranks.update(hi, ranks(lo) + ranks(hi))
+      parents.update(lo, hi)
+    parents(p1)
 
   def find(n: T): T =
-    if parent(n) != n then parent.update(n, find(parent(n)))
-    parent(n)
+    if parents(n) != n then parents.update(n, find(parents(n)))
+    parents(n)
 
-  def nodes(): Set[T] = parent.keySet.toSet
+  def nodes(): Set[T] = parents.keySet.toSet
+  def rank(n: T): Int = ranks(n)
