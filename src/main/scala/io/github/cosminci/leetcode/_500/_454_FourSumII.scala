3f9aac269df3f94febd79a00611419929c20464f
Cosmin Ciobanu <ciobanu@adobe.com>
2022-02-04 10:17:28 +0200

    [454] 4Sum II ADDENDUM * implement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_500/_454_FourSumII.scala
+++ src/main/scala/io/github/cosminci/leetcode/_500/_454_FourSumII.scala
@@ -7,16 +7,7 @@ object _454_FourSumII:
     println(fourSumCount(Array(1, 2), Array(-2, -1), Array(-1, 2), Array(0, 2)))
 
   def fourSumCount(nums1: Array[Int], nums2: Array[Int], nums3: Array[Int], nums4: Array[Int]): Int =
-    val nums12Sums = mutable.Map.empty[Int, Int]
-    nums1.foreach { n1 =>
-      nums2.foreach { n2 =>
-        nums12Sums.update(n1 + n2, nums12Sums.getOrElse(n1 + n2, 0) + 1)
-      }
-    }
-    var result = 0
-    nums3.foreach { n3 =>
-      nums4.foreach { n4 =>
-        result += nums12Sums.getOrElse(-(n3 + n4), 0)
-      }
-    }
-    result
+    val nums12Sums = for { n1 <- nums1; n2 <- nums2 } yield n1 + n2
+    val nums12SumCounts = nums12Sums.groupMapReduce(identity)(_ => 1)(_ + _)
+    val nums34Sums = for { n3 <- nums3; n4 <- nums4 } yield n3 + n4
+    nums34Sums.map(s => nums12SumCounts.getOrElse(-s, 0)).sum
