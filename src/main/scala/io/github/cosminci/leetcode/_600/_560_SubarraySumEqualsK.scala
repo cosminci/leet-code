a1092b264ee331c989a0afd5d286ca7c31ffe102
Cosmin Ciobanu <ciobanu@adobe.com>
2022-02-10 15:46:24 +0200

    [560] Subarray Sum Equals K ADDENDUM * simplify prefixSum + hashMap solution

--- src/main/scala/io/github/cosminci/leetcode/_600/_560_SubarraySumEqualsK.scala
+++ src/main/scala/io/github/cosminci/leetcode/_600/_560_SubarraySumEqualsK.scala
@@ -3,30 +3,13 @@ package io.github.cosminci.leetcode._600
 object _560_SubarraySumEqualsK:
   def main(args: Array[String]): Unit =
     println(subarraySum(Array(1, 2, 3), 3))
-    println(subarraySumMap(Array(1, 2, 3), 3))
-
-  def subarraySumMap(nums: Array[Int], k: Int): Int =
-    nums
-      .foldLeft((Map(0 -> 1), 0, 0)) { case ((cumulativeSumCounts, kCount, cumulativeSum), n) =>
-        val newCumulativeSum = cumulativeSum + n
-        val newKCount =
-          if cumulativeSumCounts.contains(newCumulativeSum - k) then kCount + cumulativeSumCounts(newCumulativeSum - k)
-          else kCount
-        val newCumulativeSumCounts = cumulativeSumCounts.updatedWith(newCumulativeSum) {
-          case None    => Some(1)
-          case Some(c) => Some(c + 1)
-        }
-        (newCumulativeSumCounts, newKCount, newCumulativeSum)
-      }
-      ._2
 
   def subarraySum(nums: Array[Int], k: Int): Int =
-    var count = 0
-    nums.indices.foreach { i =>
-      var sum = 0
-      (i until nums.length).foreach { j =>
-        sum = sum + nums(j)
-        if sum == k then count += 1
+    nums
+      .foldLeft((Map(0 -> 1), 0, 0)) { case ((sumCounts, count, prevSum), n) =>
+        val newSum       = prevSum + n
+        val newCount     = count + sumCounts.getOrElse(newSum - k, 0)
+        val newSumCounts = sumCounts + (newSum -> (sumCounts.getOrElse(newSum, 0) + 1))
+        (newSumCounts, newCount, newSum)
       }
-    }
-    count
+      ._2
