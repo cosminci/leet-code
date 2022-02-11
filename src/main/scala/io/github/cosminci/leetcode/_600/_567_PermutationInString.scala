18bf579488e6f41fc9371fc97e4732bff101e339
Cosmin Ciobanu <ciobanu@adobe.com>
2022-02-11 09:29:50 +0200

    [567] Permutation in String ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_600/_567_PermutationInString.scala
+++ src/main/scala/io/github/cosminci/leetcode/_600/_567_PermutationInString.scala
@@ -4,17 +4,18 @@ import io.github.cosminci.utils
 
 object _567_PermutationInString:
   def main(args: Array[String]): Unit =
-    println(checkInclusion("adc", "dcda"))
+    println(checkInclusion("abc", "ccccbabbb"))
 
   def checkInclusion(s1: String, s2: String): Boolean =
     if s1.length > s2.length then return false
 
     val s1Chars = utils.characterCounts(s1)
-    val s2Chars = utils.characterCounts(s2.take(s1.length)).toArray
+    val s2Chars = utils.characterCounts(s2.take(s1.length))
 
-    var s2Idx = s1.length
-    while s2Idx != s2.length && !s1Chars.sameElements(s2Chars) do
-      s2Chars(s2(s2Idx - s1.length) - 'a') -= 1
-      s2Chars(s2(s2Idx) - 'a') += 1
-      s2Idx += 1
-    s1Chars.sameElements(s2Chars)
+    (s1.length until s2.length).foldLeft(s2Chars) { case (s2Chars, s2Idx) =>
+      if s1Chars == s2Chars then return true
+      val oldChar = s2(s2Idx - s1.length) - 'a'
+      val newChar = s2(s2Idx) - 'a'
+      val shrunk  = s2Chars.updated(oldChar, s2Chars(oldChar) - 1)
+      shrunk.updated(newChar, shrunk(newChar) + 1)
+    } == s1Chars
