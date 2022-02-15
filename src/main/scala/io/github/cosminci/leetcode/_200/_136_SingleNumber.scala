9e2ee96d93d219cc46554d67b4c2b875a079c26c
Cosmin Ciobanu <ciobanu@adobe.com>
2022-02-15 08:57:57 +0200

    [136] Single Number ADDENDUM * use reduce instead of fold since nums.len > 0

--- src/main/scala/io/github/cosminci/leetcode/_200/_136_SingleNumber.scala
+++ src/main/scala/io/github/cosminci/leetcode/_200/_136_SingleNumber.scala
@@ -1,10 +1,5 @@
 package io.github.cosminci.leetcode._200
 
 object _136_SingleNumber:
-  def main(args: Array[String]): Unit =
-    println(singleNumber(Array(4, 1, 2, 1, 2)))
-
   def singleNumber(nums: Array[Int]): Int =
-    nums.tail.foldLeft(nums.head) { case (acc, n) =>
-      acc ^ n
-    }
+    nums.reduce(_ ^ _)
