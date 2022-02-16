876b5bcea6edc96881cb12b666c811b474d4601d
Cosmin Ciobanu <ciobanu@adobe.com>
2022-02-17 00:57:54 +0200

    [24] Swap Nodes in Pairs ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_100/_24_SwapNodesInPairs.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_24_SwapNodesInPairs.scala
@@ -2,13 +2,8 @@ package io.github.cosminci.leetcode._100
 
 import io.github.cosminci.utils.ListNode
 
-object _24_SwapNodesInPairs {
-  def swapPairs(head: ListNode): ListNode = {
-    if (head == null || head.next == null) return head
-    val newHead = head.next
-    val next = newHead.next
-    newHead.next = head
-    head.next = swapPairs(next)
-    newHead
-  }
-}
+object _24_SwapNodesInPairs:
+
+  def swapPairs(head: ListNode): ListNode =
+    if head == null || head.next == null then head
+    else new ListNode(head.next.x, new ListNode(head.x, swapPairs(head.next.next)))
