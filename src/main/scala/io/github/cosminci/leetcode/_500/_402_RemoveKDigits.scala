b6832d24c9013ef3d199f397189808bed1e5ceca
Cosmin Ciobanu <ciobanu@adobe.com>
2022-02-18 13:56:42 +0200

    [402] Remove K Digits ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_500/_402_RemoveKDigits.scala
+++ src/main/scala/io/github/cosminci/leetcode/_500/_402_RemoveKDigits.scala
@@ -11,15 +11,15 @@ object _402_RemoveKDigits:
     println(removeKdigits("9", 1))
 
   def removeKdigits(num: String, k: Int): String =
-    val builder = new StringBuilder()
-    var kLeft   = k
+    @annotation.tailrec
+    def dfs(stack: Seq[Char], idx: Int, kLeft: Int): Seq[Char] =
+      if idx == num.length then stack.dropRight(kLeft)
+      else if kLeft == 0 then stack ++ num.slice(idx, num.length)
+      else
+        stack.lastOption match
+          case None                    => dfs(stack :+ num(idx), idx + 1, kLeft)
+          case Some(v) if v > num(idx) => dfs(stack.dropRight(1), idx, kLeft - 1)
+          case Some(v)                 => dfs(stack :+ num(idx), idx + 1, kLeft)
 
-    num.foreach { d =>
-      while kLeft > 0 && builder.lastOption.exists(_ > d) do
-        builder.deleteCharAt(builder.length() - 1)
-        kLeft -= 1
-      builder.append(d)
-    }
-
-    val result = builder.dropRight(kLeft).dropWhile(_ == '0').toString
-    if result.isEmpty then "0" else result
+    val result = dfs(stack = Seq.empty, idx = 0, kLeft = k).dropWhile(_ == '0')
+    Option.when(result.isEmpty)("0").getOrElse(result.mkString)
