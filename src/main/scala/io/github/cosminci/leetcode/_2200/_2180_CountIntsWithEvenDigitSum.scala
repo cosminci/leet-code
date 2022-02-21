937c045cfebb38be61a83ff322ba12f6bcec17d2
Cosmin Ciobanu <ciobanu@adobe.com>
2022-02-21 23:35:50 +0200

    [2180] Count Integers With Even Digit Sum

---
+++ src/main/scala/io/github/cosminci/leetcode/_2200/_2180_CountIntsWithEvenDigitSum.scala
@@ -0,0 +1,10 @@
+package io.github.cosminci.leetcode._2200
+
+object _2180_CountIntsWithEvenDigitSum:
+
+  def countEven(num: Int): Int =
+    def dfs(n: Int): Int =
+      if n == 0 then 0
+      else n % 10 + dfs(n / 10)
+
+    (num - dfs(num) % 2) / 2
