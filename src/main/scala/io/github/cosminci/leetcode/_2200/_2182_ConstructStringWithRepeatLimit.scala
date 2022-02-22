9b3e68e41948f802682c0e33a39ac84072a2664c
Cosmin Ciobanu <ciobanu@adobe.com>
2022-02-22 09:48:01 +0200

    [2182] Construct String with Repeat Limit ADDENDUM * slight changes to feel less dirty

--- src/main/scala/io/github/cosminci/leetcode/_2200/_2182_ConstructStringWithRepeatLimit.scala
+++ src/main/scala/io/github/cosminci/leetcode/_2200/_2182_ConstructStringWithRepeatLimit.scala
@@ -1,33 +1,33 @@
 package io.github.cosminci.leetcode._2200
 
-import scala.collection.mutable
-import scala.collection.mutable.TreeMap
+import scala.collection.immutable.TreeMap
 
-object _2182_ConstructStringWithRepeatLimit:
+object _2182_ConstructStringWithRepeatLimit {
+  def main(args: Array[String]): Unit = {
+    println(repeatLimitedString("cczazcc", 3))
+  }
 
-  def repeatLimitedString(s: String, repeatLimit: Int): String =
-    val result = new StringBuilder("_")
-    val counts = mutable.TreeMap.from(s.groupMapReduce(identity)(_ => 1)(_ + _))(Ordering.Char.reverse)
-
+  def repeatLimitedString(s: String, repeatLimit: Int): String = {
     @annotation.tailrec
-    def dfs(): Unit =
-      if counts.isEmpty then ()
-      else
-        val maybeHead = Option.when(counts.head._1 != result.last)(counts.head)
-        maybeHead.orElse(counts.minAfter((result.last - 1).toChar)) match
-          case None => ()
+    def dfs(counts: TreeMap[Char, Int], result: StringBuilder, prev: Char): String =
+      if (counts.isEmpty) result.toString()
+      else {
+        Option.when(counts.head._1 != prev)(counts.head).orElse(counts.minAfter((prev - 1).toChar)) match {
+          case None => result.toString()
           case Some((char, count)) =>
-            val repeat = if counts.head._1 != char then 1 else count.min(repeatLimit)
-            result.append(s"$char" * repeat)
-            updateCount(counts, char, repeat)
-            dfs()
-
-    def updateCount(counts: mutable.TreeMap[Char, Int], char: Char, repeat: Int): Unit =
-      counts.updateWith(char) {
-        case None                   => None
-        case Some(v) if v <= repeat => None
-        case Some(v)                => Some(v - repeat)
+            val repeat = if (counts.head._1 != char) 1 else count.min(repeatLimit)
+            dfs(substract(counts, char, repeat), result.append(char.toString * repeat), char)
+        }
       }
 
-    dfs()
-    result.drop(1).toString()
+    def substract(counts: TreeMap[Char, Int], char: Char, repeat: Int): TreeMap[Char, Int] =
+      counts.updatedWith(char) {
+        case None => None
+        case Some(v) if v <= repeat => None
+        case Some(v) => Some(v - repeat)
+      }
+
+    val counts = TreeMap.from(s.groupMapReduce(identity)(_ => 1)(_ + _))(Ordering.Char.reverse)
+    dfs(counts, result = new StringBuilder, prev = '_')
+  }
+}
