0388a4951addb562841e578cd15458e4a2fa43f9
Cosmin Ciobanu <ciobanu@adobe.com>
2022-02-28 23:59:31 +0200

    [2186] Minimum Number of Steps to Make Two Strings Anagram II

---
+++ src/main/scala/io/github/cosminci/leetcode/_2200/_2186_MinStepsToMakeTwoStringsAnagramsII.scala
@@ -0,0 +1,13 @@
+package io.github.cosminci.leetcode._2200
+
+object _2186_MinStepsToMakeTwoStringsAnagramsII:
+  def minSteps(s: String, t: String): Int =
+    def countChars(s: String) = s.groupMapReduce(identity)(_ => 1)(_ + _)
+
+    def diffChars(s1Chars: Map[Char, Int], s2Chars: Map[Char, Int]) =
+      s1Chars.foldLeft(0) { case (acc, (char, count)) =>
+        acc + (count - s2Chars.getOrElse(char, 0)).max(0)
+      }
+
+    val (sChars, tChars) = (countChars(s), countChars(t))
+    diffChars(sChars, tChars) + diffChars(tChars, sChars)
