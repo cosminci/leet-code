4bc2b3a5682e6bb07f28710fd2f75c770e53efb3
Cosmin Ciobanu <ciobanu@adobe.com>
2022-03-01 09:38:48 +0200

    [338] Counting Bits ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_400/_338_CountingBits.scala
+++ src/main/scala/io/github/cosminci/leetcode/_400/_338_CountingBits.scala
@@ -2,24 +2,10 @@ package io.github.cosminci.leetcode._400
 
 object _338_CountingBits:
 
-  def countBitsBottomUp(n: Int): Array[Int] =
-    val dp = Array.ofDim[Int](n + 1)
-    dp(0) = 0
-    var offset = 1
-    (1 to n).foreach { i =>
-      if offset * 2 == i then offset = i
-      dp(i) = 1 + dp(i - offset)
-    }
-    dp
-
   def countBitsBruteForce(n: Int): Array[Int] =
-    val results = Array.ofDim[Int](n + 1)
-    (0 to n).foreach { i =>
-      var count = 0
-      var num   = i
-      while num != 0 do
-        count += num % 2
-        num = num / 2
-      results(i) = count
-    }
-    results
+    @annotation.tailrec
+    def dfs(n: Int, c: Int): Int =
+      if n == 0 then c
+      else dfs(n >> 1, c + (n & 1))
+
+    (0 to n).map(dfs(_, c = 0)).toArray
