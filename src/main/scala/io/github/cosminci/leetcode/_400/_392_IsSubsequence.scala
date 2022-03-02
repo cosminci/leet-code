3c1607bea9140e26e8cb5b3b76e1b9530d781d25
Cosmin Ciobanu <ciobanu@adobe.com>
2022-03-02 23:07:21 +0200

    [392] Is Subsequence ADDENDUM * minor refactoring

--- src/main/scala/io/github/cosminci/leetcode/_400/_392_IsSubsequence.scala
+++ src/main/scala/io/github/cosminci/leetcode/_400/_392_IsSubsequence.scala
@@ -1,24 +1,13 @@
 package io.github.cosminci.leetcode._400
 
-import scala.annotation.tailrec
-
 object _392_IsSubsequence:
-  def main(args: Array[String]): Unit =
-    println(isSubsequenceRecursive("abc", "ahbgdc"))
-    println(isSubsequenceLoop("abc", "ahbgdc"))
 
   def isSubsequenceRecursive(s: String, t: String): Boolean =
-    @tailrec
+    @annotation.tailrec
     def dfs(sIdx: Int, tIdx: Int): Boolean =
-      if sIdx == s.length then return true
-      if tIdx == t.length then return false
-      if s(sIdx) == t(tIdx) then dfs(sIdx + 1, tIdx + 1) else dfs(sIdx, tIdx + 1)
-    dfs(0, 0)
+      if sIdx == s.length then true
+      else if tIdx == t.length then false
+      else if s(sIdx) == t(tIdx) then dfs(sIdx + 1, tIdx + 1)
+      else dfs(sIdx, tIdx + 1)
 
-  def isSubsequenceLoop(s: String, t: String): Boolean =
-    var (sIdx, tIdx) = (0, 0)
-    while sIdx != s.length do
-      if tIdx == t.length then return false
-      if s(sIdx) == t(tIdx) then sIdx += 1
-      tIdx += 1
-    true
+    dfs(sIdx = 0, tIdx = 0)
