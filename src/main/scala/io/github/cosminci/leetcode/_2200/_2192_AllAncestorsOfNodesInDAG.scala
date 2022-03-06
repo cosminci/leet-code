6e1c2ed856dbc31181e752d463fb3365006e8fbf
Cosmin Ciobanu <ciobanu@adobe.com>
2022-03-06 12:44:53 +0200

    [2192] All Ancestors of a Node in a Directed Acyclic Graph

---
+++ src/main/scala/io/github/cosminci/leetcode/_2200/_2192_AllAncestorsOfNodesInDAG.scala
@@ -0,0 +1,22 @@
+package io.github.cosminci.leetcode._2200
+
+import scala.collection.mutable
+import scala.collection.immutable.TreeSet
+
+object _2192_AllAncestorsOfNodesInDAG {
+  def getAncestors(n: Int, edges: Array[Array[Int]]): List[List[Int]] = {
+    val adjList = edges.foldLeft(Map.empty[Int, TreeSet[Int]].withDefaultValue(TreeSet.empty[Int])) {
+      case (adjList, Array(from, to)) =>
+        adjList.updated(to, adjList(to) + from)
+    }
+
+    val mem = mutable.Map.empty[Int, TreeSet[Int]]
+    def dfs(node: Int): TreeSet[Int] =
+      mem.getOrElseUpdate(node, {
+        if (adjList(node).isEmpty) TreeSet.empty
+        else adjList(node).flatMap(ancestor => dfs(ancestor) + ancestor)
+      })
+
+    (0 until n).map(dfs(_).toList).toList
+  }
+}
