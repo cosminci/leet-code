25d317869c3ebb2261776b085337f978bc565250
Cosmin Ciobanu <ciobanu@adobe.com>
2022-03-06 17:19:54 +0200

    [2193] Minimum Number of Moves to Make Palindrome

---
+++ src/main/scala/io/github/cosminci/leetcode/_2200/_2193_MinNumOfMovesToMakePalindrome.scala
@@ -0,0 +1,13 @@
+package io.github.cosminci.leetcode._2200
+
+object _2193_MinNumOfMovesToMakePalindrome:
+
+  def minMovesToMakePalindrome(s: String): Int =
+    @annotation.tailrec
+    def dfs(s: String, res: Int): Int =
+      if s.isEmpty then res
+      else
+        val idx = s.indexOf(s.last)
+        if idx == s.length - 1 then dfs(s.dropRight(1), res + idx / 2)
+        else dfs(s.patch(idx, Nil, 1).dropRight(1), res + idx)
+    dfs(s, res = 0)
