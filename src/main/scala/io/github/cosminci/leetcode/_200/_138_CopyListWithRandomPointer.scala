8da8e2fe0689914e5d7083b5adeb123113f80aec
Cosmin Ciobanu <ciobanu@adobe.com>
2022-03-12 08:45:52 +0200

    [138] Copy List with Random Pointer ADDENDUM * reimplement w/ minimal mutability

--- src/main/scala/io/github/cosminci/leetcode/_200/_138_CopyListWithRandomPointer.scala
+++ src/main/scala/io/github/cosminci/leetcode/_200/_138_CopyListWithRandomPointer.scala
@@ -3,58 +3,23 @@ package io.github.cosminci.leetcode._200
 import scala.collection.mutable
 
 object _138_CopyListWithRandomPointer:
-  def main(args: Array[String]): Unit = {}
 
-  def copyRandomListDoublePass(head: Node): Node =
-    if head == null then return null
-    val oldToNew = mutable.Map.empty[Node, Node]
-    var curr     = head
-    while curr != null do
-      val newCurr = new Node(curr.value)
-      oldToNew.update(curr, newCurr)
-      curr = curr.next
-
-    curr = head
-    while curr != null do
-      val newCurr = oldToNew(curr)
-      newCurr.next = oldToNew.getOrElse(curr.next, null)
-      newCurr.random = oldToNew.getOrElse(curr.random, null)
-      curr = curr.next
-
-    oldToNew(head)
-
-  def copyRandomListSinglePass(head: Node): Node =
-    if head == null then return null
-
-    val newHead  = new Node(head.value)
-    val oldToNew = mutable.Map(head -> newHead)
-    val missingLinks =
-      if head.random != head then mutable.Map(head.random -> mutable.ListBuffer(newHead))
-      else {
-        newHead.random = newHead
-        mutable.Map.empty[Node, mutable.ListBuffer[Node]]
+  def copyRandomList(head: Node): Node =
+    val oldToNew = Iterator
+      .iterate(head)(_.next)
+      .takeWhile(_ != null)
+      .foldLeft(Map.empty[Node, Node]) { (acc, curr) =>
+        acc.updated(curr, new Node(curr.value))
       }
 
-    var (prev, curr) = (newHead, head.next)
-    while curr != null do
-      val newNode = new Node(curr.value)
-      oldToNew.update(curr, newNode)
-      prev.next = newNode
+    oldToNew.foreach { case (oldNode, newNode) =>
+      newNode.next = oldToNew.getOrElse(oldNode.next, null)
+      newNode.random = oldToNew.getOrElse(oldNode.random, null)
+    }
 
-      if missingLinks.contains(curr) then
-        missingLinks(curr).foreach { n =>
-          n.random = newNode
-        }
-
-      if oldToNew.contains(curr.random) then newNode.random = oldToNew(curr.random)
-      else missingLinks.getOrElseUpdate(curr.random, mutable.ListBuffer.empty).addOne(newNode)
-
-      prev = newNode
-      curr = curr.next
-
-    newHead
+    oldToNew.getOrElse(head, null)
 
   class Node(var _value: Int):
     var value: Int   = _value
-    var next: Node   = null
-    var random: Node = null
+    var next: Node   = _
+    var random: Node = _
