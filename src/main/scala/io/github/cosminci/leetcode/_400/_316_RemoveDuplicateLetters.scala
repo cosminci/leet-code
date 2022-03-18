ac0383d2facfab29aa03de933dd802531c8582b6
Cosmin Ciobanu <ciobanu@adobe.com>
2022-03-18 08:03:30 +0200

    [316] Remove Duplicate Letters ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_400/_316_RemoveDuplicateLetters.scala
+++ src/main/scala/io/github/cosminci/leetcode/_400/_316_RemoveDuplicateLetters.scala
@@ -8,8 +8,10 @@ object _316_RemoveDuplicateLetters:
     println(removeDuplicateLetters("bcabc"))
 
   def removeDuplicateLetters(s: String): String =
-    s.distinct.sorted.foreach { char =>
-      val suffix = s.substring(s.indexOf(char))
-      if suffix.toSet == s.toSet then return char +: removeDuplicateLetters(suffix.filter(_ != char))
-    }
-    ""
+    s.foldLeft(s.groupMapReduce(identity)(_ => 1)(_ + _), "", Set.empty[Char]) {
+      case ((counts, result, set), char) if set.contains(char) =>
+        (counts.updated(char, counts(char) - 1), result, set)
+      case ((counts, result, set), char) =>
+        val (drop, keep) = result.span(c => c > char && counts(c) > 0)
+        (counts.updated(char, counts(char) - 1), char +: keep, set.removedAll(drop) + char)
+    }._2.reverse
