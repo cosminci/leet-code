0f897652058250e38a1310fd9c64879461fa00d1
Cosmin Ciobanu <ciobanu@adobe.com>
2022-03-24 00:18:17 +0200

    [2210] Count Hills and Valleys in an Array

---
+++ src/main/scala/io/github/cosminci/leetcode/_2300/_2210_CountHillsAndValleysInAnArray.scala
@@ -0,0 +1,13 @@
+package io.github.cosminci.leetcode._2300
+
+object _2210_CountHillsAndValleysInAnArray:
+  def countHillValley(nums: Array[Int]): Int =
+    nums
+      .foldLeft(Array.empty[Int]) { (distinct, n) =>
+        if (distinct.isEmpty || n != distinct.last) distinct :+ n else distinct
+      }
+      .sliding(3)
+      .filter(_.length == 3)
+      .count { case Array(l, mid, r) =>
+        (l < mid && mid > r) || (l > mid && mid < r)
+      }
