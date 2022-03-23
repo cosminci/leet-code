16e6dfd8476d97bbab50527655b44cf13783e8bd
Cosmin Ciobanu <ciobanu@adobe.com>
2022-03-24 00:31:22 +0200

    [2211] Count Collisions on a Road

---
+++ src/main/scala/io/github/cosminci/leetcode/_2300/_2211_CountColissionsOnARoad.scala
@@ -0,0 +1,9 @@
+package io.github.cosminci.leetcode._2300
+
+object _2211_CountColissionsOnARoad:
+  def countCollisions(directions: String): Int =
+    directions
+      .dropWhile(_ == 'L')
+      .reverse
+      .dropWhile(_ == 'R')
+      .count(_ != 'S')
