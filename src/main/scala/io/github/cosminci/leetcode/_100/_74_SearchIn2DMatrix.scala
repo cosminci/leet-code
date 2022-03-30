9f5a020a9146bbcbcfdd496fd77bb29ac4b8ef7b
Cosmin Ciobanu <ciobanu@adobe.com>
2022-03-30 14:49:23 +0300

    [74] Search in 2D Matrix ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_100/_74_SearchIn2DMatrix.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_74_SearchIn2DMatrix.scala
@@ -16,12 +16,15 @@ object _74_SearchIn2DMatrix:
 
   def searchMatrix(matrix: Array[Array[Int]], target: Int): Boolean =
     def binarySearch(arr: Array[Int], t: Int): Int =
-      var (l, r) = (0, arr.length)
-      while l < r do
-        val mid = l + (r - l) / 2
-        if arr(mid) > t then r = mid
-        else l = mid + 1
-      math.max(0, l - 1)
+      @annotation.tailrec
+      def dfs(l: Int, r: Int): Int =
+        if l >= r then (l - 1).max(0)
+        else
+          val mid = l + (r - l) / 2
+          if arr(mid) > t then dfs(l, r = mid)
+          else dfs(l = mid + 1, r)
+      dfs(l = 0, r = arr.length)
+
     val targetRow = binarySearch(matrix.map(_.head), target)
     val targetCol = binarySearch(matrix(targetRow), target)
 
