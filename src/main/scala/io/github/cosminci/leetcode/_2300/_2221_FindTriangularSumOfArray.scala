3ab2d9d2389840d04f84208568fff4792a4d02c8
Cosmin Ciobanu <ciobanu@adobe.com>
2022-04-03 18:58:18 +0300

    [2221] Find Triangular Sum of an Array

---
+++ src/main/scala/io/github/cosminci/leetcode/_2300/_2221_FindTriangularSumOfArray.scala
@@ -0,0 +1,6 @@
+package io.github.cosminci.leetcode._2300
+
+object _2221_FindTriangularSumOfArray:
+  def triangularSum(nums: Array[Int]): Int =
+    val binomialCoefficients = nums.indices.scanLeft(BigInt(1))((mCk, k) => mCk * (nums.length - 1 - k) / (k + 1))
+    nums.zip(binomialCoefficients).foldLeft(BigInt(0)) { case (sum, (n, mCk)) => (sum + n * mCk) % 10 }.toInt
