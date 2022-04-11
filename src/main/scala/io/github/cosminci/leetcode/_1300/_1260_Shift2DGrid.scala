d4b0fab7e6735225900813cda62435bb4f905e6c
Cosmin Ciobanu <ciobanu@adobe.com>
2022-04-11 09:12:56 +0300

    [1260] Shift 2D Grid

---
+++ src/main/scala/io/github/cosminci/leetcode/_1300/_1260_Shift2DGrid.scala
@@ -0,0 +1,8 @@
+package io.github.cosminci.leetcode._1300
+
+object _1260_Shift2DGrid:
+
+  def shiftGrid(grid: Array[Array[Int]], k: Int): List[List[Int]] =
+    val (nums, cols) = (grid.flatten, grid(0).length)
+    val (tail, head) = nums.splitAt(nums.length - k % nums.length)
+    (head ++ tail).grouped(cols).map(_.toList).toList
