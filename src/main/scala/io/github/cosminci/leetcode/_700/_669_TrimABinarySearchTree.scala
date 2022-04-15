a6b6602bf056d50703f8abbe9ba4ddacec795b7b
Cosmin Ciobanu <ciobanu@adobe.com>
2022-04-15 09:40:37 +0300

    [669] Trim a Binary Search Tree ADDENDUM * simplify solution

--- src/main/scala/io/github/cosminci/leetcode/_700/_669_TrimABinarySearchTree.scala
+++ src/main/scala/io/github/cosminci/leetcode/_700/_669_TrimABinarySearchTree.scala
@@ -3,13 +3,9 @@ package io.github.cosminci.leetcode._700
 import io.github.cosminci.utils.TreeNode
 
 object _669_TrimABinarySearchTree:
+
   def trimBST(root: TreeNode, low: Int, high: Int): TreeNode =
-    if root == null then return null
-
-    if root.value < low then return trimBST(root.right, low, high)
-    if root.value > high then return trimBST(root.left, low, high)
-
-    root.left = trimBST(root.left, low, high)
-    root.right = trimBST(root.right, low, high)
-
-    root
+    if root == null then null
+    else if root.value < low then trimBST(root.right, low, high)
+    else if root.value > high then trimBST(root.left, low, high)
+    else new TreeNode(root.value, trimBST(root.left, low, high), trimBST(root.right, low, high))
