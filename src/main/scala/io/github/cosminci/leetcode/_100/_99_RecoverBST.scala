7aa98889acc249d19926d3e5f0336216ed6b5602
Cosmin Ciobanu <ciobanu@adobe.com>
2022-04-19 10:29:20 +0300

    [99] Recover Binary Search Tree ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_100/_99_RecoverBST.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_99_RecoverBST.scala
@@ -4,22 +4,19 @@ import io.github.cosminci.utils.TreeNode
 
 object _99_RecoverBST:
   def recoverTree(root: TreeNode): Unit =
-    var firstNode: TreeNode  = null
-    var secondNode: TreeNode = null
-    var prevNode: TreeNode   = null
+    @annotation.tailrec
+    def pushLeft(stack: Seq[TreeNode], node: TreeNode): Seq[TreeNode] =
+      if node == null then stack else pushLeft(stack :+ node, node.left)
 
-    def traverseInOrder(currentNode: TreeNode): Unit =
-      if currentNode == null then return ()
+    @annotation.tailrec
+    def dfs(curr: TreeNode, prev: TreeNode, violations: Array[(TreeNode, TreeNode)], stack: Seq[TreeNode]): Unit =
+      if curr == null && stack.isEmpty then
+        val tmp = violations.head._1.value
+        violations.head._1.value = violations.last._2.value
+        violations.last._2.value = tmp
+      else
+        val newStack :+ node = pushLeft(stack, curr)
+        val newViolations    = if node.value < prev.value then violations :+ (prev, node) else violations
+        dfs(curr = node.right, prev = node, newViolations, newStack)
 
-      traverseInOrder(currentNode.left)
-
-      if prevNode != null && firstNode == null && prevNode.value >= currentNode.value then firstNode = prevNode
-      if firstNode != null && prevNode.value >= currentNode.value then secondNode = currentNode
-      prevNode = currentNode
-
-      traverseInOrder(currentNode.right)
-
-    traverseInOrder(root)
-    val tmp = firstNode.value
-    firstNode.value = secondNode.value
-    secondNode.value = tmp
+    dfs(curr = root, prev = new TreeNode(Int.MinValue), violations = Array.empty, stack = Seq.empty)
