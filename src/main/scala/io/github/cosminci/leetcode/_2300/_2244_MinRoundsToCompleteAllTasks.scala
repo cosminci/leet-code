bd09803183862694c452af49c3f097eaadec4a63
Cosmin Ciobanu <ciobanu@adobe.com>
2022-04-21 00:27:52 +0300

    [2244] Minimum Rounds to Complete All Tasks

---
+++ src/main/scala/io/github/cosminci/leetcode/_2300/_2244_MinRoundsToCompleteAllTasks.scala
@@ -0,0 +1,12 @@
+package io.github.cosminci.leetcode._2300
+
+object _2244_MinRoundsToCompleteAllTasks:
+
+  def minimumRounds(tasks: Array[Int]): Int =
+    tasks
+      .groupMapReduce(identity)(_ => 1)(_ + _)
+      .values
+      .foldLeft(0) { (rounds, count) =>
+        if count == 1 then return -1
+        rounds + (count + 2) / 3
+      }
