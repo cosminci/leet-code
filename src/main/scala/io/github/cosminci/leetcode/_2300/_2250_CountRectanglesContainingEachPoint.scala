3e570c0ee54987b5e8d78c025d95684eb0042642
Cosmin Ciobanu <ciobanu@adobe.com>
2022-04-24 22:45:15 +0300

    [2250] Count Number of Rectangles Containing Each Point

---
+++ src/main/scala/io/github/cosminci/leetcode/_2300/_2250_CountRectanglesContainingEachPoint.scala
@@ -0,0 +1,24 @@
+package io.github.cosminci.leetcode._2300
+
+import scala.collection.Searching.*
+
+object _2250_CountRectanglesContainingEachPoint:
+
+  def countRectangles(rectangles: Array[Array[Int]], points: Array[Array[Int]]): Array[Int] =
+    val sortedWidths = rectangles
+      .foldLeft(Map.empty[Int, Array[Int]]) { case (acc, Array(width, height)) =>
+        acc.updatedWith(height) {
+          case None         => Some(Array(width))
+          case Some(widths) => Some(widths :+ width)
+        }
+      }.view.mapValues(_.sorted).toMap
+
+    points.map { case Array(x, y) =>
+      sortedWidths.map { case (height, widths) =>
+        if height < y then 0
+        else
+          widths.search(x) match
+            case Found(idx)          => widths.length - idx
+            case InsertionPoint(idx) => widths.length - idx
+      }.sum
+    }
