04c6c2104c36abd01617eb7e72f5fb5c3bac41b9
Cosmin Ciobanu <ciobanu@adobe.com>
2022-04-24 23:28:30 +0300

    [2251] Number of Flowers in Full Bloom

---
+++ src/main/scala/io/github/cosminci/leetcode/_2300/_2251_NumFlowersInFullBloom.scala
@@ -0,0 +1,14 @@
+package io.github.cosminci.leetcode._2300
+
+object _2251_NumFlowersInFullBloom:
+
+  def fullBloomFlowers(flowers: Array[Array[Int]], persons: Array[Int]): Array[Int] =
+    val prefixSum = flowers
+      .flatMap { case Array(start, end) => Array((start, 1), (end + 1, -1)) }
+      .sorted
+      .scanLeft((0, 0)) { case ((_, flowersInBloom), (time, delta)) => (time, flowersInBloom + delta) }
+
+    persons.map { time =>
+      val idx = prefixSum.search((time, Int.MaxValue)).insertionPoint - 1
+      prefixSum(idx)._2
+    }
