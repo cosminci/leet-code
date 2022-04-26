04f3fea4cbde176eaf55d01b28d257cc52aa7eef
Cosmin Ciobanu <ciobanu@adobe.com>
2022-04-26 08:28:08 +0300

    [1584] Min Cost to Connect All Points ADDENDUM * reimplement [Prim's algorithm]

--- src/main/scala/io/github/cosminci/leetcode/_1600/_1584_MinCostToConnectAllPoints.scala
+++ src/main/scala/io/github/cosminci/leetcode/_1600/_1584_MinCostToConnectAllPoints.scala
@@ -5,39 +5,34 @@ import scala.collection.mutable
 object _1584_MinCostToConnectAllPoints:
 
   def main(args: Array[String]): Unit =
-    println(minCostConnectPoints(Array(Array(0, 0), Array(2, 2), Array(3, 10), Array(5, 2), Array(7, 0))))
+    println(minCostConnectPointsPrim(Array(Array(0, 0), Array(2, 2), Array(3, 10), Array(5, 2), Array(7, 0))))
 
-  def minCostConnectPoints(points: Array[Array[Int]]): Int =
-    if points.length == 1 then return 0
+  def minCostConnectPointsPrim(points: Array[Array[Int]]): Int =
+    val pq      = mutable.PriorityQueue.empty[(Int, Int)]
+    val visited = Array.ofDim[Boolean](points.length)
 
-    val adjList = mutable.Map.empty[Point, mutable.Set[Edge]]
-    points.foreach { case Array(x1, y1) =>
-      points.foreach { case Array(x2, y2) =>
-        if x1 != x2 || y1 != y2 then
-          val edgeWeight = math.abs(x1 - x2) + math.abs(y1 - y2)
-          adjList
-            .getOrElseUpdate(Point(x1, y1), mutable.Set.empty)
-            .add(Edge(Point(x2, y2), edgeWeight))
-          adjList
-            .getOrElseUpdate(Point(x2, y2), mutable.Set.empty)
-            .add(Edge(Point(x1, y1), edgeWeight))
+    def enqueueNewPoints(i: Int): Unit =
+      points.indices
+        .filter(n => !visited(n))
+        .foreach { j =>
+          val (Array(x0, y0), Array(x1, y1)) = (points(i), points(j))
+          val distance                       = (x0 - x1).abs + (y0 - y1).abs
+          pq.enqueue((-distance, j))
+        }
+
+    def nextCheapestPoint(curr: Int) =
+      Iterator
+        .iterate((0, curr))(_ => pq.dequeue())
+        .dropWhile { case (_, j) => visited(j) }
+        .next()
+
+    Iterator
+      .iterate((0, 0, 1)) { case (totalCost, i, connected) =>
+        visited(i) = true
+        enqueueNewPoints(i)
+        val (distance, j) = nextCheapestPoint(i)
+        (totalCost - distance, j, connected + 1)
       }
-    }
-    given Ordering[Edge] = (e1, e2) => e2.weight.compareTo(e1.weight)
-
-    val root    = adjList.keysIterator.next()
-    val toVisit = mutable.PriorityQueue.from(adjList(root))
-    val visited = mutable.Set(root)
-
-    var cost = 0
-    while visited.size != points.length do
-      val edge = toVisit.dequeue()
-      if !visited.contains(edge.adjacent) then
-        visited.add(edge.adjacent)
-        toVisit.addAll(adjList(edge.adjacent))
-        cost += edge.weight
-    cost
-
-  case class Point(x: Int, y: Int)
-
-  case class Edge(adjacent: Point, weight: Int)
+      .dropWhile { case (_, _, connected) => connected < points.length }
+      .next()
+      ._1
