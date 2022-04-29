39308c7544aa238c491ccb0d6e0cd7884d07dba1
Cosmin Ciobanu <ciobanu@adobe.com>
2022-04-29 09:37:49 +0300

    [1631] Path With Minimum Effort

---
+++ src/main/scala/io/github/cosminci/leetcode/_1700/_1631_PathWithMinEffort.scala
@@ -0,0 +1,28 @@
+package io.github.cosminci.leetcode._1700
+
+import scala.collection.mutable
+
+import io.github.cosminci.utils
+
+object _1631_PathWithMinEffort:
+
+  def minimumEffortPath(heights: Array[Array[Int]]): Int =
+    val (m, n)    = (heights.length, heights.head.length)
+    val toVisit   = mutable.PriorityQueue.from(Seq((0, 0, 0)))
+    val minEffort = Array.tabulate(m, n) { case (x, y) => if x == 0 && y == 0 then 0 else Int.MaxValue }
+
+    @annotation.tailrec
+    def dfs(): Int =
+      val (effort, x, y) = toVisit.dequeue()
+      if x == m - 1 && y == n - 1 then return -effort
+      utils
+        .neighbours(x, y, heights)
+        .foreach { case (nx, ny) =>
+          val newEffort = (heights(nx)(ny) - heights(x)(y)).abs.max(-effort)
+          if newEffort < minEffort(nx)(ny) then
+            minEffort(nx)(ny) = newEffort
+            toVisit.enqueue((-newEffort, nx, ny))
+        }
+      dfs()
+
+    dfs()
