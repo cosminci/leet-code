308843ed377af6b26b3a59c716379af926b5e0ac
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-08 01:24:36 +0300

    [2260] Minimum Consecutive Cards to Pick Up

---
+++ src/main/scala/io/github/cosminci/leetcode/_2300/_2260_MinConsecutiveCardsToPickUp.scala
@@ -0,0 +1,12 @@
+package io.github.cosminci.leetcode._2300
+
+object _2260_MinConsecutiveCardsToPickUp:
+
+  def minimumCardPickup(cards: Array[Int]): Int =
+    val minDistance = cards.zipWithIndex
+      .foldLeft(Map.empty[Int, Int], Int.MaxValue) { case ((lastPosition, minDistance), (card, idx)) =>
+        lastPosition.get(card) match
+          case Some(prevIdx) => (lastPosition.updated(card, idx), minDistance.min(idx - prevIdx + 1))
+          case None          => (lastPosition.updated(card, idx), minDistance)
+      }._2
+    if minDistance == Int.MaxValue then -1 else minDistance
