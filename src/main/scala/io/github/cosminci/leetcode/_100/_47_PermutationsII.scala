a2a430900b443594852e4432c90882e0afd0bea9
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-12 08:48:05 +0300

    [47] Permutations II ADDENDUM * replace flatMap with for comprehension * use splitAt for better semantics * remove memoization

--- src/main/scala/io/github/cosminci/leetcode/_100/_47_PermutationsII.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_47_PermutationsII.scala
@@ -1,24 +1,17 @@
 package io.github.cosminci.leetcode._100
 
-import scala.collection.mutable
-
 object _47_PermutationsII:
   def main(args: Array[String]): Unit =
     println(permuteUnique(Array(1, 2, 3)))
 
   def permuteUnique(input: Array[Int]): List[List[Int]] =
-    val mem = mutable.Map.empty[Int, Set[Seq[Int]]]
+    def dfs(idx: Int): Set[List[Int]] =
+      if idx == input.length - 1 then Set(List(input.last))
+      else for
+        permutation <- dfs(idx + 1)
+        splitIdx    <- 0 to permutation.size
+      yield
+        val (left, right) = permutation.splitAt(splitIdx)
+        left :+ input(idx) :++ right
 
-    def dfs(idx: Int): Set[Seq[Int]] =
-      if idx == input.length - 1 then return Set(Seq(input.last))
-      if mem.contains(idx) then return mem(idx)
-
-      val result = dfs(idx + 1).flatMap { permutation =>
-        (0 to permutation.size).map { i =>
-          (permutation.take(i) :+ input(idx)) ++ permutation.takeRight(permutation.size - i)
-        }
-      }
-      mem.update(idx, result)
-      result
-
-    dfs(0).map(_.toList).toList
+    dfs(idx = 0).toList
