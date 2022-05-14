5c99f72eff9eb86fb97d2f6a9a5191359c464d9f
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-14 08:55:39 +0300

    [743] Network Delay Time ADDENDUM * reimplement using Bellman-Ford

--- src/main/scala/io/github/cosminci/leetcode/_800/_743_NetworkTimeDelay.scala
+++ src/main/scala/io/github/cosminci/leetcode/_800/_743_NetworkTimeDelay.scala
@@ -3,27 +3,15 @@ package io.github.cosminci.leetcode._800
 import scala.collection.mutable
 
 object _743_NetworkTimeDelay:
-  def main(args: Array[String]): Unit =
-    println(networkDelayTime(Array(Array(1, 2, 1), Array(2, 3, 2), Array(1, 3, 4)), 3, 1))
-  def networkDelayTime(times: Array[Array[Int]], n: Int, k: Int): Int =
-    val adjList = mutable.Map.empty[Int, mutable.Map[Int, Int]]
-    (1 to n).foreach { node =>
-      adjList.update(node, mutable.Map.empty)
-    }
-    times.foreach { case Array(from, to, weight) =>
-      adjList(from).update(to, weight)
-    }
-    given Ordering[(Int, Int)] = (x, y) => y._2.compareTo(x._2)
 
-    val pqueue  = mutable.PriorityQueue((k, 0))
-    val visited = mutable.Set.empty[Int]
-    var max     = 0
-    while pqueue.nonEmpty do
-      val (node, timeSoFar) = pqueue.dequeue()
-      if !visited.contains(node) then
-        visited.add(node)
-        max = math.max(max, timeSoFar)
-        adjList(node).foreach { case (neighbour, timeToReach) =>
-          pqueue.enqueue((neighbour, timeSoFar + timeToReach))
-        }
-    if visited.size == n then max else -1
+  def networkDelayTime(times: Array[Array[Int]], n: Int, k: Int): Int =
+    val dist = Array.tabulate(n + 1)(i => if i == k then 0 else Int.MaxValue)
+
+    for
+      _              <- 0 until n
+      Array(u, v, w) <- times
+      if dist(u) != Int.MaxValue
+    do dist(v) = dist(v).min(dist(u) + w)
+
+    val maxDist = dist.tail.max
+    if maxDist < Int.MaxValue then maxDist else -1
