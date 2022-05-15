fed5a0120dfa0404b7f6b1f085207aaaecf797b3
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-15 12:22:37 +0300

    [2273] Find Resultant Array After Removing Anagrams

---
+++ src/main/scala/io/github/cosminci/leetcode/_2300/_2273_FindResultantArrayAfterRemovingAnagrams.scala
@@ -0,0 +1,8 @@
+package io.github.cosminci.leetcode._2300
+
+object _2273_FindResultantArrayAfterRemovingAnagrams:
+
+  def removeAnagrams(words: Array[String]): List[String] =
+    words.indices.collect {
+      case i if i == 0 || words(i).sorted != words(i - 1).sorted => words(i)
+    }.toList
