9fed718989c27ec86daf84f2a7c133ad107687fb
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-19 09:56:41 +0300

    [329] Longest Increasing Path in a Matrix ADDENDUM * reimplement more concisely

--- src/main/scala/io/github/cosminci/leetcode/_400/_329_LongestIncreasingPathInAMatrix.scala
+++ src/main/scala/io/github/cosminci/leetcode/_400/_329_LongestIncreasingPathInAMatrix.scala
@@ -5,41 +5,18 @@ import io.github.cosminci.utils
 import scala.collection.mutable
 
 object _329_LongestIncreasingPathInAMatrix:
-  def main(args: Array[String]): Unit =
-    println(
-      longestIncreasingPath(
-        Array(
-          Array(9, 9, 4),
-          Array(6, 6, 8),
-          Array(2, 1, 1)
-        )
-      )
-    )
 
   def longestIncreasingPath(matrix: Array[Array[Int]]): Int =
-    val computed           = Array.ofDim[Int](matrix.length, matrix.head.length)
-    var overallLongestPath = 1
+    val (m, n)                 = (matrix.length, matrix.head.length)
+    def inGrid(x: Int, y: Int) = x >= 0 && x < m && y >= 0 && y < n
 
-    def dfs(x: Int, y: Int): Int =
-      val longestPath = utils
-        .neighbours(x, y, matrix)
-        .collect {
-          case (nx, ny) if matrix(nx)(ny) < matrix(x)(y) =>
-            if computed(nx)(ny) != 0 then computed(nx)(ny) else dfs(nx, ny)
-        }
+    val mem = mutable.Map.empty[(Int, Int), Int]
+    def dfs(x: Int, y: Int): Int = mem.getOrElseUpdate((x, y),
+      Seq((-1, 0), (0, -1), (0, 1), (1, 0))
+        .collect { case (dx, dy) if inGrid(x + dx, y + dy) => (x + dx, y + dy) }
+        .collect { case (nx, ny) if matrix(nx)(ny) < matrix(x)(y) => dfs(nx, ny) }
         .maxOption
         .getOrElse(0) + 1
+    )
 
-      computed(x)(y) = longestPath
-      longestPath
-
-    matrix.indices.foreach { row =>
-      matrix(row).indices.foreach { col =>
-        val coordinate = (row, col)
-        if computed(row)(col) == 0 then
-          val longestPath = dfs(row, col)
-          overallLongestPath = math.max(overallLongestPath, longestPath)
-      }
-    }
-
-    overallLongestPath
+    (0 until m).flatMap(x => (0 until n).map(y => dfs(x, y))).max
