66bf702f361e09806937efc469aa6554b46733f7
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-20 14:31:00 +0300

    [63] Unique Paths II ADDENDUM * reimplement using top-down DP w/ memoization

--- src/main/scala/io/github/cosminci/leetcode/_100/_63_UniquePathsII.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_63_UniquePathsII.scala
@@ -1,16 +1,18 @@
 package io.github.cosminci.leetcode._100
 
+import scala.collection.mutable
+
 object _63_UniquePathsII:
+
   def uniquePathsWithObstacles(obstacleGrid: Array[Array[Int]]): Int =
-    val m  = obstacleGrid.length
-    val n  = obstacleGrid.head.length
-    val dp = Array.ofDim[Int](m + 1, n + 1)
-    dp(0)(1) = 1
-    (1 to m).foreach { i =>
-      (1 to n).foreach { j =>
-        dp(i)(j) =
-          if obstacleGrid(i - 1)(j - 1) == 1 then 0
-          else dp(i - 1)(j) + dp(i)(j - 1)
-      }
-    }
-    dp(m)(n)
+    val mem = mutable.Map.empty[(Int, Int), Int]
+
+    def dfs(x: Int, y: Int): Int = mem.getOrElseUpdate((x, y),
+      if obstacleGrid(x)(y) == 1 then 0
+      else if x == 0 && y == 0 then 1
+      else if x == 0 then dfs(x, y - 1)
+      else if y == 0 then dfs(x - 1, y)
+      else dfs(x - 1, y) + dfs(x, y - 1)
+    )
+
+    dfs(x = obstacleGrid.length - 1, y = obstacleGrid.head.length - 1)
