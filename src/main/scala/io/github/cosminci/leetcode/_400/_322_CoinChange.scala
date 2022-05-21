13029cd400bf0cf62a320e8cf946ebcd8cd96d63
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-21 23:52:07 +0300

    [322] Coin Change ADDENDUM * reimplement top-down dp w/ memoization more concisely

--- src/main/scala/io/github/cosminci/leetcode/_400/_322_CoinChange.scala
+++ src/main/scala/io/github/cosminci/leetcode/_400/_322_CoinChange.scala
@@ -3,43 +3,19 @@ package io.github.cosminci.leetcode._400
 import scala.collection.mutable
 
 object _322_CoinChange:
-  def main(args: Array[String]): Unit =
-    List(
-      (Array(1, 2, 5), 11),
-      (Array(2), 3),
-      (Array(1), 0),
-      (Array(1), 1),
-      (Array(1), 2)
-    ).foreach { case (coins, amount) =>
-      println(s"TopDown: ${coinChangeTopDown(coins, amount)}")
-      println(s"BottomUp: ${coinChangeBottomUp(coins, amount)}")
-    }
 
-  def coinChangeBottomUp(coins: Array[Int], amount: Int): Int =
-    val dp = Array.fill[Int](amount + 1)(amount + 1)
-    dp(0) = 0
-    (1 to amount).foreach { a =>
-      coins.foreach { c =>
-        if a - c >= 0 then dp(a) = math.min(dp(a), dp(a - c) + 1)
-      }
-    }
-    if dp.last != amount + 1 then dp.last else -1
-
-  def coinChangeTopDown(coins: Array[Int], amount: Int): Int =
-    if amount == 0 then return 0
-
+  def coinChange(coins: Array[Int], amount: Int): Int =
     val mem = mutable.Map.empty[Int, Int]
-    def dfs(sum: Int): Int =
-      if sum == 0 then return 0
-      if sum < 0 then return -1
-      if mem.contains(sum) then return mem(sum)
 
-      val result = coins
-        .collect { case c if dfs(sum - c) >= 0 => dfs(sum - c) }
+    def dfs(sum: Int): Int = mem.getOrElseUpdate(sum,
+      if sum < 0 then -1
+      else if sum == 0 then 0
+      else coins
+        .map(c => dfs(sum - c))
+        .filter(_ >= 0)
         .minOption
         .map(_ + 1)
         .getOrElse(-1)
+    )
 
-      mem.update(sum, result)
-      result
     dfs(amount)
