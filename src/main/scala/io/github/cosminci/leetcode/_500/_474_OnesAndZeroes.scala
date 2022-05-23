b42cd833ce398ba80db38266bb45e80fcc47db48
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-23 08:04:51 +0300

    [474] Ones and Zeroes ADDENDUM * reimplement more concisely

--- src/main/scala/io/github/cosminci/leetcode/_500/_474_OnesAndZeroes.scala
+++ src/main/scala/io/github/cosminci/leetcode/_500/_474_OnesAndZeroes.scala
@@ -3,9 +3,6 @@ package io.github.cosminci.leetcode._500
 import scala.collection.mutable
 
 object _474_OnesAndZeroes:
-  def main(args: Array[String]): Unit =
-    println(findMaxForm(Array("10", "0001", "111001", "1", "0"), 5, 3))
-    println(findMaxForm(Array("10", "0", "1"), 1, 1))
 
   def findMaxForm(strs: Array[String], m: Int, n: Int): Int =
     def oneZeroCounts(s: String): (Int, Int) =
@@ -14,21 +11,14 @@ object _474_OnesAndZeroes:
       }
 
     val sorted = strs.map(oneZeroCounts).sortBy { case (ones, zeroes) => -(ones + zeroes) }
-    val mem    = mutable.Map.empty[(Int, Int, Int), Int]
-    def dfs(idx: Int, mBudget: Int, nBudget: Int): Int =
-      if idx == strs.length then return 0
-      if mem.contains((idx, mBudget, nBudget)) then return mem((idx, mBudget, nBudget))
 
-      val (ones, zeroes) = sorted(idx)
-      val result =
-        if ones > nBudget || zeroes > mBudget then dfs(idx + 1, mBudget, nBudget)
-        else
-          math.max(
-            1 + dfs(idx + 1, mBudget - zeroes, nBudget - ones),
-            dfs(idx + 1, mBudget, nBudget)
-          )
+    val mem = mutable.Map.empty[(Int, Int, Int), Int]
+    def dfs(i: Int, m: Int, n: Int): Int = mem.getOrElseUpdate((i, m, n),
+      if i == strs.length then 0
+      else
+        val (ones, zeroes) = sorted(i)
+        if ones > n || zeroes > m then dfs(i + 1, m, n)
+        else dfs(i + 1, m, n).max(1 + dfs(i + 1, m - zeroes, n - ones))
+    )
 
-      mem.update((idx, mBudget, nBudget), result)
-      result
-
-    dfs(idx = 0, mBudget = m, nBudget = n)
+    dfs(i = 0, m, n)
