af58c1f4ef2abbf9bf3305f4cab1420b4b519800
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-23 07:59:00 +0300

    [647] Palindromic Substrings ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_700/_647_PalindromicSubstrings.scala
+++ src/main/scala/io/github/cosminci/leetcode/_700/_647_PalindromicSubstrings.scala
@@ -7,16 +7,11 @@ object _647_PalindromicSubstrings:
     println(countSubstrings("aaa"))
 
   def countSubstrings(s: String): Int =
-    (1 until s.length).foldLeft(1) { case (acc, origin) =>
-      acc +
-        count(s, left = origin, right = origin) +
-        count(s, left = origin - 1, right = origin)
-    }
+    @annotation.tailrec
+    def dfs(l: Int, r: Int, cnt: Int): Int =
+      if l < 0 || r == s.length || s(l) != s(r) then cnt
+      else dfs(l - 1, r + 1, cnt + 1)
 
-  def count(s: String, left: Int, right: Int): Int =
-    var (result, l, r) = (0, left, right)
-    while l >= 0 && r <= s.length - 1 && s(l) == s(r) do
-      l -= 1
-      r += 1
-      result += 1
-    result
+    (1 until s.length).map { i =>
+      dfs(l = i, r = i, cnt = 0) + dfs(l = i - 1, r = i, cnt = 0)
+    }.sum + 1
