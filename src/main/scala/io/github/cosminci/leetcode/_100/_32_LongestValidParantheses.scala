cafe3f44fd702379b56d533c0bc679a63f6b0a98
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-24 21:11:42 +0300

    [32] Longest Valid Parantheses ADDENDUM * reimplement with stack

--- src/main/scala/io/github/cosminci/leetcode/_100/_32_LongestValidParantheses.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_32_LongestValidParantheses.scala
@@ -1,27 +1,11 @@
 package io.github.cosminci.leetcode._100
 
 object _32_LongestValidParantheses:
-  def main(args: Array[String]): Unit =
-    println((longestValidParentheses("(()")))
-    println((longestValidParentheses(")()())")))
-    println((longestValidParentheses("")))
 
   def longestValidParentheses(s: String): Int =
-    var max = 0
-
-    var (l, r) = (0, 0)
-    s.indices.foreach { i =>
-      if s(i) == '(' then l += 1 else r += 1
-      if l == r then max = math.max(max, r * 2)
-      else if r > l then
-        l = 0; r = 0
-    }
-
-    l = 0; r = 0
-    s.indices.reverse.foreach { i =>
-      if s(i) == '(' then l += 1 else r += 1
-      if l == r then max = math.max(max, l * 2)
-      else if l > r then
-        l = 0; r = 0
-    }
-    max
+    s.indices
+      .foldLeft(Array(-1), 0) { case ((stack, result), i) =>
+        if s.charAt(i) == ')' && stack.length > 1 && s(stack.last) == '(' then
+          (stack.dropRight(1), result.max(i - stack.dropRight(1).last))
+        else (stack :+ i, result)
+      }._2
