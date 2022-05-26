ec6cfddb814e07f07161fd36a1c798268e1af4a5
Cosmin Ciobanu <ciobanu@adobe.com>
2022-05-26 12:08:07 +0300

    [2279] Maximum Bags With Full Capacity of Rocks

---
+++ src/main/scala/io/github/cosminci/leetcode/_2300/_2279_MaxBagsWithFullCapacityOfRocks.scala
@@ -0,0 +1,19 @@
+package io.github.cosminci.leetcode._2300
+
+object _2279_MaxBagsWithFullCapacityOfRocks:
+
+  def maximumBags(capacity: Array[Int], rocks: Array[Int], additionalRocks: Int): Int =
+    capacity
+      .zip(rocks)
+      .map { case (c, r) => c - r }
+      .sorted
+      .foldLeft(0, additionalRocks) { case ((fullBags, rocksLeft), capacityLeft) =>
+        if capacityLeft == 0 then (fullBags + 1, rocksLeft)
+        else
+          val rocksToUse      = capacityLeft.min(rocksLeft)
+          val newRocksLeft    = rocksLeft - rocksToUse
+          val newCapacityLeft = capacityLeft - rocksToUse
+          if newCapacityLeft == 0 then (fullBags + 1, newRocksLeft)
+          else return fullBags
+      }
+      ._1
