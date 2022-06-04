ab65134b43ab214b1bb6d56c81e0e49e53f8b6c4
Cosmin Ciobanu <ciobanu@adobe.com>
2022-06-04 13:10:35 +0300

    [51] NQueens ADDENDUM * minor refactoring

--- src/main/scala/io/github/cosminci/leetcode/_100/_51_NQueens.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_51_NQueens.scala
@@ -1,34 +1,28 @@
 package io.github.cosminci.leetcode._100
 
-import scala.annotation.tailrec
-
 object _51_NQueens:
   def main(args: Array[String]): Unit =
     println(solveNQueens(7))
 
   def solveNQueens(n: Int): List[List[String]] =
-    @tailrec
+    @annotation.tailrec
     def solve(prevConfigs: List[List[(Int, Int)]]): List[List[(Int, Int)]] =
-      if prevConfigs.isEmpty then return List.empty
-      val newCol = prevConfigs.head.length
-      if newCol == n then return prevConfigs
+      if prevConfigs.isEmpty then List.empty
+      else if prevConfigs.head.length == n then prevConfigs
+      else
+        val newConfigs = for
+          prevConfig <- prevConfigs
+          row        <- 0 until n
+          col = prevConfigs.head.length
+          if isValid(prevConfig, row, col)
+        yield prevConfig :+ (row, col)
+        solve(newConfigs)
 
-      val newConfigs = prevConfigs.flatMap { prevValidConfig =>
-        (0 until n).map(i => (i, newCol)).collect {
-          case newPosition if isValid(prevValidConfig, newPosition) =>
-            prevValidConfig :+ newPosition
-        }
-      }
-      solve(newConfigs)
-
-    solve((0 until n).map(i => List((i, 0))).toList).map { queens =>
-      val grid = Array.fill(n, n)('.')
-      queens.foreach { case (x, y) => grid(x)(y) = 'Q' }
-      grid.map(_.mkString).toList
+    solve((0 until n).map(row => List((row, 0))).toList).map { queens =>
+      Array.tabulate(n, n) { case pos => if queens.contains(pos) then 'Q' else '.' }.map(_.mkString).toList
     }
 
-  def isValid(prevValidConfig: List[(Int, Int)], newPosition: (Int, Int)) =
-    val (x, y) = newPosition
+  private def isValid(prevValidConfig: List[(Int, Int)], x: Int, y: Int) =
     prevValidConfig.forall { case (prevX, prevY) =>
       prevX != x && prevY != y && (prevX - prevY) != (x - y) && (prevX + prevY) != (x + y)
     }
