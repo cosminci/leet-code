6d9dcbbf88de1bc0f57407b0290e43517b12e465
Cosmin Ciobanu <ciobanu@adobe.com>
2022-06-09 11:49:20 +0300

    [1332] Remove Palindromic Subsequences

---
+++ src/main/scala/io/github/cosminci/leetcode/_1400/_1332_RemovePalindromicSubsequences.scala
@@ -0,0 +1,8 @@
+package io.github.cosminci.leetcode._1400
+
+object _1332_RemovePalindromicSubsequences:
+
+  def removePalindromeSub(s: String): Int =
+    if s.isEmpty then 0
+    else if s == s.reverse then 1
+    else 2
