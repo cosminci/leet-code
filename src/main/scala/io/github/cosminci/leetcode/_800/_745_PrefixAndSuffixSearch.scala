114ac7e36dc7d36d6710d17e612fe2591a1e43eb
Cosmin Ciobanu <ciobanu@adobe.com>
2022-06-18 20:27:42 +0300

    [745] Prefix and Suffix Search ADDENDUM * simplify solution

--- src/main/scala/io/github/cosminci/leetcode/_800/_745_PrefixAndSuffixSearch.scala
+++ src/main/scala/io/github/cosminci/leetcode/_800/_745_PrefixAndSuffixSearch.scala
@@ -4,58 +4,28 @@ import scala.collection.mutable
 
 object _745_PrefixAndSuffixSearch:
 
-  def main(args: Array[String]): Unit =
-    val wordFilter = new WordFilter(
-      Array(
-        "cabaabaaaa",
-        "ccbcababac",
-        "bacaabccba",
-        "bcbbcbacaa",
-        "abcaccbcaa",
-        "accabaccaa",
-        "cabcbbbcca",
-        "ababccabcb",
-        "caccbbcbab",
-        "bccbacbcba"
-      )
-    )
-    println(wordFilter.f("bccbacbcba", "a"))
-    println(wordFilter.f("ab", "abcaccbcaa"))
-    println(wordFilter.f("a", "aa"))
-    println(wordFilter.f("cabaaba", "abaaaa"))
-    println(wordFilter.f("cacc", "accbbcbab"))
-
   class WordFilter(val words: Array[String]):
-    private class TrieNode(val next: mutable.Map[Char, TrieNode], var highestIndex: Int)
+    private class TrieNode(val next: mutable.Map[Char, TrieNode] = mutable.Map.empty, var highestIndex: Int = -1)
 
-    private val root = new TrieNode(mutable.Map.empty, highestIndex = -1)
-
-    words.indices.foreach { i =>
-      val word = words(i)
+    private val root = words.zipWithIndex.foldLeft(new TrieNode()) { case (root, (word, i)) =>
       (0 until word.length)
-        .map { j =>
-          val (fh, sh) = word.splitAt(j)
-          s"$sh#$word"
-        }
-        .foreach { w =>
-          w.foldLeft(root) { case (node, char) =>
+        .map(i => s"${word.substring(i)}#$word")
+        .foldLeft(root) { (root, wordWrap) =>
+          wordWrap.foldLeft(root) { (node, char) =>
             if node.next.contains(char) then
-              val next = node.next(char)
-              next.highestIndex = i
-              next
+              val nextNode = node.next(char)
+              nextNode.highestIndex = i
+              nextNode
             else
-              val newNode = new TrieNode(mutable.Map.empty, highestIndex = i)
+              val newNode = new TrieNode(highestIndex = i)
               node.next.update(char, newNode)
               newNode
           }
+          root
         }
     }
 
     def f(prefix: String, suffix: String): Int =
-      val target = s"$suffix#$prefix"
-      target
-        .foldLeft(root) { case (node, char) =>
-          if node.next.contains(char) then node.next(char)
-          else return -1
-        }
+      s"$suffix#$prefix"
+        .foldLeft(root)((node, char) => if node.next.contains(char) then node.next(char) else return -1)
         .highestIndex
