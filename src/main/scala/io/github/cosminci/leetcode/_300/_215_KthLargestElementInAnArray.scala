76489189bafbb09bd90584c7898a134e419f9706
Cosmin Ciobanu <ciobanu@adobe.com>
2022-06-22 09:37:30 +0300

    [215] Kth Largest Element in an Array ADDENDUM * minor refactoring

--- src/main/scala/io/github/cosminci/leetcode/_300/_215_KthLargestElementInAnArray.scala
+++ src/main/scala/io/github/cosminci/leetcode/_300/_215_KthLargestElementInAnArray.scala
@@ -3,25 +3,14 @@ package io.github.cosminci.leetcode._300
 import scala.collection.mutable
 
 object _215_KthLargestElementInAnArray:
-  def main(args: Array[String]): Unit =
-    println(findKthLargest(Array(3, 2, 1, 5, 6, 4), 2))
-    println(findKthLargestPivot(Array(3, 2, 1, 5, 6, 4), 2))
-    println(findKthLargestPivot(Array(4, 2, 3, 1, 2, 3, 5, 5, 6), 9))
 
   def findKthLargest(nums: Array[Int], k: Int): Int =
-    val smallest = mutable.PriorityQueue.from(nums)
-    var result   = 0
-    (1 to k).foreach(_ => result = smallest.dequeue())
-    result
-
-  def findKthLargestPivot(nums: Array[Int], k: Int): Int =
-    val target = nums.length - k
-
-    def swap(i: Int, j: Int) =
+    def swap(i: Int, j: Int): Unit =
       val tmp = nums(j)
       nums(j) = nums(i)
       nums(i) = tmp
 
+    @annotation.tailrec
     def findPivot(start: Int, end: Int): Int =
       val pivot = nums(end)
       var i     = start
@@ -30,8 +19,8 @@ object _215_KthLargestElementInAnArray:
           swap(i, j)
           i += 1
       }
-      if i - 1 == target then pivot
-      else if i - 1 < target then findPivot(i, end)
+      if i - 1 == nums.length - k then pivot
+      else if i - 1 < nums.length - k then findPivot(i, end)
       else findPivot(start, i - 2)
 
     findPivot(0, nums.length - 1)
