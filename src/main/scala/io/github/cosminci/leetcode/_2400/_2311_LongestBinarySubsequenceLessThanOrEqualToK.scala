8e008ec6e3ff1ddc8688280def10df2551c2935d
Cosmin Ciobanu <ciobanu@adobe.com>
2022-06-22 12:55:13 +0300

    [2311] Longest Binary Subsequence Less Than or Equal to K

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2311_LongestBinarySubsequenceLessThanOrEqualToK.scala
@@ -0,0 +1,12 @@
+package io.github.cosminci.leetcode._2400
+
+object _2311_LongestBinarySubsequenceLessThanOrEqualToK:
+
+  def longestSubsequence(s: String, k: Int): Int =
+    @annotation.tailrec
+    def dfs(i: Int, value: Int, pow: Int, count: Int): Int =
+      if i < 0 || value + pow > k then s.count(_ == '0') + count
+      else if s(i) == '1' then dfs(i - 1, value + pow, pow << 1, count + 1)
+      else dfs(i - 1, value, pow << 1, count)
+
+    dfs(i = s.indices.last, value = 0, pow = 1, count = 0)
