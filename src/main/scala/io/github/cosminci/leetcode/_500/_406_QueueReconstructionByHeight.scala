390eef99e628d5b79fd351bb96378ba9f9528abe
Cosmin Ciobanu <ciobanu@adobe.com>
2022-06-29 09:04:11 +0300

    [406] Queue Reconstruction by Height ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_500/_406_QueueReconstructionByHeight.scala
+++ src/main/scala/io/github/cosminci/leetcode/_500/_406_QueueReconstructionByHeight.scala
@@ -3,20 +3,12 @@ package io.github.cosminci.leetcode._500
 import java.util.LinkedList
 
 object _406_QueueReconstructionByHeight:
-  def main(args: Array[String]): Unit =
-    println(
-      reconstructQueue(
-        Array(Array(7, 0), Array(4, 4), Array(7, 1), Array(5, 0), Array(6, 1), Array(5, 2))
-      ).map(_.toList).toList
-    )
 
   def reconstructQueue(people: Array[Array[Int]]): Array[Array[Int]] =
-    val result = new LinkedList[Array[Int]]()
     people
-      .sortWith { case (x, y) =>
-        if x(0) == y(0) then x(1) < y(1) else y(0) < x(0)
+      .sortBy { case Array(h, k) => (-h, k) }
+      .foldLeft(Array.empty[Array[Int]]) {
+        case (result, p @ Array(_, k)) =>
+          val (fh, sh) = result.splitAt(k)
+          (fh :+ p) ++ sh
       }
-      .foreach { person =>
-        result.add(person(1), person)
-      }
-    return result.toArray(people)
