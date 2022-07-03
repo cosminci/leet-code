fa8522ef4a7e1a709e5f7c862912b958b92ef573
Cosmin Ciobanu <ciobanu@adobe.com>
2022-07-03 08:56:49 +0300

    [2325] Decode the Message

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2325_DecodeTheMessage.scala
@@ -0,0 +1,11 @@
+package io.github.cosminci.leetcode._2400
+
+object _2325_DecodeTheMessage:
+
+  def decodeMessage(key: String, message: String): String =
+    val table = ('a' to 'z')
+      .sortBy(c => key.indexOf(c))
+      .zip('a' to 'z')
+      .toMap + (' ' -> ' ')
+
+    message.map(table)
