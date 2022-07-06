f8d31412bbdd62a05a64d339a68c6db39a8989d0
Cosmin Ciobanu <ciobanu@adobe.com>
2022-07-06 09:04:13 +0300

    [509] Fibonacci Number ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_600/_509_FibonacciNumber.scala
+++ src/main/scala/io/github/cosminci/leetcode/_600/_509_FibonacciNumber.scala
@@ -6,13 +6,8 @@ object _509_FibonacciNumber:
     println(fib(4))
 
   def fib(n: Int): Int =
-    var prev2 = 0
-    var prev1 = 1
-    if n == 0 then return prev2
-    if n == 1 then return prev1
-    (2 to n).foreach { _ =>
-      val f = prev2 + prev1
-      prev2 = prev1
-      prev1 = f
-    }
-    prev1
+    Iterator
+      .iterate((0, 1)) { case (prev2, prev1) => (prev1, prev2 + prev1) }
+      .drop(n + 1)
+      .next()
+      ._1
