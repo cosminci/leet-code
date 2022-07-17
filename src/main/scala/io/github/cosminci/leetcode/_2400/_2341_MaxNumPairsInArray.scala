f8cfe990a893ceea13c7a88dfe177a34ad594339
Cosmin Ciobanu <ciobanu@adobe.com>
2022-07-17 21:59:13 +0300

    [2341] Maximum Number of Pairs in Array

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2341_MaxNumPairsInArray.scala
@@ -0,0 +1,10 @@
+package io.github.cosminci.leetcode._2400
+
+object _2341_MaxNumPairsInArray:
+
+  def numberOfPairs(nums: Array[Int]): Array[Int] =
+    nums
+      .groupMapReduce(identity)(_ => 1)(_ + _)
+      .foldLeft(Array(0, 0)) { case (result, (_, count)) =>
+        Array(result(0) + count / 2, result(1) + count % 2)
+      }
