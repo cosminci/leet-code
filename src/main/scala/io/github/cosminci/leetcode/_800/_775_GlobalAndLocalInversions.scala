4fe3714c746660649628045a2848897fb5d9ab0a
Cosmin Ciobanu <ciobanu@adobe.com>
2022-07-17 21:20:23 +0300

    [MINOR] Move solution to correct package

--- src/main/scala/io/github/cosminci/leetcode/_800/_775_GlobalAndLocalInversions.scala
+++ src/main/scala/io/github/cosminci/leetcode/_800/_775_GlobalAndLocalInversions.scala
@@ -1,13 +1,11 @@
-package io.github.cosminci.leetcode._700
+package io.github.cosminci.leetcode._800
 
-object _775_GlobalAndLocalInversions {
-  def main(args: Array[String]): Unit = {
+object _775_GlobalAndLocalInversions:
+  def main(args: Array[String]): Unit =
     println(isIdealPermutation(Array(2, 1, 0)))
     println(isIdealPermutation(Array(2, 0, 1)))
     println(isIdealPermutation(Array(1, 0, 2)))
     println(isIdealPermutation(Array(1, 2, 0)))
-  }
 
   def isIdealPermutation(nums: Array[Int]): Boolean =
     nums.indices.forall(i => math.abs(nums(i) - i) <= 1)
-}
