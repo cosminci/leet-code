85c804a70738f98a35e0d43b77f900d0509aa93e
Cosmin Ciobanu <ciobanu@adobe.com>
2022-07-21 21:27:52 +0300

    [92] Reverse Linked List II * simplify implementation

--- src/main/scala/io/github/cosminci/leetcode/_100/_92_ReverseLinkedListII.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_92_ReverseLinkedListII.scala
@@ -1,34 +1,22 @@
 package io.github.cosminci.leetcode._100
 
-import io.github.cosminci.utils._
+import io.github.cosminci.utils.*
 
 object _92_ReverseLinkedListII:
-  def main(args: Array[String]): Unit =
-    println(linkedListToSeq(reverseBetween(seqToLinkedList(Seq(5, 1)), 1, 2)))
 
   def reverseBetween(head: ListNode, left: Int, right: Int): ListNode =
-    if right - left < 1 then return head
+    val dummy    = new ListNode(0, head)
+    val preStart = Iterator.iterate(dummy)(_.next).drop(left - 1).next()
 
-    val dummy = new ListNode(0, head)
+    val (preLast, last) = Iterator
+      .iterate((preStart.next, preStart.next.next)) { case (prev, curr) =>
+        val next = curr.next
+        curr.next = prev
+        (curr, next)
+      }
+      .drop(right - left)
+      .next()
 
-    var (prev, curr, count) = (dummy, head, 1)
-    while count < left do
-      prev = curr
-      curr = curr.next
-      count += 1
-
-    val startHead = prev
-    prev = curr
-    curr = curr.next
-
-    while count < right do
-      val next = curr.next
-      curr.next = prev
-      prev = curr
-      curr = next
-      count += 1
-
-    startHead.next.next = curr
-    startHead.next = prev
-
+    preStart.next.next = last
+    preStart.next = preLast
     dummy.next
