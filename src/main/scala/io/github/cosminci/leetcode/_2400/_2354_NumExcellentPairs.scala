514833c0076db8bbee75403d8ec258bcea9447c7
Cosmin Ciobanu <ciobanu@adobe.com>
2022-07-25 00:11:02 +0300

    [2354] Number of Excellent Pairs

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2354_NumExcellentPairs.scala
@@ -0,0 +1,16 @@
+package io.github.cosminci.leetcode._2400
+
+import io.github.cosminci.leetcode._200._191_NumberOfOneBits.hammingWeight
+
+object _2354_NumExcellentPairs:
+
+  def countExcellentPairs(nums: Array[Int], k: Int): Long =
+    val hwCounter = nums.distinct.groupMapReduce(hammingWeight)(_ => 1L)(_ + _)
+
+    val counts = for
+      (hw1, cnt1) <- hwCounter
+      (hw2, cnt2) <- hwCounter
+      if hw1 + hw2 >= k
+    yield cnt1 * cnt2
+
+    counts.sum
