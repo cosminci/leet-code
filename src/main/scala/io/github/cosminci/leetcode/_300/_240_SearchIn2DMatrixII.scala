90a9f36ae2baefdabcbe4ca275255870ff086d1e
Cosmin Ciobanu <ciobanu@adobe.com>
2022-07-24 07:47:17 +0300

    [240] Search a 2D Matrix II ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_300/_240_SearchIn2DMatrixII.scala
+++ src/main/scala/io/github/cosminci/leetcode/_300/_240_SearchIn2DMatrixII.scala
@@ -1,25 +1,15 @@
 package io.github.cosminci.leetcode._300
 
-object _240_SearchIn2DMatrixII {
-  def main(args: Array[String]): Unit = {
-    println(searchMatrix(Array(
-      Array(1, 4, 7, 11, 15),
-      Array(2, 5, 8, 12, 19),
-      Array(3, 6, 9, 16, 22),
-      Array(10, 13, 14, 17, 24),
-      Array(18, 21, 23, 26, 30)
-    ), 30))
-  }
+object _240_SearchIn2DMatrixII:
 
-  def searchMatrix(matrix: Array[Array[Int]], target: Int): Boolean = {
-    var (topRow, lastCol) = (0, matrix.head.length - 1)
-    while (topRow < matrix.length && lastCol >= 0) {
-      if (matrix(topRow)(lastCol) == target) return true
-      if (matrix(topRow)(lastCol) > target)
-        lastCol -= 1
-      else
-        topRow += 1
-    }
-    return false
-  }
-}
+  def searchMatrix(matrix: Array[Array[Int]], target: Int): Boolean =
+    Iterator
+      .iterate((matrix.indices.last, 0)) { case (r, c) =>
+        if matrix(r)(c) > target then (r - 1, c)
+        else if matrix(r)(c) < target then (r, c + 1)
+        else return true
+      }
+      .dropWhile { case (r, c) => r >= 0 && c < matrix.head.length }
+      .next()
+
+    false
