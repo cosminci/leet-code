7c16146c75a137e3a84300da857c2259fafa86e3
Cosmin Ciobanu <ciobanu@adobe.com>
2022-07-24 21:24:33 +0300

    [2349] Design a Number Container System

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2349_DesignANumberContainerSystem.scala
@@ -0,0 +1,17 @@
+package io.github.cosminci.leetcode._2400
+
+import scala.collection.mutable
+
+object _2349_DesignANumberContainerSystem:
+
+  class NumberContainers:
+    private val idxToNum = mutable.Map.empty[Int, Int]
+    private val numToIdx = mutable.Map.empty[Int, mutable.TreeSet[Int]]
+
+    def change(idx: Int, num: Int): Unit =
+      idxToNum.get(idx).foreach(prev => numToIdx(prev).remove(idx))
+      idxToNum.update(idx, num)
+      numToIdx.getOrElseUpdate(num, mutable.TreeSet.empty).add(idx)
+
+    def find(num: Int): Int =
+      numToIdx.get(num).flatMap(_.headOption).getOrElse(-1)
