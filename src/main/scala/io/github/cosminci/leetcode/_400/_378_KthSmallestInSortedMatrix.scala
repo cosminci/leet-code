d119f28bd653e26ddc6238fae62118b12c59b896
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-02 08:33:37 +0300

    [378] Kth Smallest Element in a Sorted Matrix ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_400/_378_KthSmallestInSortedMatrix.scala
+++ src/main/scala/io/github/cosminci/leetcode/_400/_378_KthSmallestInSortedMatrix.scala
@@ -1,28 +1,16 @@
 package io.github.cosminci.leetcode._400
 
-import scala.collection.mutable
+import scala.collection.immutable.TreeSet
 
 object _378_KthSmallestInSortedMatrix:
 
-  def main(args: Array[String]): Unit =
-    println(kthSmallest(Array(Array(1, 2, 3), Array(2, 3, 4), Array(3, 4, 5)), 9))
-
   def kthSmallest(matrix: Array[Array[Int]], k: Int): Int =
-    if k == 1 then return matrix(0)(0)
-    if matrix.length == 1 then return matrix(0)(k - 1)
-    if matrix(0).length == 1 then return matrix(k - 1)(0)
-
-    given Ordering[XYElem] = (x: XYElem, y: XYElem) => y.value.compareTo(x.value)
-    val pqueue = mutable.PriorityQueue.from(matrix(0).zipWithIndex.map { case (value, col) =>
-      XYElem(value, 0, col)
-    })
-
-    (1 to k)
-      .foldLeft(XYElem(0, 0, 0)) { case (_, _) =>
-        val min = pqueue.dequeue()
-        if min.x + 1 < matrix.length then pqueue.enqueue(XYElem(matrix(min.x + 1)(min.y), min.x + 1, min.y))
-        min
+    Iterator
+      .iterate(TreeSet.from(matrix.head.zipWithIndex.map { case (v, c) => (v, 0, c) })) { heap =>
+        val (_, r, c) = heap.head
+        heap.tail ++ Option.when(r < matrix.length - 1)((matrix(r + 1)(c), r + 1, c))
       }
-      .value
-
-  case class XYElem(value: Int, x: Int, y: Int)
+      .drop(k - 1)
+      .next()
+      .head
+      ._1
