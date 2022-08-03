a45556052fcd949e3316d5c4d117b6502d8255f6
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-03 08:32:21 +0300

    [729] My Calendar I ADDENDUM * simplify implementation

--- src/main/scala/io/github/cosminci/leetcode/_800/_729_MyCalendarI.scala
+++ src/main/scala/io/github/cosminci/leetcode/_800/_729_MyCalendarI.scala
@@ -1,22 +1,14 @@
 package io.github.cosminci.leetcode._800
 
-import java.util.{Comparator, TreeMap}
+import java.util
 import scala.util.Try
 
 object _729_MyCalendarI:
-  def main(args: Array[String]): Unit =
-    val calendar = new MyCalendar()
-    println(calendar.book(10, 20))
-    println(calendar.book(15, 25))
-    println(calendar.book(20, 30))
 
-  class MyCalendar():
-    private val bookings = new TreeMap[Int, Int]
+  class MyCalendar:
+    private val bookings = new util.TreeMap[Int, Int]
 
     def book(start: Int, end: Int): Boolean =
       val prevEnd   = Try(bookings.floorEntry(start).getValue).getOrElse(Int.MinValue)
       val nextStart = Try(bookings.ceilingEntry(start).getKey).getOrElse(Int.MaxValue)
-      if prevEnd > start || nextStart < end then return false
-
-      bookings.put(start, end)
-      true
+      Option.when(prevEnd <= start && nextStart >= end)(bookings.put(start, end)).isDefined
