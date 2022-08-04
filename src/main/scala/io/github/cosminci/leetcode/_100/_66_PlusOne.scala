06407e5a63be483de6cbefacfb3c89e60c2ee84b
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-04 08:45:39 +0300

    [66] Plus One

---
+++ src/main/scala/io/github/cosminci/leetcode/_100/_66_PlusOne.scala
@@ -0,0 +1,10 @@
+package io.github.cosminci.leetcode._100
+
+object _66_PlusOne:
+
+  def plusOne(digits: Array[Int]): Array[Int] =
+    val (res, carry) = digits.foldRight(Array.empty[Int], 1) { case (d, (res, carry)) =>
+      if d + carry == 10 then (0 +: res, 1)
+      else ((d + carry) +: res, 0)
+    }
+    if carry == 1 then 1 +: res else res
