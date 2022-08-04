b68a8da537cc0cd6e2a25f5b4ae7f0a6d194d71f
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-04 08:24:49 +0300

    [27] Remove Element

---
+++ src/main/scala/io/github/cosminci/leetcode/_100/_27_RemoveElement.scala
@@ -0,0 +1,11 @@
+package io.github.cosminci.leetcode._100
+
+object _27_RemoveElement:
+
+  def removeElement(nums: Array[Int], v: Int): Int =
+    nums.indices.foldLeft(0) {
+      case (k, i) if nums(i) == v => k
+      case (k, i) =>
+        nums(k) = nums(i)
+        k + 1
+    }
