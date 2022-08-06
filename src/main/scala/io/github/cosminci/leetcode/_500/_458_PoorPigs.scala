c6905a345a62f074c8590bf9dcb0d35474ea3784
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-06 10:08:37 +0300

    [458] Poor Pigs ADDENDUM * add second solution

--- src/main/scala/io/github/cosminci/leetcode/_500/_458_PoorPigs.scala
+++ src/main/scala/io/github/cosminci/leetcode/_500/_458_PoorPigs.scala
@@ -7,3 +7,6 @@ object _458_PoorPigs:
       .iterate(0)(_ + 1)
       .dropWhile(pigs => math.pow(minutesToTest / minutesToDie + 1, pigs) < buckets)
       .next()
+
+  def poorPigs2(buckets: Int, minutesToDie: Int, minutesToTest: Int): Int =
+    math.ceil(math.log(buckets) / math.log(minutesToTest / minutesToDie + 1)).toInt
