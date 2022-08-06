cca00bb50a61d8d5245545f45d1c0fcc04a73856
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-07 01:26:02 +0300

    [2363] Merge Similar Items

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2363_MergeSimilarItems.scala
@@ -0,0 +1,10 @@
+package io.github.cosminci.leetcode._2400
+
+object _2363_MergeSimilarItems:
+
+  def mergeSimilarItems(items1: Array[Array[Int]], items2: Array[Array[Int]]): List[List[Int]] =
+    (items1 ++ items2)
+      .groupMapReduce(_.head)(_.last)(_ + _)
+      .toList
+      .sorted
+      .map { case (value, totalWeight) => List(value, totalWeight) }
