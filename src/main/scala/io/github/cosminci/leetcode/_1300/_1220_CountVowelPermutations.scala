72cdaa7d7c355ea5cb6dab2a34f4f2b433786ae3
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-07 08:49:26 +0300

    [1220] Count Vowels Permutation ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_1300/_1220_CountVowelPermutations.scala
+++ src/main/scala/io/github/cosminci/leetcode/_1300/_1220_CountVowelPermutations.scala
@@ -2,22 +2,15 @@ package io.github.cosminci.leetcode._1300
 
 object _1220_CountVowelPermutations:
 
-  private val div                = (math.pow(10, 9) + 7).toLong
-
-  def main(args: Array[String]): Unit =
-    println(countVowelPermutation(20000))
-
   def countVowelPermutation(n: Int): Int =
-    val dp = Array.ofDim[Long](n, 5)
-    (0 until 5).foreach(i => dp(0)(i) = 1)
-    (1 until n).foreach { i =>
-      val prevCounts = dp(i - 1)
-      dp(i)(0) = mod(prevCounts(1) + prevCounts(2) + prevCounts(4))
-      dp(i)(1) = mod(prevCounts(0) + prevCounts(2))
-      dp(i)(2) = mod(prevCounts(1) + prevCounts(3))
-      dp(i)(3) = mod(prevCounts(2))
-      dp(i)(4) = mod(prevCounts(2) + prevCounts(3))
-    }
-    mod(dp.last.sum).toInt
-
-  def mod(n: Long): Long = n % div
+    ((1 until n)
+      .foldLeft(Array.fill[Long](5)(1)) { (dp, _) =>
+        Array(
+          dp(1) + dp(2) + dp(4),
+          dp(0) + dp(2),
+          dp(1) + dp(3),
+          dp(2),
+          dp(2) + dp(3)
+        ).map(_ % 1_000_000_007)
+      }
+      .sum % 1_000_000_007).toInt
