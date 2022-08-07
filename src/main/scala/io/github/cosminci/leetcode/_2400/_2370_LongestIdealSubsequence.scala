146849b109a602052fae592cb404a3e8d7a8ccd3
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-07 17:43:30 +0300

    [2370] Longest Ideal Subsequence

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2370_LongestIdealSubsequence.scala
@@ -0,0 +1,14 @@
+package io.github.cosminci.leetcode._2400
+
+object _2370_LongestIdealSubsequence:
+
+  def longestIdealString(s: String, k: Int): Int =
+    s.foldLeft(Map.empty[Char, Int]) { (longestByLastChar, char) =>
+      longestByLastChar.updated(char,
+        (-k to k)
+          .map(shift => (char + shift).toChar)
+          .flatMap(longestByLastChar.get)
+          .maxOption
+          .getOrElse(0) + 1
+      )
+    }.values.max
