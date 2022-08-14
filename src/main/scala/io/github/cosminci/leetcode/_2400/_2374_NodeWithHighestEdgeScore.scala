c7b4fc59c054e2dd5ab054d9d556f8ae5193741d
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-14 14:24:24 +0300

    [2374] Node With Highest Edge Score

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2374_NodeWithHighestEdgeScore.scala
@@ -0,0 +1,9 @@
+package io.github.cosminci.leetcode._2400
+
+object _2374_NodeWithHighestEdgeScore:
+
+  def edgeScore(edges: Array[Int]): Int =
+    edges.indices
+      .groupMapReduce(edges)(_.toLong)(_ + _)
+      .maxBy { case (node, edgeScore) => (edgeScore, -node) }
+      ._1
