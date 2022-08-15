77b06cdbe6d82e23eb035ec86ad8f139212fe6b5
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-15 09:32:04 +0300

    [13] Roman to Integer ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_100/_13_RomanToInteger.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_13_RomanToInteger.scala
@@ -2,26 +2,11 @@ package io.github.cosminci.leetcode._100
 
 object _13_RomanToInteger:
 
-  def main(args: Array[String]): Unit =
-    println(romanToInt("MMMCMXCIII"))
-
   def romanToInt(s: String): Int =
-    var result   = 0
-    var idx      = 0
-    var currChar = romanValue(s(idx))
-    while idx < s.length - 1 do
-      val nextChar = romanValue(s(idx + 1))
-      if currChar >= nextChar then result += currChar
-      else result -= currChar
-      currChar = nextChar
-      idx += 1
-    result + romanValue(s.last)
+    val values = Map('M' -> 1000, 'D' -> 500, 'C' -> 100, 'L' -> 50, 'X' -> 10, 'V' -> 5, 'I' -> 1)
 
-  private def romanValue(char: Char) = char match
-    case 'M' => 1000
-    case 'D' => 500
-    case 'C' => 100
-    case 'L' => 50
-    case 'X' => 10
-    case 'V' => 5
-    case 'I' => 1
+    if s.length == 1 then values(s.head)
+    else
+      s.indices.tail.sliding(2).foldLeft(0) { case (res, Seq(curr, next)) =>
+        if curr < next then res - curr else res + curr
+      } + values(s.last)
