4c951cca684037d83b57d3e6d3f958958edf4ae6
Cosmin Ciobanu <cosmin.ciobanu@gmail.com>
2022-08-21 00:40:30 +0300

    [2379] Minimum Recolors to Get K Consecutive Black Blocks

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2379_MinRecolorToGetKConsecutiveBlocks.scala
@@ -0,0 +1,8 @@
+package io.github.cosminci.leetcode._2400
+
+object _2379_MinRecolorToGetKConsecutiveBlocks {
+
+  def minimumRecolors(blocks: String, k: Int): Int =
+    blocks.sliding(k).map(_.count(_ == 'W')).min
+
+}
