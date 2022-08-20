c95b7918f7e72eed27c87349d6623099e2f31457
Cosmin Ciobanu <cosmin.ciobanu@gmail.com>
2022-08-21 00:54:21 +0300

    [2380] Time Needed to Rearrange a Binary String

--- src/main/scala/io/github/cosminci/leetcode/_2400/_2379_MinRecolorToGetKConsecutiveBlocks.scala
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2379_MinRecolorToGetKConsecutiveBlocks.scala
@@ -1,8 +1,6 @@
 package io.github.cosminci.leetcode._2400
 
-object _2379_MinRecolorToGetKConsecutiveBlocks {
+object _2379_MinRecolorToGetKConsecutiveBlocks:
 
   def minimumRecolors(blocks: String, k: Int): Int =
     blocks.sliding(k).map(_.count(_ == 'W')).min
-
-}
