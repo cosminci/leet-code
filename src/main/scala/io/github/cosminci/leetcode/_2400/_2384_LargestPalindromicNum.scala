7c6e9a5eba6cfe6c1687757318e63cdf8919e305
Cosmin Ciobanu <cosmin.ciobanu@gmail.com>
2022-08-21 16:34:53 +0300

    [2384] Largest Palindromic Number

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2384_LargestPalindromicNum.scala
@@ -0,0 +1,14 @@
+package io.github.cosminci.leetcode._2400
+
+object _2384_LargestPalindromicNum:
+
+  def largestPalindromic(num: String): String =
+    val counts = num.groupMapReduce(identity)(_ => 1)(_ + _).withDefaultValue(0)
+
+    val center = "9876543210".find(digit => counts(digit) % 2 == 1)
+    val leftHalf = "9876543210"
+      .flatMap(digit => digit.toString * (counts(digit) / 2))
+      .dropWhile(_ == '0')
+
+    val result = leftHalf ++ center ++ leftHalf.reverse
+    if result.isEmpty then "0" else result
