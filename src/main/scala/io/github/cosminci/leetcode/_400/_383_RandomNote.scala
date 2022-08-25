368a62385614e0299aa494dad0b708840c957da2
Cosmin Ciobanu <cosmin.ciobanu@gmail.com>
2022-08-25 07:48:13 +0300

    [383] Ransom Note

---
+++ src/main/scala/io/github/cosminci/leetcode/_400/_383_RandomNote.scala
@@ -0,0 +1,9 @@
+package io.github.cosminci.leetcode._400
+
+object _383_RandomNote:
+
+  def canConstruct(ransomNote: String, magazine: String): Boolean =
+    val available = magazine.groupMapReduce(identity)(_ => 1)(_ + _)
+    ransomNote.groupMapReduce(identity)(_ => 1)(_ + _).forall { case (char, required) =>
+      available.getOrElse(char, 0) >= required
+    }
