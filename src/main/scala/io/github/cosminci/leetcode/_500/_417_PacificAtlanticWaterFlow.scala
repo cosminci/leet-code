ff0407407cad53a656982cea42ae32b6ab4f7e82
Cosmin Ciobanu <ciobanu@adobe.com>
2022-08-31 08:39:57 +0300

    [417] Pacific Atlantic Water Flow ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_500/_417_PacificAtlanticWaterFlow.scala
+++ src/main/scala/io/github/cosminci/leetcode/_500/_417_PacificAtlanticWaterFlow.scala
@@ -5,46 +5,24 @@ import io.github.cosminci.utils
 import scala.collection.mutable
 
 object _417_PacificAtlanticWaterFlow:
-  def main(args: Array[String]): Unit =
-    println(
-      pacificAtlantic(
-        Array(
-          Array(1, 2, 2, 3, 5),
-          Array(3, 2, 3, 4, 4),
-          Array(2, 4, 5, 3, 1),
-          Array(6, 7, 1, 4, 5),
-          Array(5, 1, 1, 2, 4)
-        )
-      )
-    )
 
   def pacificAtlantic(heights: Array[Array[Int]]): List[List[Int]] =
-    val reachable = Array.ofDim[Int](heights.length, heights.head.length)
+    val (m, n) = (heights.indices.last, heights.head.indices.last)
 
-    val toVisitPacific = mutable.Queue.from {
-      heights.indices.map(r => (r, 0)) ++
-        heights.head.indices.map(c => (0, c))
-    }
-    while toVisitPacific.nonEmpty do
-      val (x, y) = toVisitPacific.dequeue()
-      if reachable(x)(y) != 1 then
-        reachable(x)(y) = 1
-        utils.neighbours(x, y, heights).foreach { case (nx, ny) =>
-          if heights(nx)(ny) >= heights(x)(y) then toVisitPacific.enqueue((nx, ny))
-        }
+    @annotation.tailrec
+    def reachable(toVisit: Set[(Int, Int)], visited: Set[(Int, Int)]): Set[(Int, Int)] =
+      toVisit.headOption match
+        case None => visited
+        case Some(coord @ (x, y)) =>
+          val next = utils
+            .neighbours(x, y, heights)
+            .filter { case ncoord @ (nx, ny) => !visited.contains(ncoord) && heights(x)(y) <= heights(nx)(ny) }
+          reachable(toVisit - coord ++ next, visited ++ next)
 
-    val results = mutable.ListBuffer.empty[List[Int]]
-    val toVisitAtlantic = mutable.Queue.from {
-      heights.indices.map(r => (r, heights(r).length - 1)) ++
-        heights.head.indices.map(c => (heights.length - 1, c))
-    }
-    while toVisitAtlantic.nonEmpty do
-      val (x, y) = toVisitAtlantic.dequeue()
-      if reachable(x)(y) != 2 && reachable(x)(y) != 3 then
-        reachable(x)(y) = if reachable(x)(y) == 1 then 3 else 2
-        if reachable(x)(y) == 3 then results.addOne(List(x, y))
-        utils.neighbours(x, y, heights).foreach { case (nx, ny) =>
-          if heights(nx)(ny) >= heights(x)(y) then toVisitAtlantic.enqueue((nx, ny))
-        }
+    val pacificSeed  = ((0 to m).map(x => (x, 0)) ++ (0 to n).map(y => (0, y))).toSet
+    val atlanticSeed = ((0 to m).map(x => (x, n)) ++ (0 to n).map(y => (m, y))).toSet
 
-    results.toList
+    val pacificReachable  = reachable(toVisit = pacificSeed, visited = pacificSeed)
+    val atlanticReachable = reachable(toVisit = atlanticSeed, visited = atlanticSeed)
+
+    pacificReachable.intersect(atlanticReachable).map { case (x, y) => List(x, y) }.toList
