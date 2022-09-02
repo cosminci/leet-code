ce70e00a0e9dbc0b405f6457a428fda02f292b48
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-02 08:31:39 +0300

    [637] Average of Levels in Binary Tree ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_700/_637_AverageOfLevelsInBinaryTree.scala
+++ src/main/scala/io/github/cosminci/leetcode/_700/_637_AverageOfLevelsInBinaryTree.scala
@@ -5,16 +5,15 @@ import io.github.cosminci.utils.TreeNode
 import scala.collection.mutable
 
 object _637_AverageOfLevelsInBinaryTree:
+
   def averageOfLevels(root: TreeNode): Array[Double] =
-    val toVisit = mutable.Queue(root)
-    var results = mutable.ListBuffer.empty[Double]
+    @annotation.tailrec
+    def dfs(level: Seq[TreeNode], res: Array[Double]): Array[Double] =
+      if level.isEmpty then res
+      else
+        dfs(
+          level.flatMap(node => Seq(Option(node.left), Option(node.right)).flatten),
+          res :+ level.map(_.value.toDouble).sum / level.length
+        )
 
-    while toVisit.nonEmpty do
-      val levelNodes = toVisit.dequeueAll(_ => true)
-      levelNodes.foreach { n =>
-        if n.left != null then toVisit.enqueue(n.left)
-        if n.right != null then toVisit.enqueue(n.right)
-      }
-      results.append(levelNodes.map(_.value.toDouble).sum / levelNodes.size)
-
-    results.toArray
+    dfs(level = Seq(root), res = Array.empty)
