095ad320d6573c0ab61fe131dc7f2c8ec63b4f4f
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-06 08:14:08 +0300

    [2399] Check Distances Between Same Letters

---
+++ src/main/scala/io/github/cosminci/leetcode/_2400/_2399_CheckDistancesBetweenSameLetters.scala
@@ -0,0 +1,10 @@
+package io.github.cosminci.leetcode._2400
+
+object _2399_CheckDistancesBetweenSameLetters {
+
+  def checkDistances(s: String, distance: Array[Int]): Boolean =
+    ('a' to 'z').forall { c =>
+      val (i, j) = (s.indexOf(c), s.lastIndexOf(c))
+      (i == -1) || distance(c - 'a') == j - i - 1
+    }
+}
