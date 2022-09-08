04360c41ff0de4cf6652ba23e4c0a20d43877461
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-08 10:26:37 +0300

    [94] Binary Tree Inorder Traversal ADDENDUM * remove mutable solution * make recursive solution easier to read

--- src/main/scala/io/github/cosminci/leetcode/_100/_94_BinaryTreeInOrderTraversal.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_94_BinaryTreeInOrderTraversal.scala
@@ -5,23 +5,9 @@ import io.github.cosminci.utils.TreeNode
 import scala.collection.mutable
 
 object _94_BinaryTreeInOrderTraversal:
+
   def inorderTraversalRecursive(root: TreeNode): List[Int] =
-    if root == null then List.empty
-    else inorderTraversalRecursive(root.left).appended(root.value).appendedAll(inorderTraversalRecursive(root.right))
-
-  def inorderTraversalDFS(root: TreeNode): List[Int] = {
-    val toVisit = mutable.Stack.empty[TreeNode]
-    val result = mutable.ListBuffer.empty[Int]
-
-    var currentParent = root
-    while (currentParent != null || toVisit.nonEmpty) {
-      while (currentParent != null) {
-        toVisit.push(currentParent)
-        currentParent = currentParent.left
-      }
-      currentParent = toVisit.pop()
-      result.addOne(currentParent.value)
-      currentParent = currentParent.right
-    }
-    result.toList
-  }
+    def dfs(node: TreeNode): List[Int] =
+      if node == null then List.empty
+      else (dfs(node.left) :+ node.value) ++ dfs(node.right)
+    dfs(root)
