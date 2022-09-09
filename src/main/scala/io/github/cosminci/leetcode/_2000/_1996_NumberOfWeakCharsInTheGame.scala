7379d414ee9055ac6167f82511ce61f832aed961
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-09 08:34:17 +0300

    [1996] The Number of Weak Characters in the Game ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_2000/_1996_NumberOfWeakCharsInTheGame.scala
+++ src/main/scala/io/github/cosminci/leetcode/_2000/_1996_NumberOfWeakCharsInTheGame.scala
@@ -1,27 +1,15 @@
 package io.github.cosminci.leetcode._2000
 
-import scala.collection.mutable
+object _1996_NumberOfWeakCharsInTheGame:
 
-object _1996_NumberOfWeakCharsInTheGame {
-  def main(args: Array[String]): Unit = {
-    println(numberOfWeakCharacters(Array(Array(5, 5), Array(6, 3), Array(3, 6))))
-    println(numberOfWeakCharacters(Array(Array(2, 2), Array(2, 3))))
-    println(numberOfWeakCharacters(Array(Array(1, 5), Array(10, 4), Array(4, 3))))
-  }
-
-  def numberOfWeakCharacters(properties: Array[Array[Int]]): Int = {
-    given Ordering[Array[Int]] = (x, y) => if (x.head == y.head) y.last.compare(x.last) else x.head.compare(y.head)
-    properties.sortInPlace()
-
-    val higherDefense = mutable.Stack.empty[Int]
-    var count = 0
-    properties.foreach { case Array(attack, defense) =>
-      while (higherDefense.headOption.exists(_ < defense)) {
-        higherDefense.pop()
-        count += 1
+  def numberOfWeakCharacters(properties: Array[Array[Int]]): Int =
+    properties.length - properties
+      .sortBy { case Array(attack, defense) => (attack, -defense) }
+      .map { case Array(_, defense) => defense }
+      .foldLeft(Seq.empty[Int]) { (monoStack, defense) =>
+        defense +: Iterator
+          .iterate(monoStack)(_.tail)
+          .dropWhile(_.headOption.exists(_ < defense))
+          .next()
       }
-      higherDefense.push(defense)
-    }
-    count
-  }
-}
+      .length
