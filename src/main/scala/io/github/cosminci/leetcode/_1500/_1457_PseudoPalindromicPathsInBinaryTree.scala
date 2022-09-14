731e2159d574a6ad4b78d3442c3f08dd73b31118
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-14 08:44:02 +0300

    [1457] Pseudo-Palindromic Paths in a Binary Tree

---
+++ src/main/scala/io/github/cosminci/leetcode/_1500/_1457_PseudoPalindromicPathsInBinaryTree.scala
@@ -0,0 +1,16 @@
+package io.github.cosminci.leetcode._1500
+
+import io.github.cosminci.utils.TreeNode
+
+object _1457_PseudoPalindromicPathsInBinaryTree:
+
+  def pseudoPalindromicPaths(root: TreeNode): Int =
+    def dfs(node: TreeNode, bitmask: Int): Int =
+      val newBitmask = 1 << node.value ^ bitmask
+
+      if node.left == null && node.right == null then
+        if (1 to 9).count(shift => (newBitmask >> shift & 1) > 0) <= 1 then 1 else 0
+      else
+        Seq(Option(node.left), Option(node.right)).flatten.map(dfs(_, newBitmask)).sum
+
+    dfs(root, bitmask = 0)
