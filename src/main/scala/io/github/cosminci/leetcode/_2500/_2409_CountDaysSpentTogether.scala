477f275e95e149d49e14f9957f5a058474dd17b7
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-17 19:08:40 +0300

    [2409] Count Days Spent Together

---
+++ src/main/scala/io/github/cosminci/leetcode/_2500/_2409_CountDaysSpentTogether.scala
@@ -0,0 +1,16 @@
+package io.github.cosminci.leetcode._2500
+
+object _2409_CountDaysSpentTogether:
+
+  def countDaysTogether(arriveAlice: String, leaveAlice: String, arriveBob: String, leaveBob: String): Int =
+    val days = Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+    def day(s: String) =
+      val Seq(month, day) = s.split('-').toSeq
+      (0 until month.toInt - 1).map(days).sum + day.toInt
+
+    val (aliceA, aliceL) = (day(arriveAlice), day(leaveAlice))
+    val (bobA, bobL)     = (day(arriveBob), day(leaveBob))
+    val (maxA, minL)     = (aliceA.max(bobA), aliceL.min(bobL))
+
+    if maxA > minL then 0 else minL - maxA + 1
