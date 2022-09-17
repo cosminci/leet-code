a69c8f1f38f2affcfb45f67a522dca0da4406e9b
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-17 12:35:57 +0300

    [336] Palindrome Pairs ADDENDUM * reimplement more concisely

--- src/main/scala/io/github/cosminci/leetcode/_400/_336_PalindromePairs.scala
+++ src/main/scala/io/github/cosminci/leetcode/_400/_336_PalindromePairs.scala
@@ -1,39 +1,21 @@
 package io.github.cosminci.leetcode._400
 
 object _336_PalindromePairs:
-  def main(args: Array[String]): Unit =
-    println(palindromePairs(Array("a", "")))
-    println(palindromePairs(Array("abcd", "dcba", "lls", "s", "sssll")))
 
-  def palindromePairs(words: Array[String]): List[List[Int]] =
-    val wordIndices = words.zipWithIndex.toMap
-
-    val reversedPairs = wordIndices.collect {
-      case (w, idx) if wordIndices.get(w.reverse).exists(_ != idx) =>
-        List(idx, wordIndices(w.reverse))
-    }.toList
-
-    wordIndices.foldLeft(reversedPairs) { case (pairs, (w, idx)) =>
-      val newPairs =
-        if w == "" then
-          wordIndices
-            .filter { case (w, idx) => w.nonEmpty && w == w.reverse }
-            .flatMap { case (_, idx2) =>
-              List(List(idx2, idx), List(idx, idx2))
-            }
-        else
-          (1 until w.length).toList.flatMap { i =>
-            val (prefix, suffix)   = w.splitAt(i)
-            val (prefixR, suffixR) = (prefix.reverse, suffix.reverse)
-            val prefixPaliAndReversedSuffixMatch = Option.when(
-              prefix == prefixR &&
-                wordIndices.get(suffixR).exists(_ != idx)
-            )(List(wordIndices(suffixR), idx))
-            val suffixPaliAndReversedPrefixMatch = Option.when(
-              suffix == suffixR &&
-                wordIndices.get(prefixR).exists(_ != idx)
-            )(List(idx, wordIndices(prefixR)))
-            List.empty ++ prefixPaliAndReversedSuffixMatch ++ suffixPaliAndReversedPrefixMatch
-          }
-      pairs ++ newPairs
+  def palindromePairs(input: Array[String]): List[List[Int]] =
+    val words = input.zipWithIndex.toMap
+    words.toList.flatMap { case (w, i) =>
+      (w.indices :+ w.length).flatMap { j =>
+        val (prefix, suffix)   = w.splitAt(j)
+        val (prefixR, suffixR) = (prefix.reverse, suffix.reverse)
+        val maybePair1 = Option
+          .when(prefix == prefixR && suffixR != w && words.contains(suffixR))(
+            List(words(suffix.reverse), i)
+          )
+        val maybePair2 = Option
+          .when(j != w.length && suffix == suffixR && prefixR != w && words.contains(prefixR))(
+            List(i, words(prefix.reverse))
+          )
+        List(maybePair1, maybePair2).flatten
+      }
     }
