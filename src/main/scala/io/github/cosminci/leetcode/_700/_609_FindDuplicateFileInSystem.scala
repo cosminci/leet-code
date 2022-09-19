9c31463e63d37a1dea942960e18ad8d6181ca7ce
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-19 09:54:05 +0300

    [609] Find Duplicate File in System

---
+++ src/main/scala/io/github/cosminci/leetcode/_700/_609_FindDuplicateFileInSystem.scala
@@ -0,0 +1,17 @@
+package io.github.cosminci.leetcode._700
+
+object _609_FindDuplicateFileInSystem:
+
+  def findDuplicate(paths: Array[String]): List[List[String]] =
+    val regex = "(.+)\\((.+)\\)".r
+    def parse(line: String) =
+      line.split(' ').toSeq match
+        case dir +: files =>
+          files.map { case regex(file, content) => content -> s"$dir/$file" }
+
+    paths
+      .flatMap(parse)
+      .groupMap { case (content, _) => content } { case (_, path) => path }
+      .values
+      .collect { case group if group.length > 1 => group.toList }
+      .toList
