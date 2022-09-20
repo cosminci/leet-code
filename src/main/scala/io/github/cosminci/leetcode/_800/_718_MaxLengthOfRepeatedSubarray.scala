b4e5247da978bc62e76e6f382b4071cbcd64a334
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-20 08:32:42 +0300

    [718] Maximum Length of Repeated Subarray ADDENDUM * simplify solution

--- src/main/scala/io/github/cosminci/leetcode/_800/_718_MaxLengthOfRepeatedSubarray.scala
+++ src/main/scala/io/github/cosminci/leetcode/_800/_718_MaxLengthOfRepeatedSubarray.scala
@@ -1,20 +1,13 @@
 package io.github.cosminci.leetcode._800
 
 object _718_MaxLengthOfRepeatedSubarray:
-  def main(args: Array[String]): Unit =
-    println(findLength(Array(1, 2, 3, 2, 1), Array(3, 2, 1, 4, 7)))
 
   def findLength(nums1: Array[Int], nums2: Array[Int]): Int =
-    val dp  = Array.ofDim[Int](nums1.length + 1, nums2.length + 1)
-    var max = 0
-    nums1.indices.foreach { n1Idx =>
-      val n1DpIdx = n1Idx + 1
-      val n1      = nums1(n1Idx)
-      nums2.indices.foreach { n2Idx =>
-        val n2DpIdx = n2Idx + 1
-        val n2      = nums2(n2Idx)
-        dp(n1DpIdx)(n2DpIdx) = if n1 != n2 then 0 else 1 + dp(n1DpIdx - 1)(n2DpIdx - 1)
-        if dp(n1DpIdx)(n2DpIdx) > max then max = dp(n1DpIdx)(n2DpIdx)
-      }
-    }
-    max
+    val dp = Array.ofDim[Int](nums1.length + 1, nums2.length + 1)
+
+    for
+      i <- nums1.indices
+      j <- nums2.indices
+    do dp(i + 1)(j + 1) = if nums1(i) == nums2(j) then 1 + dp(i)(j) else 0
+
+    dp.map(_.max).max
