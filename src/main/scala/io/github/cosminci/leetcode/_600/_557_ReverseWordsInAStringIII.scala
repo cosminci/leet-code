c460bdb0795e440cf074010607bc773ae6deed40
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-22 17:03:48 +0300

    [557] Reverse Words in a String III ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_600/_557_ReverseWordsInAStringIII.scala
+++ src/main/scala/io/github/cosminci/leetcode/_600/_557_ReverseWordsInAStringIII.scala
@@ -1,13 +1,6 @@
 package io.github.cosminci.leetcode._600
 
 object _557_ReverseWordsInAStringIII:
+
   def reverseWords(s: String): String =
-    val result   = new StringBuilder
-    val currWord = new StringBuilder
-    s.foreach { char =>
-      if char == ' ' then
-        result.append(currWord.reverse.toString()).append(' ')
-        currWord.clear()
-      else currWord.append(char)
-    }
-    result.append(currWord.reverse.toString()).toString()
+    s.split(' ').map(_.reverse).mkString(" ")
