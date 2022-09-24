7ab326a633f1c9bf8e8b816f16b908cf59b9bad5
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-24 08:05:06 +0300

    [113] Path Sum II ADDENDUM * minor cosmetic improvements

--- src/main/scala/io/github/cosminci/leetcode/_200/_113_PathSumII.scala
+++ src/main/scala/io/github/cosminci/leetcode/_200/_113_PathSumII.scala
@@ -3,20 +3,15 @@ package io.github.cosminci.leetcode._200
 import io.github.cosminci.utils.TreeNode
 
 object _113_PathSumII:
-  def main(args: Array[String]): Unit =
-    println(pathSum(new TreeNode(5), 5))
 
   def pathSum(root: TreeNode, targetSum: Int): List[List[Int]] =
-    if root == null then return List.empty
-
     def dfs(path: Seq[TreeNode], remaining: Int): Seq[Seq[TreeNode]] =
       val node = path.last
       if node.left == null && node.right == null then
-        if node.value == remaining then return Seq(path)
-        else return Seq.empty
+        Option.when(node.value == remaining)(Seq(path)).getOrElse(Seq.empty)
+      else
+        Seq(Option(node.left), Option(node.right)).flatten
+          .flatMap(n => dfs(path :+ n, remaining - node.value))
 
-      val left  = if node.left != null then dfs(path :+ node.left, remaining - node.value) else Seq.empty
-      val right = if node.right != null then dfs(path :+ node.right, remaining - node.value) else Seq.empty
-      left ++ right
-
-    dfs(Seq(root), targetSum).map(_.map(_.value).toList).toList
+    if root == null then List.empty
+    else dfs(Seq(root), targetSum).map(_.map(_.value).toList).toList
