563b5ea497ff593c45e3635a1542bb067b7e3a83
Cosmin Ciobanu <ciobanu@adobe.com>
2022-09-28 09:26:08 +0300

    [19] Remove Nth Node From End of List ADDENDUM * simplify solution

--- src/main/scala/io/github/cosminci/leetcode/_100/_19_RemoveNthNodeFromEndOfList.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_19_RemoveNthNodeFromEndOfList.scala
@@ -1,19 +1,16 @@
 package io.github.cosminci.leetcode._100
 
-import io.github.cosminci.utils._
+import io.github.cosminci.utils.*
 
 object _19_RemoveNthNodeFromEndOfList:
-  def main(args: Array[String]): Unit =
-    println(linkedListToSeq(removeNthFromEnd(seqToLinkedList(Seq(1, 5, 2)), 2)))
 
   def removeNthFromEnd(head: ListNode, n: Int): ListNode =
-    var dummy           = new ListNode(666, head)
-    var (behind, ahead) = (dummy, dummy)
+    def dfs(curr: ListNode, n: Int): (ListNode, Int) =
+      if curr == null then (curr, 0)
+      else
+        val (next, count) = dfs(curr.next, n)
+        curr.next = next
+        if count + 1 == n then (curr.next, count + 1)
+        else (curr, count + 1)
 
-    (0 to n).foreach { _ => ahead = ahead.next }
-    while ahead != null do
-      ahead = ahead.next
-      behind = behind.next
-
-    behind.next = behind.next.next
-    dummy.next
+    dfs(head, n)._1
