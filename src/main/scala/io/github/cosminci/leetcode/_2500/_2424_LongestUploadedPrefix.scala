fcd269ebd6ba94bce2a282db280d7479fc23797a
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-01 23:35:40 +0300

    [2424] Longest Uploaded Prefix

---
+++ src/main/scala/io/github/cosminci/leetcode/_2500/_2424_LongestUploadedPrefix.scala
@@ -0,0 +1,15 @@
+package io.github.cosminci.leetcode._2500
+
+import scala.collection.mutable
+
+object _2424_LongestUploadedPrefix:
+
+  class LUPrefix(n: Int):
+    private val pending   = mutable.Set.empty[Int]
+    private var maxPrefix = 0
+
+    def upload(video: Int) =
+      pending.add(video)
+      while pending.contains(maxPrefix + 1) do maxPrefix += 1
+
+    def longest(): Int = maxPrefix
