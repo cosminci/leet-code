542197d41912a6b0be24757854d329816fef73db
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-03 10:29:36 +0300

    [1578] Minimum Time to Make Rope Colorful

---
+++ src/main/scala/io/github/cosminci/leetcode/_1600/_1578_MinTimeToMakeRopeColorful.scala
@@ -0,0 +1,15 @@
+package io.github.cosminci.leetcode._1600
+
+import scala.util.chaining.*
+
+object _1578_MinTimeToMakeRopeColorful:
+
+  def minCost(colors: String, neededTime: Array[Int]): Int =
+    (colors :+ 'X', neededTime :+ 0).pipe { case (colors, neededTime) =>
+      colors.indices.tail
+        .foldLeft(0, neededTime.head, neededTime.head) { case ((cost, sum, max), i) =>
+          if colors(i) == colors(i - 1) then (cost, sum + neededTime(i), max.max(neededTime(i)))
+          else (cost + sum - max, neededTime(i), neededTime(i))
+        }
+        ._1
+    }
