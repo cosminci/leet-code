a64eb3c5e83ec991dc7c130a06a1a207d5300d51
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-05 08:47:07 +0300

    [623] Add One Row to Tree ADDENDUM * cosmetic improvements

--- src/main/scala/io/github/cosminci/leetcode/_700/_623_AddOneRowToTree.scala
+++ src/main/scala/io/github/cosminci/leetcode/_700/_623_AddOneRowToTree.scala
@@ -2,19 +2,19 @@ package io.github.cosminci.leetcode._700
 
 import io.github.cosminci.utils.TreeNode
 
-import scala.collection.mutable
+import scala.util.chaining.*
 
-object _623_AddOneRowToTree {
+object _623_AddOneRowToTree:
+
   def addOneRow(root: TreeNode, value: Int, depth: Int): TreeNode =
-    if (depth == 1) new TreeNode(value, _left = root)
-    else {
-      (2 until depth).foldLeft(Seq(root)) {
-        case (nodes, _) =>
-          nodes.flatMap(node => Seq.empty ++ Option(node.left) ++ Option(node.right))
-      }.foreach { node =>
-        node.left = new TreeNode(value, _left = node.left)
-        node.right = new TreeNode(value, _right = node.right)
-      }
-      root
-    }
-}
+    if depth == 1 then new TreeNode(value, _left = root)
+    else
+      (2 until depth)
+        .foldLeft(Seq(root)) { case (nodes, _) =>
+          nodes.flatMap(node => Seq(Option(node.left), Option(node.right)).flatten)
+        }
+        .foreach { node =>
+          node.left = new TreeNode(value, _left = node.left)
+          node.right = new TreeNode(value, _right = node.right)
+        }
+        .pipe { _ => root }
