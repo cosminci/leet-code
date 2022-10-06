7285492c7eebdc033f2a47c220472266d646f73e
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-06 08:33:08 +0300

    [981] Time Based Key-Value Store  ADDENDUM * cosmetic improvements

--- src/main/scala/io/github/cosminci/leetcode/_1000/_981_TimeBasedKVStore.scala
+++ src/main/scala/io/github/cosminci/leetcode/_1000/_981_TimeBasedKVStore.scala
@@ -2,15 +2,22 @@ package io.github.cosminci.leetcode._1000
 
 import scala.collection.mutable
 
-object _981_TimeBasedKVStore {
+object _981_TimeBasedKVStore:
 
-  class TimeMap {
+  class TimeMap:
     private val store = mutable.Map.empty[String, mutable.TreeMap[Int, String]]
 
-    def set(key: String, value: String, ts: Int) =
-      store.getOrElseUpdate(key, mutable.TreeMap.empty).update(ts, value)
+    def set(key: String, value: String, ts: Int): Unit =
+      store
+        .getOrElseUpdate(key, mutable.TreeMap.empty)
+        .update(ts, value)
 
     def get(key: String, ts: Int): String =
-      store.get(key).flatMap(values => values.get(ts).orElse(values.maxBefore(ts).map(_._2))).getOrElse("")
-  }
-}
+      store
+        .get(key)
+        .flatMap { values =>
+          values
+            .get(ts)
+            .orElse(values.maxBefore(ts).map { case (_, v) => v })
+        }
+        .getOrElse("")
