43b1385dd29840aa5ed6237a1cd0374ddcd1249f
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-08 12:28:32 +0300

    [16] 3Sum Closest ADDENDUM * reimplement w/o mutability

--- src/main/scala/io/github/cosminci/leetcode/_100/_16_ThreeSumClosest.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_16_ThreeSumClosest.scala
@@ -2,19 +2,16 @@ package io.github.cosminci.leetcode._100
 
 object _16_ThreeSumClosest:
 
-  def main(args: Array[String]): Unit =
-    println(threeSumClosest(Array(1, 1, -1, 3, -5, 3, -2, 4, 7, 5, -1), -14))
+  def threeSumClosest(nums: Array[Int], target: Int): Int =
+    nums.sortInPlace()
+    (0 until nums.length - 2).foldLeft(if target < 0 then Int.MinValue else Int.MaxValue) { (closest, i) =>
+      @annotation.tailrec
+      def dfs(j: Int, k: Int, closest: Int): Int =
+        if j >= k then closest
+        else
+          val sum        = nums(i) + nums(j) + nums(k)
+          val newClosest = if (target - sum).abs < (target - closest).abs then sum else closest
+          if sum > target then dfs(j, k - 1, newClosest) else dfs(j + 1, k, newClosest)
 
-  def threeSumClosest(n: Array[Int], target: Int): Int =
-    var closest = if target < 0 then Int.MinValue else Int.MaxValue
-    val nums    = n.sorted
-    (0 until nums.length - 2).foreach { i =>
-      var j = i + 1
-      var k = nums.length - 1
-      while j < k do
-        val sum = nums(i) + nums(j) + nums(k)
-        if math.abs(target - sum) < math.abs(target - closest) then closest = sum
-        if sum > target then k -= 1
-        else j += 1
+      dfs(j = i + 1, k = nums.length - 1, closest)
     }
-    closest
