e233b173365989fa886e3ac6c2665092dc3479cd
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-09 10:35:02 +0300

    [2432] The Employee That Worked on the Longest Task

---
+++ src/main/scala/io/github/cosminci/leetcode/_2500/_2432_EmployeeThatWorkedOnLongestTask.scala
@@ -0,0 +1,13 @@
+package io.github.cosminci.leetcode._2500
+
+object _2432_EmployeeThatWorkedOnLongestTask:
+
+  def hardestWorker(n: Int, logs: Array[Array[Int]]): Int =
+    logs.tail
+      .foldLeft(logs.head.head, logs.head.last, logs.head.last) {
+        case ((result, maxDuration, prevEnd), Array(employee, newEnd)) =>
+          if newEnd - prevEnd > maxDuration then (employee, newEnd - prevEnd, newEnd)
+          else if newEnd - prevEnd < maxDuration then (result, maxDuration, newEnd)
+          else (result.min(employee), maxDuration, newEnd)
+      }
+      ._1
