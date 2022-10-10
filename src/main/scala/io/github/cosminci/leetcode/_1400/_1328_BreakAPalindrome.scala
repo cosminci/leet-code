98b6dfb54b302bbf5fff7c081b5a36b91e6c88dd
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-10 08:29:32 +0300

    [1328] Break a Palindrome ADDENDUM * cosmetic improvements

--- src/main/scala/io/github/cosminci/leetcode/_1400/_1328_BreakAPalindrome.scala
+++ src/main/scala/io/github/cosminci/leetcode/_1400/_1328_BreakAPalindrome.scala
@@ -1,20 +1,10 @@
 package io.github.cosminci.leetcode._1400
 
-object _1328_BreakAPalindrome {
-  def main(args: Array[String]): Unit = {
-    println(breakPalindrome("abccba"))
-    println(breakPalindrome("a"))
-    println(breakPalindrome("aa"))
-    println(breakPalindrome("aba"))
-  }
+object _1328_BreakAPalindrome:
 
-  def breakPalindrome(palindrome: String): String = {
-    val n = palindrome.length
-    if (n == 1) return ""
-
-    (0 until n / 2).find(idx => palindrome(idx) != 'a') match {
-      case Some(idx) => palindrome.updated(idx, 'a')
-      case None => palindrome.updated(n - 1, 'b')
-    }
-  }
-}
+  def breakPalindrome(palindrome: String): String =
+    if palindrome.length == 1 then ""
+    else
+      (0 until palindrome.length / 2).find(palindrome(_) != 'a') match
+        case Some(idx) => palindrome.updated(idx, 'a')
+        case None      => palindrome.updated(palindrome.length - 1, 'b')
