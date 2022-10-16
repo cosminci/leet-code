7359651a590ff4916153e7d9a7c5fa2da1f1801a
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-16 15:00:10 +0300

    2441. Largest Positive Integer That Exists With Its Negative

---
+++ src/main/scala/io/github/cosminci/leetcode/_2500/_2441_LargestPosIntThatExistsWithItsNegative.scala
@@ -0,0 +1,9 @@
+package io.github.cosminci.leetcode._2500
+
+object _2441_LargestPosIntThatExistsWithItsNegative:
+
+  def findMaxK(nums: Array[Int]): Int =
+    val distinct = nums.toSet
+    nums.foldLeft(-1) { (res, n) =>
+      if nums.contains(-n) then res.max(n) else res
+    }
