1ce76e9a97a5f7465bb92068395d161f9e23e384
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-16 15:41:16 +0300

    2444. Count Subarrays With Fixed Bounds

---
+++ src/main/scala/io/github/cosminci/leetcode/_2500/_2444_CountSubarraysWithFixedBounds.scala
@@ -0,0 +1,18 @@
+package io.github.cosminci.leetcode._2500
+
+object _2444_CountSubarraysWithFixedBounds:
+
+  def countSubarrays(nums: Array[Int], minK: Int, maxK: Int): Long =
+    nums.zipWithIndex
+      .foldLeft(0L, 0, -1, -1) { case ((count, left, lastMinK, lastMaxK), (num, right)) =>
+        if num < minK || num > maxK then
+          (count, right + 1, lastMinK, lastMaxK)
+        else
+          val newLastMinK = if num == minK then right else lastMinK
+          val newLastMaxK = if num == maxK then right else lastMaxK
+          val newCount =
+            if newLastMinK < left || newLastMaxK < left then 0
+            else newLastMinK.min(newLastMaxK) - left + 1
+
+          (count + newCount, left, newLastMinK, newLastMaxK)
+      }._1
