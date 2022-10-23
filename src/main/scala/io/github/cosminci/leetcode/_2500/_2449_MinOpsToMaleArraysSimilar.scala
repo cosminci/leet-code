0c3b99dfdd82f7d66f667631c15ede58ba1f7842
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-24 01:38:59 +0300

    2449. Minimum Number of Operations to Make Arrays Similar

---
+++ src/main/scala/io/github/cosminci/leetcode/_2500/_2449_MinOpsToMaleArraysSimilar.scala
@@ -0,0 +1,9 @@
+package io.github.cosminci.leetcode._2500
+
+object _2449_MinOpsToMaleArraysSimilar:
+
+  def makeSimilar(nums: Array[Int], target: Array[Int]): Long =
+    nums.sortInPlaceBy(n => (n % 2, n))
+    target.sortInPlaceBy(n => (n % 2, n))
+
+    nums.zip(target).map { case (n, t) => (n - t).abs.toLong }.sum / 4
