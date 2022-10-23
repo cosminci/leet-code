a58fa80b748d9ae9f930b901bf7fce3cf0a21a53
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-24 00:36:40 +0300

    645. Set Mismatch

---
+++ src/main/scala/io/github/cosminci/leetcode/_700/_645_SetMismatch.scala
@@ -0,0 +1,11 @@
+package io.github.cosminci.leetcode._700
+
+import scala.util.chaining.*
+
+object _645_SetMismatch:
+  def findErrorNums(nums: Array[Int]): Array[Int] =
+    nums
+      .foldLeft(Set.from(1 to nums.length), 0) { case ((left, duplicate), n) =>
+        if left.contains(n) then (left - n, duplicate) else (left, n)
+      }
+      .pipe { case (missing, duplicate) => Array(duplicate) ++ missing }
