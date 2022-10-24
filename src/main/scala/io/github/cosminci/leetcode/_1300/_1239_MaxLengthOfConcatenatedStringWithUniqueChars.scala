730b32a3fbf2dd503f31460344c9a60f0d5af1fa
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-24 10:04:38 +0300

    1239. Maximum Length of a Concatenated String with Unique Characters ADDENDUM * simplify implementation

--- src/main/scala/io/github/cosminci/leetcode/_1300/_1239_MaxLengthOfConcatenatedStringWithUniqueChars.scala
+++ src/main/scala/io/github/cosminci/leetcode/_1300/_1239_MaxLengthOfConcatenatedStringWithUniqueChars.scala
@@ -1,29 +1,20 @@
 package io.github.cosminci.leetcode._1300
 
 object _1239_MaxLengthOfConcatenatedStringWithUniqueChars:
-  def main(args: Array[String]): Unit =
-    println(maxLength(List("un", "iq", "ue")))
-    println(maxLength(List("cha", "r", "act", "ers")))
-    println(maxLength(List("abcdefghijklmnopqrstuvwxyz")))
-    println(maxLength(List("yy", "bkhwmpbiisbldzknpm")))
 
   def maxLength(arr: List[String]): Int =
     val bitsets = arr.collect {
-      case s if (s.distinct == s) =>
-        s.foldLeft(0) { case (bitset, char) =>
-          bitset | 1 << (char - 'a')
-        }
+      case s if s.distinct.length == s.length =>
+        s.foldLeft(0)((bitset, char) => bitset | 1 << (char - 'a'))
     }
 
-    def bitcount(bitset: Int): Int =
-      if bitset == 0 then 0
-      else if (bitset & 1) == 1 then 1 + bitcount(bitset >> 1)
-      else bitcount(bitset >> 1)
+    @annotation.tailrec
+    def bitCount(bitset: Int, cnt: Int): Int =
+      if bitset == 0 then cnt else bitCount(bitset >> 1, cnt + (bitset & 1))
 
-    def dfs(aggregateBitset: Int, idx: Int): Int =
-      if idx == bitsets.length then bitcount(aggregateBitset)
-      else if (aggregateBitset & bitsets(idx)) == 0 then
-        math.max(dfs(aggregateBitset | bitsets(idx), idx + 1), dfs(aggregateBitset, idx + 1))
-      else dfs(aggregateBitset, idx + 1)
+    def dfs(i: Int, bitset: Int): Int =
+      if i == bitsets.length then bitCount(bitset, cnt = 0)
+      else if (bitset & bitsets(i)) > 0 then dfs(i + 1, bitset)
+      else dfs(i + 1, bitset | bitsets(i)).max(dfs(i + 1, bitset))
 
-    dfs(0, 0)
+    dfs(i = 0, bitset = 0)
