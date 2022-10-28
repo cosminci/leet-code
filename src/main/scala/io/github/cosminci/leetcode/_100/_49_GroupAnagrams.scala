ddd2309209ac8243eada3ba5f75be10434b06f9e
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-28 08:11:26 +0300

    49. Group Anagrams ADDENDUM * rewrite grouping via groupMapReduce

--- src/main/scala/io/github/cosminci/leetcode/_100/_49_GroupAnagrams.scala
+++ src/main/scala/io/github/cosminci/leetcode/_100/_49_GroupAnagrams.scala
@@ -1,10 +1,10 @@
 package io.github.cosminci.leetcode._100
 
-import io.github.cosminci.utils
-
 object _49_GroupAnagrams:
-  def main(args: Array[String]): Unit =
-    println(groupAnagrams(Array("eat", "tea", "tan", "ate", "nat", "bat")))
 
   def groupAnagrams(strs: Array[String]): List[List[String]] =
-    strs.groupBy(s => utils.characterCounts(s)).values.map(_.toList).toList
+    strs
+      .groupBy(_.groupMapReduce(identity)(_ => 1)(_ + _))
+      .values
+      .map(_.toList)
+      .toList
