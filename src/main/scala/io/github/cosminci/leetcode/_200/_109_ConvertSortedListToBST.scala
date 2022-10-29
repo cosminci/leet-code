5875fe91e75917aec601a0454960b05864f314d6
Cosmin Ciobanu <ciobanu@adobe.com>
2022-10-29 14:38:21 +0300

    109. Convert Sorted List to Binary Search Tree

---
+++ src/main/scala/io/github/cosminci/leetcode/_200/_109_ConvertSortedListToBST.scala
@@ -0,0 +1,17 @@
+package io.github.cosminci.leetcode._200
+
+import io.github.cosminci.utils.*
+
+object _109_ConvertSortedListToBST:
+
+  def sortedListToBST(head: ListNode): TreeNode =
+    val nums = linkedListToSeq(head)
+
+    def dfs(i: Int, j: Int): TreeNode =
+      if i > j then null
+      else if i == j then new TreeNode(nums(i))
+      else
+        val mid = (i + j) / 2
+        new TreeNode(nums(mid), dfs(i, mid - 1), dfs(mid + 1, j))
+
+    dfs(i = 0, j = nums.length - 1)
