cd9cfc939b62710bda6bef99c629816eaf081688
Cosmin Ciobanu <ciobanu@adobe.com>
2022-11-11 10:14:17 +0200

    26. Remove Duplicates from Sorted Array ADDENDUM * tailrec rewrite

--- src/main/scala/com/leetcode/cosminci/_100/_26_RemoveDuplicatesFromSortedArray.scala
+++ src/main/scala/com/leetcode/cosminci/_100/_26_RemoveDuplicatesFromSortedArray.scala
@@ -2,17 +2,13 @@ package com.leetcode.cosminci._100
 
 object _26_RemoveDuplicatesFromSortedArray:
 
-  def main(args: Array[String]): Unit =
-    println(removeDuplicates(Array(0, 0, 1, 1, 1, 2, 2, 3, 3, 4)))
-
   def removeDuplicates(nums: Array[Int]): Int =
-    if nums.isEmpty then return 0
+    @annotation.tailrec
+    def dfs(i: Int, j: Int): Int =
+      if j == nums.length then i + 1
+      else if nums(i) == nums(j) then dfs(i, j + 1)
+      else
+        nums(i + 1) = nums(j)
+        dfs(i + 1, j + 1)
 
-    var i = 0
-    var j = 0
-    while j < nums.length do
-      if nums(i) != nums(j) then
-        i += 1
-        nums(i) = nums(j)
-      j += 1
-    i + 1
+    if nums.isEmpty then 0 else dfs(i = 0, j = 0)
