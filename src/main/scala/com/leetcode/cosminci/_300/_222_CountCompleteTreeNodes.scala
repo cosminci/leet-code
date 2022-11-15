9113164a50210ed72222fdae37ce3a7fb1e2bea0
Cosmin Ciobanu <ciobanu@adobe.com>
2022-11-15 08:39:55 +0200

    222. Count Complete Tree Nodes ADDENDUM * reimplement w/o mutability

--- src/main/scala/com/leetcode/cosminci/_300/_222_CountCompleteTreeNodes.scala
+++ src/main/scala/com/leetcode/cosminci/_300/_222_CountCompleteTreeNodes.scala
@@ -2,18 +2,18 @@ package com.leetcode.cosminci._300
 
 import com.leetcode.cosminci.utils.TreeNode
 
+import scala.util.chaining.*
+
 object _222_CountCompleteTreeNodes:
 
-  def countNodes(root: TreeNode): Int =
-    if root == null then return 0
-
-    var (left, right) = (root, root)
-    var height        = 0
-
-    while right != null do
-      left = left.left
-      right = right.right
-      height += 1
-
-    if left == null then (1 << height) - 1
-    else 1 + countNodes(root.left) + countNodes(root.right)
+  def countNodes(node: TreeNode): Int =
+    if node == null then 0
+    else
+      Iterator
+        .iterate((node, node, 0)) { case (l, r, h) => (l.left, r.right, h + 1) }
+        .dropWhile { case (_, r, _) => r != null }
+        .next()
+        .pipe { case (l, _, h) =>
+          if l == null then (1 << h) - 1
+          else 1 + countNodes(node.left) + countNodes(node.right)
+        }
