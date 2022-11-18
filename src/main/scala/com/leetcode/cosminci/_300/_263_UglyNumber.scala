7131a89cbebbaff2a719a8f59551aa8d768e0f23
Cosmin Ciobanu <ciobanu@adobe.com>
2022-11-18 09:03:06 +0200

    263. Ugly Number ADDENDUM * reimplement w/o mutability

--- src/main/scala/com/leetcode/cosminci/_300/_263_UglyNumber.scala
+++ src/main/scala/com/leetcode/cosminci/_300/_263_UglyNumber.scala
@@ -1,12 +1,11 @@
 package com.leetcode.cosminci._300
 
 object _263_UglyNumber:
-  def main(args: Array[String]): Unit =
-    println(isUgly(6))
 
   def isUgly(n: Int): Boolean =
-    var num = n
-    Seq(2, 3, 5).foreach { divisor =>
-      while num != 0 && num % divisor == 0 do num = num / divisor
-    }
-    num == 1
+    Seq(2, 3, 5).foldLeft(n) { (n, divisor) =>
+      Iterator
+        .iterate(n)(_ / divisor)
+        .dropWhile(n => n != 0 && n % divisor == 0)
+        .next()
+    } == 1
