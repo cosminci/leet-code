1b0556f89c989d9e82c8e62f557d7fe9ced22bd1
Cosmin Ciobanu <ciobanu@adobe.com>
2022-11-26 09:58:56 +0200

    1235. Maximum Profit in Job Scheduling ADDENDUM * reimplement w/o mutability & fluent syntax

--- src/main/scala/com/leetcode/cosminci/_1300/_1235_MaxProfitInJobScheduling.scala
+++ src/main/scala/com/leetcode/cosminci/_1300/_1235_MaxProfitInJobScheduling.scala
@@ -1,40 +1,19 @@
 package com.leetcode.cosminci._1300
 
+import scala.collection.immutable.TreeMap
+import scala.util.chaining.*
 
-object _1235_MaxProfitInJobScheduling {
-  def main(args: Array[String]): Unit = {
-    println(jobScheduling(Array(4, 2, 4, 8, 2), Array(5, 5, 5, 10, 8), Array(1, 2, 8, 10, 4)))
-    println(jobScheduling(Array(1, 2, 3, 4, 6), Array(3, 5, 10, 6, 9), Array(20, 20, 100, 70, 60)))
-    println(jobScheduling(Array(1, 1, 1), Array(2, 3, 4), Array(5, 6, 4)))
-  }
+object _1235_MaxProfitInJobScheduling:
 
-  case class Job(start: Int, end: Int, profit: Int)
-  def jobScheduling(startTime: Array[Int], endTime: Array[Int], profit: Array[Int]): Int = {
-    val jobs = startTime.zip(endTime).zip(profit).map {
-      case ((s, e), p) => Job(s, e, p)
-    }.sortBy(_.end)
-
-    def latestNonConflicting(endIdx: Int, targetStart: Int): Int = {
-      var (l, r) = (0, endIdx)
-      while (l < r) {
-        val mid = l + (r - l) / 2
-        if (jobs(mid).end <= targetStart)
-          l = mid + 1
-        else
-          r = mid
+  def jobScheduling(startTime: Array[Int], endTime: Array[Int], profit: Array[Int]): Int =
+    startTime
+      .zip(endTime)
+      .zip(profit)
+      .map { case ((start, end), profit) => (start, end, profit) }
+      .sortBy { case (_, end, _) => end }
+      .foldLeft(TreeMap(0 -> 0)) { case (dp, (start, end, profit)) =>
+        val newProfit = dp.maxBefore(start + 1).map { case (_, profit) => profit }.getOrElse(0) + profit
+        if newProfit <= dp.last.pipe { case (_, maxProfit) => maxProfit } then dp else dp + (end -> newProfit)
       }
-      l - 1
-    }
-
-    val dp = Array.ofDim[Int](jobs.length)
-    dp(0) = jobs.head.profit
-
-    (1 until jobs.length).foreach { i =>
-      val latestAv = latestNonConflicting(i, jobs(i).start)
-      val profit = jobs(i).profit + (if (latestAv != -1) dp(latestAv) else 0)
-      dp(i) = math.max(profit, dp(i - 1))
-    }
-
-    dp.last
-  }
-}
+      .last
+      .pipe { case (_, maxProfit) => maxProfit }
