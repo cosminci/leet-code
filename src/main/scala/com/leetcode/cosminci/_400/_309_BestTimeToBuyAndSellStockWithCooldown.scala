679f0c74a732a02e9f7b7e81923bc51fce74fe77
Cosmin Ciobanu <ciobanu@adobe.com>
2022-12-23 13:07:56 +0200

    309. Best Time to Buy and Sell Stock with Cooldown ADDENDUM * minor refactoring

--- src/main/scala/com/leetcode/cosminci/_400/_309_BestTimeToBuyAndSellStockWithCooldown.scala
+++ src/main/scala/com/leetcode/cosminci/_400/_309_BestTimeToBuyAndSellStockWithCooldown.scala
@@ -1,14 +1,12 @@
 package com.leetcode.cosminci._400
 
-import scala.collection.mutable
+import scala.util.chaining.*
 
 object _309_BestTimeToBuyAndSellStockWithCooldown:
-  def main(args: Array[String]): Unit =
-    println(maxProfit(Array(1, 2, 3, 0, 2)))
 
   def maxProfit(prices: Array[Int]): Int =
-    val (canBuy, _, mustRest) = prices.tail.foldLeft(0, -prices.head, Int.MinValue) {
-      case ((prevCanBuy, prevCanSell, prevMustRest), price) =>
-        (math.max(prevCanBuy, prevMustRest), math.max(prevCanSell, prevCanBuy - price), prevCanSell + price)
-    }
-    math.max(canBuy, mustRest)
+    prices.tail
+      .foldLeft(0, -prices.head, Int.MinValue) { case ((prevCanBuy, prevCanSell, prevMustRest), price) =>
+        (prevCanBuy max prevMustRest, prevCanSell max (prevCanBuy - price), prevCanSell + price)
+      }
+      .pipe { case (canBuy, _, mustRest) => canBuy max mustRest }
