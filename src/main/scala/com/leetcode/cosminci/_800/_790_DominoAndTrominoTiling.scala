36938ae54647eac621f2f0b027c0e7785b5e83c6
Cosmin Ciobanu <ciobanu@adobe.com>
2022-12-24 14:37:00 +0200

    790. Domino and Tromino Tiling ADDENDUM * minor refactoring

--- src/main/scala/com/leetcode/cosminci/_800/_790_DominoAndTrominoTiling.scala
+++ src/main/scala/com/leetcode/cosminci/_800/_790_DominoAndTrominoTiling.scala
@@ -1,10 +1,11 @@
 package com.leetcode.cosminci._800
 
+import scala.util.chaining.*
+
 object _790_DominoAndTrominoTiling:
+
   def numTilings(n: Int): Int =
-    (1 to n)
-      .foldLeft(-1L, 0L, 1L) { case ((prev3, prev2, prev1), _) =>
-        (prev2, prev1, (2 * prev1 + prev3) % 1_000_000_007)
-      }
-      ._3
-      .toInt
+    Iterator
+      .iterate((-1L, 0L, 1L)) { case (prev3, prev2, prev1) => (prev2, prev1, (2 * prev1 + prev3) % 1_000_000_007) }
+      .drop(n).next()
+      .pipe { case (_, _, res) => res.toInt }
