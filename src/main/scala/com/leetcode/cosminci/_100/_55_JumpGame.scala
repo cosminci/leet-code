8050a7575e5e49e38a915ef3e5d6c850badbf556
Cosmin Ciobanu <ciobanu@adobe.com>
2022-12-26 09:44:42 +0200

    55. Jump Game ADDENDUM * minor refactoring

--- src/main/scala/com/leetcode/cosminci/_100/_55_JumpGame.scala
+++ src/main/scala/com/leetcode/cosminci/_100/_55_JumpGame.scala
@@ -2,13 +2,8 @@ package com.leetcode.cosminci._100
 
 object _55_JumpGame:
 
-  def main(args: Array[String]): Unit =
-    println(canJump(Array(0, 2, 3)))
-    println(canJump(Array(2, 3, 1, 1, 4)))
-    println(canJump(Array(3, 2, 1, 0, 4)))
-
   def canJump(nums: Array[Int]): Boolean =
-    nums.indices.foldLeft(0) { case (max, i) =>
-      if (max < i) return false
-      math.max(max, i + nums(i))
-    } >= nums.length - 1
+    nums.indices.foldLeft(0) { (prevMax, i) =>
+      if (prevMax < i) return false
+      prevMax max (i + nums(i))
+    } >= nums.indices.last
