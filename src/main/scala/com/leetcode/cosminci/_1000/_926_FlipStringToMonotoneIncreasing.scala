37e1f25a9a84039dac8a8592427169ef0bfc2259
Cosmin Ciobanu <ciobanu@adobe.com>
2023-01-17 08:35:21 +0200

    926. Flip String to Monotone Increasing ADDENDUM * reimplement w/o mutability

--- src/main/scala/com/leetcode/cosminci/_1000/_926_FlipStringToMonotoneIncreasing.scala
+++ src/main/scala/com/leetcode/cosminci/_1000/_926_FlipStringToMonotoneIncreasing.scala
@@ -1,21 +1,9 @@
 package com.leetcode.cosminci._1000
 
 object _926_FlipStringToMonotoneIncreasing:
-  def main(args: Array[String]): Unit =
-    println(minFlipsMonoIncr("11011"))
-    println(minFlipsMonoIncr("000111"))
-    println(minFlipsMonoIncr("100111"))
-    println(minFlipsMonoIncr("00110"))
-    println(minFlipsMonoIncr("010110"))
-    println(minFlipsMonoIncr("00011000"))
 
   def minFlipsMonoIncr(s: String): Int =
-    var onesRemaining     = s.count(_ == '1')
-    var zeroesRemaining   = s.length - onesRemaining
-    var onesAccumulated   = 0
-
-    (Seq(onesRemaining, zeroesRemaining) ++ (0 until s.length).map { i =>
-      if s(i) == '1' then onesAccumulated += 1
-      else zeroesRemaining -= 1
-      onesAccumulated + zeroesRemaining
-    }).min
+    s.foldLeft(0, 0) { case ((result, ones), ch) =>
+      if (ch == '1') (result, ones + 1)
+      else (ones.min(result + 1), ones)
+    }._1
