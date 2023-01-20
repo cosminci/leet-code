f6c97c54d1d76125426fc404af68c2880ed02339
Cosmin Ciobanu <ciobanu@adobe.com>
2023-01-21 00:33:26 +0200

    491. Non-decreasing Subsequences ADDENDUM * simplify implementation

--- src/main/scala/com/leetcode/cosminci/_500/_491_IncreasingSubsequences.scala
+++ src/main/scala/com/leetcode/cosminci/_500/_491_IncreasingSubsequences.scala
@@ -3,29 +3,19 @@ package com.leetcode.cosminci._500
 import scala.collection.mutable
 
 object _491_IncreasingSubsequences:
-  def main(args: Array[String]): Unit =
-    println(findSubsequences(Array(4, 6, 7, 7)))
 
   def findSubsequences(nums: Array[Int]): List[List[Int]] =
-    if nums.length < 2 then return List.empty
-
-    val mem = mutable.Map.empty[(Int, Int), Seq[Seq[Int]]]
-    def dfs(idx: Int, prevValue: Int): Seq[Seq[Int]] =
-      if idx == nums.length then return Seq(Seq.empty)
-      if mem.contains(idx, prevValue) then return mem((idx, prevValue))
-
-      val result =
-        if nums(idx) >= prevValue then
-          dfs(idx + 1, nums(idx)).map(_.prepended(nums(idx))) ++ dfs(idx + 1, prevValue)
-        else dfs(idx + 1, prevValue)
-
-      mem.update((idx, prevValue), result)
-      result
+    val mem = mutable.Map.empty[(Int, Int), List[List[Int]]]
+    def dfs(i: Int, prev: Int): List[List[Int]] = mem.getOrElseUpdate((i, prev),
+      if i == nums.length then List(List.empty)
+      else if nums(i) < prev then dfs(i + 1, prev)
+      else dfs(i + 1, nums(i)).map(nums(i) +: _) ++ dfs(i + 1, prev)
+    )
 
     val subsequences = for
-      i <- 0 until nums.length - 1
+      i <- nums.indices
       j <- i + 1 until nums.length
       if nums(i) <= nums(j)
-    yield dfs(j + 1, nums(j)).map(Seq(nums(i), nums(j)) ++ _)
+    yield dfs(j + 1, nums(j)).map(List(nums(i), nums(j)) ++: _)
 
-    subsequences.flatten.distinct.map(_.toList).toList
+    subsequences.flatten.distinct.toList
