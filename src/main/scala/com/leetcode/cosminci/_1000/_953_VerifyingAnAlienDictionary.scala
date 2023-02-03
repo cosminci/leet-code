5b0afe416e699f44fe584718ee14e3e3ea1aa45e
Cosmin Ciobanu <ciobanu@adobe.com>
2023-02-03 10:44:46 +0200

    953. Verifying an Alien Dictionary

--- src/main/scala/com/leetcode/cosminci/_1000/_953_VerifyingAnAlienDictionary.scala
+++ src/main/scala/com/leetcode/cosminci/_1000/_953_VerifyingAnAlienDictionary.scala
@@ -1,22 +1,8 @@
 package com.leetcode.cosminci._1000
 
-import scala.collection.mutable
-
 object _953_VerifyingAnAlienDictionary:
 
-  def main(args: Array[String]): Unit =
-    println(isAlienSorted(Array("hello", "leetcode"), "hlabcdefgijkmnopqrstuvwxyz"))
-
   def isAlienSorted(words: Array[String], order: String): Boolean =
-    val charIndices = mutable.Map.empty[Char, Int]
-    order.indices.foreach(i => charIndices.update(order(i), i))
-    (0 until words.length - 1).foreach { i =>
-      val (w1, w2) = (words(i), words(i + 1))
-      if !inOrder(w1, w2, charIndices) then return false
-    }
-    true
-
-  def inOrder(w1: String, w2: String, charIndices: mutable.Map[Char, Int]): Boolean =
-    var idx = 0
-    while idx < w1.length && idx < w2.length && charIndices(w1(idx)) == charIndices(w2(idx)) do idx += 1
-    idx == w1.length || (idx < w2.length && charIndices(w1(idx)) < charIndices(w2(idx)))
+    words
+      .sortBy(_.map(ch => ('a' + order.indexOf(ch)).toChar).mkString)
+      .sameElements(words)
