0aff84fc11cfeca6bb8be30358cc412c9ed0e37a
Cosmin Ciobanu <ciobanu@adobe.com>
2023-02-07 16:36:25 +0200

    904. Fruit Into Baskets ADDENDUM * reimplement more concisely

--- src/main/scala/com/leetcode/cosminci/_1000/_904_FruitIntoBaskets.scala
+++ src/main/scala/com/leetcode/cosminci/_1000/_904_FruitIntoBaskets.scala
@@ -4,42 +4,14 @@ import scala.collection.mutable
 
 object _904_FruitIntoBaskets:
 
-  def main(args: Array[String]): Unit =
-    println(totalFruitTopDown(Array(3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4)))
-    println(totalFruitSliding(Array(3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4)))
-
-  def totalFruitSliding(fruits: Array[Int]): Int =
-    val baskets = mutable.Map.empty[Int, Int]
-    fruits(2) = 3
-    var max    = 0
-    var (l, r) = (0, 0)
-    while r < fruits.length do
-      if baskets.size == 2 && !baskets.contains(fruits(r)) then
-        max = math.max(max, baskets.values.sum)
-        while baskets.size == 2 do
-          baskets.updateWith(fruits(l)) {
-            case Some(1) | None => None
-            case Some(c)        => Some(c - 1)
-          }
-          l += 1
-      baskets.updateWith(fruits(r)) {
-        case None    => Some(1)
-        case Some(c) => Some(c + 1)
-      }
-      r += 1
-    math.max(max, baskets.values.sum)
-
-  def totalFruitTopDown(fruits: Array[Int]): Int =
+  def totalFruit(fruits: Array[Int]): Int = {
     val mem = mutable.Map.empty[(Int, Set[Int]), Int]
 
-    def dfs(idx: Int, fruitTypes: Set[Int]): Int =
-      if idx == fruits.length then return 0
-      if fruitTypes.size == 2 && !fruitTypes.contains(fruits(idx)) then return 0
-      if mem.contains((idx, fruitTypes)) then return mem((idx, fruitTypes))
+    def dfs(i: Int, fruitTypes: Set[Int]): Int = mem.getOrElseUpdate((i, fruitTypes),
+      if (i == fruits.length) 0
+      else if (fruitTypes.size == 2 && !fruitTypes.contains(fruits(i))) 0
+      else 1 + dfs(i + 1, fruitTypes + fruits(i))
+    )
 
-      val result = 1 + dfs(idx + 1, fruitTypes + fruits(idx))
-      mem.update((idx, fruitTypes), result)
-
-      result
-
-    fruits.indices.map(start => dfs(start, Set.empty)).max
+    fruits.indices.map(dfs(_, Set.empty)).max
+  }
