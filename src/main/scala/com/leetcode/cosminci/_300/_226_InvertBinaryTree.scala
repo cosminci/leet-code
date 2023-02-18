27dc1df72d38c1f5b5d1b345b497ab5de14c67ef
Cosmin Ciobanu <ciobanu@adobe.com>
2023-02-18 08:20:53 +0200

    226. Invert Binary Tree ADDENDUM * reimplement w/o mutability

--- src/main/scala/com/leetcode/cosminci/_300/_226_InvertBinaryTree.scala
+++ src/main/scala/com/leetcode/cosminci/_300/_226_InvertBinaryTree.scala
@@ -5,23 +5,7 @@ import com.leetcode.cosminci.utils.TreeNode
 import scala.collection.mutable
 
 object _226_InvertBinaryTree:
-  def invertTreeRecursive(root: TreeNode): TreeNode =
-    if root == null then return null
-    val tmp = root.left
-    root.left = invertTreeRecursive(root.right)
-    root.right = invertTreeRecursive(tmp)
-    root
 
-  def invertTreeQueue(root: TreeNode): TreeNode =
-    if root == null then return null
-    val queue = mutable.Queue(root)
-
-    while queue.nonEmpty do
-      val node = queue.dequeue()
-      if node.left != null then queue.enqueue(node.left)
-      if node.right != null then queue.enqueue(node.right)
-
-      val tmp = node.left
-      node.left = node.right
-      node.right = tmp
-    root
+  def invertTree(root: TreeNode): TreeNode =
+    if root == null then null
+    else new TreeNode(root.value, invertTree(root.right), invertTree(root.left))
