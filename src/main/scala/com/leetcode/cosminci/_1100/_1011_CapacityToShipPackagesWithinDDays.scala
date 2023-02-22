d282a369f614eb1ebca58f0f83ce5ea84b984141
Cosmin Ciobanu <ciobanu@adobe.com>
2023-02-22 12:29:46 +0200

    1011. Capacity To Ship Packages Within D Days ADDENDUM * reimplement w/o mutability

--- src/main/scala/com/leetcode/cosminci/_1100/_1011_CapacityToShipPackagesWithinDDays.scala
+++ src/main/scala/com/leetcode/cosminci/_1100/_1011_CapacityToShipPackagesWithinDDays.scala
@@ -1,24 +1,20 @@
 package com.leetcode.cosminci._1100
 
+import scala.util.chaining.*
+
 object _1011_CapacityToShipPackagesWithinDDays:
 
-  def main(args: Array[String]): Unit =
-    println(shipWithinDays(Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 5))
-    println(shipWithinDays(Array(3, 2, 2, 4, 1, 4), 3))
-    println(shipWithinDays(Array(1, 2, 3, 1, 1), 4))
-
-  def shipWithinDays(weights: Array[Int], days: Int): Int =
+  def shipWithinDays(weights: Array[Int], maxDays: Int): Int =
     def canShip(capacity: Int): Boolean =
       weights
-        .foldLeft((1, 0)) { case ((days, load), w) =>
-          if load + w > capacity then (days + 1, w)
-          else (days, load + w)
-        }
-        ._1 <= days
+        .foldLeft((1, 0)) { case ((days, load), w) => if load + w > capacity then (days + 1, w) else (days, load + w) }
+        .pipe { case (days, _) => days <= maxDays }
 
-    var (l, r) = (weights.max, weights.sum)
-    while l < r do
-      val mid = l + (r - l) / 2
-      if canShip(mid) then r = mid
-      else l = mid + 1
-    l
+    @annotation.tailrec
+    def search(l: Int, r: Int): Int =
+      if l >= r then l
+      else
+        val mid = l + (r - l) / 2
+        if canShip(mid) then search(l, mid) else search(mid + 1, r)
+
+    search(l = weights.max, r = weights.sum)
