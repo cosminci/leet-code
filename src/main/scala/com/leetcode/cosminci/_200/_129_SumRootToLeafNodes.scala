e1fde3d0cf58198151c03d1ff07d14daa48716b3
Cosmin Ciobanu <ciobanu@adobe.com>
2023-03-14 21:13:59 +0200

    129. Sum Root to Leaf Numbers  ADDENDUM * cosmetic improvements

--- src/main/scala/com/leetcode/cosminci/_200/_129_SumRootToLeafNodes.scala
+++ src/main/scala/com/leetcode/cosminci/_200/_129_SumRootToLeafNodes.scala
@@ -3,16 +3,13 @@ package com.leetcode.cosminci._200
 import com.leetcode.cosminci.utils.TreeNode
 
 object _129_SumRootToLeafNodes:
-  def main(args: Array[String]): Unit =
-    println(sumNumbers(new TreeNode(1, new TreeNode(2), new TreeNode(3))))
 
   def sumNumbers(root: TreeNode): Int =
     def dfs(prev: Int, node: TreeNode): Int =
-      if node == null then return 0
-
-      val newValue = prev * 10 + node.value
-
-      if node.left == null && node.right == null then newValue
-      else dfs(newValue, node.left) + dfs(newValue, node.right)
-
-    dfs(0, root)
+      if node == null then 0
+      else {
+        val curr = prev * 10 + node.value
+        if node.left == null && node.right == null then curr
+        else dfs(curr, node.left) + dfs(curr, node.right)
+      }
+    dfs(prev = 0, node = root)
