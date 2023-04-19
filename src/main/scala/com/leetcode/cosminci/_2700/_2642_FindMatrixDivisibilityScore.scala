126e32d5a23bd9ff94ae456d14c9ba5b5df5629f
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-04-19 09:08:51 +0300

    2644. Find the Maximum Divisibility Score

---
+++ src/main/scala/com/leetcode/cosminci/_2700/_2642_FindMatrixDivisibilityScore.scala
@@ -0,0 +1,15 @@
+package com.leetcode.cosminci._2700
+
+import scala.util.chaining.*
+
+object _2642_FindMatrixDivisibilityScore:
+
+  def maxDivScore(nums: Array[Int], divisors: Array[Int]): Int =
+    divisors
+      .foldLeft(0, -1) { case ((globalMax, currMax), div) =>
+        val count = nums.count(_ % div == 0)
+        if count < currMax then (globalMax, currMax)
+        else if count > currMax then (div, count)
+        else (div.min(globalMax), count)
+      }
+      .pipe { case (res, _) => res }
