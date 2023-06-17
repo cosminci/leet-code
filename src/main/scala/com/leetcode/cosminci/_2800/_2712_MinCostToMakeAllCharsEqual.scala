37dfda5920fae03bcbb9cb00b89aa66f2cddc8f7
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-06-17 20:36:34 +0300

    2712. Minimum Cost to Make All Characters Equal ADDENDUM

--- src/main/scala/com/leetcode/cosminci/_2800/_2712_MinCostToMakeAllCharsEqual.scala
+++ src/main/scala/com/leetcode/cosminci/_2800/_2712_MinCostToMakeAllCharsEqual.scala
@@ -5,5 +5,5 @@ object _2712_MinCostToMakeAllCharsEqual:
   def minimumCost(s: String): Long =
     (1 until s.length)
       .filter(i => s(i) != s(i - 1))
-      .map(i => i.min(s.length - i).toLong)
-      .sum
+      .map(i => i.min(s.length - i))
+      .foldLeft(0L)(_ + _)
