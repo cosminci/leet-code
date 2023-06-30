88f4b3cc1dc27899a03e94a2418a8ca0bd92e10b
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-06-30 15:11:26 +0300

    1970. Last Day Where You Can Still Cross ADDENDUM * reimplement w/o mutability

--- src/main/scala/com/leetcode/cosminci/_2000/_1970_LastDayWhereYouCanStillCross.scala
+++ src/main/scala/com/leetcode/cosminci/_2000/_1970_LastDayWhereYouCanStillCross.scala
@@ -1,48 +1,27 @@
 package com.leetcode.cosminci._2000
 
 import scala.collection.mutable
+import scala.util.chaining.*
 
 object _1970_LastDayWhereYouCanStillCross:
-  def main(args: Array[String]): Unit =
-    println(latestDayToCross(3, 3, Array(Array(1, 2), Array(2, 1), Array(3, 3), Array(2, 2), Array(1, 1))))
 
-  def latestDayToCross(numRows: Int, numCols: Int, cells: Array[Array[Int]]): Int =
-    val firstRowCells = (1 to numCols).map(c => (1, c))
-    val floodedCellsPerDay = cells.scanLeft(Set.empty[(Int, Int)]) { case (alreadyFlooded, Array(x, y)) =>
-      alreadyFlooded + ((x, y))
-    }
-
-    def neighbours(r: Int, c: Int): Seq[(Int, Int)] =
-      val below = Option.when(r < numRows)((r + 1, c))
-      val left  = Option.when(c > 1)((r, c - 1))
-      val right = Option.when(c < numCols)((r, c + 1))
-      val above = Option.when(r > 1)((r - 1, c))
-      Seq(above, below, left, right).flatten
+  def latestDayToCross(m: Int, n: Int, cells: Array[Array[Int]]): Int =
+    val flooded = cells.scanLeft(Set.empty[(Int, Int)]) { case (flooded, Array(r, c)) => flooded + ((r - 1, c - 1)) }
 
     def canReach(day: Int): Boolean =
-      val floodedCells = floodedCellsPerDay(day)
-      val startCells   = firstRowCells.filter(cell => !floodedCells.contains(cell))
-      val toVisit      = mutable.Stack.from(startCells)
-      val visited      = mutable.Set.from(startCells)
-
-      while toVisit.nonEmpty do
-        val cell @ (r, c) = toVisit.pop()
-        if r == numRows then return true
-
-        neighbours(r, c).foreach { neighbour =>
-          if !visited.contains(neighbour) && !floodedCells.contains(neighbour) then
-            visited.add(neighbour)
-            toVisit.push(neighbour)
+      val startCells = (0 until n).map(c => (0, c)).filterNot(flooded(day).contains)
+      Iterator
+        .iterate((startCells, startCells.toSet)) { case ((r, c) +: toVisit, visited) =>
+          val newToVisit = Seq((r - 1, c), (r, c - 1), (r, c + 1), (r + 1, c))
+            .filter { case (nr, nc) => nr >= 0 && nr < m && nc >= 0 && nc < n }
+            .filterNot(visited.contains)
+            .filterNot(flooded(day).contains)
+          (toVisit ++ newToVisit, visited ++ newToVisit)
         }
+        .dropWhile { case (toVisit, _) => toVisit.nonEmpty && toVisit.headOption.forall { case (r, _) => r != m - 1 } }
+        .next().pipe { case (toVisit, _) => toVisit.headOption.exists { case (r, _) => r == m - 1 } }
 
-      false
-
-    def binarySearch(left: Int, right: Int): Int =
-      var (l, r) = (left, right)
-      while l < r do
-        val mid = l + (r - l) / 2
-        if canReach(mid) then l = mid + 1
-        else r = mid
-      l - 1
-
-    binarySearch(1, cells.length)
+    Iterator
+      .iterate((1, cells.length)) { case (l, r) => ((l + r) / 2).pipe(m => if (canReach(m)) (m + 1, r) else (l, m)) }
+      .dropWhile { case (l, r) => l < r }
+      .next().pipe { case (l, _) => l - 1 }
