fad0047970105491aeb7866f92e92d5c86320236
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-07-15 19:59:00 +0300

    2767. Partition String Into Minimum Beautiful Substrings

---
+++ src/main/scala/com/leetcode/cosminci/_2800/_2767_PartitionStringIntoMinBeautifulSubstr.scala
@@ -0,0 +1,16 @@
+package com.leetcode.cosminci._2800
+
+import scala.util.chaining.*
+
+object _2767_PartitionStringIntoMinBeautifulSubstr:
+
+  def minimumBeautifulSubstrings(s: String): Int =
+    val pows = Array(0, 1, 2, 3, 4, 5, 6).map(pow => math.pow(5, pow).toInt.toBinaryString).reverse
+
+    def dfs(i: Int): Int =
+      if i == s.length then 0
+      else pows
+        .collect { case pow if s.startsWith(pow, i) => 1 + dfs(i + pow.length) }
+        .minOption.getOrElse(s.length * 2)
+
+    dfs(i = 0).pipe(res => if res > s.length then -1 else res)
