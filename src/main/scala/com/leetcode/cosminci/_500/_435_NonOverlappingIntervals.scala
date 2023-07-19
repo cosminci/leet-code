611343247732d8b29bc467c50f28ae26d065df4b
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-07-19 08:21:10 +0300

    435. Non-overlapping Intervals ADDENDUM * reimplement w/o mutability

--- src/main/scala/com/leetcode/cosminci/_500/_435_NonOverlappingIntervals.scala
+++ src/main/scala/com/leetcode/cosminci/_500/_435_NonOverlappingIntervals.scala
@@ -1,21 +1,14 @@
 package com.leetcode.cosminci._500
 
+import scala.util.chaining.*
+
 object _435_NonOverlappingIntervals:
-  def main(args: Array[String]): Unit =
-    println(eraseOverlapIntervals(Array(Array(0, 2), Array(1, 3), Array(2, 4), Array(3, 5), Array(4, 6))))
-    println(eraseOverlapIntervals(Array(Array(1, 2), Array(2, 3), Array(3, 4), Array(1, 3))))
-    println(eraseOverlapIntervals(Array(Array(1, 2), Array(1, 2), Array(1, 2))))
-    println(eraseOverlapIntervals(Array(Array(1, 2), Array(2, 3))))
 
   def eraseOverlapIntervals(intervals: Array[Array[Int]]): Int =
-    intervals.sortInPlaceBy(_.last)
-
-    var deleteCount = 0
-    intervals.indices.tail.foldLeft(intervals.head) { case (prev, i) =>
-      if intervals(i).head < prev.last then
-        deleteCount += 1
-        prev
-      else intervals(i)
-    }
-
-    deleteCount
+    intervals
+      .sortBy(_.last).tail
+      .foldLeft(0, intervals.minBy(_.last)) { case ((cnt, prev), curr) =>
+        if curr.head >= prev.last then (cnt, curr)
+        else (cnt + 1, prev)
+      }
+      .pipe { case (cnt, _) => cnt }
