42e6d2102c0e188d3d6b09d0e801c6b90569ee85
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-07-24 10:04:56 +0300

    50. Pow(x, n) ADDENDUM * cosmetic improvements

--- src/main/scala/com/leetcode/cosminci/_100/_50_Pow.scala
+++ src/main/scala/com/leetcode/cosminci/_100/_50_Pow.scala
@@ -1,16 +1,10 @@
 package com.leetcode.cosminci._100
 
-import scala.collection.mutable
-
 object _50_Pow:
-  def main(args: Array[String]): Unit =
-    println(myPow(2, Int.MinValue))
-    println(myPow(-2.0, -2))
 
   def myPow(x: Double, n: Int): Double =
     def dfs(num: Double, pow: Int): Double =
-      if pow == 0 then return 1
-      val oddFactor = if pow % 2 == 0 then 1 else num
-      dfs(num * num, pow / 2) * oddFactor
+      if pow == 0 then 1
+      else dfs(num * num, pow / 2) * (if pow % 2 == 0 then 1 else num)
 
-    if n < 0 then 1 / dfs(x, math.abs(n)) else dfs(x, n)
+    if n < 0 then 1 / dfs(x, n.abs) else dfs(x, n)
