383cb3128aaf59534d29295ac148609b67afc76f
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-07-28 07:59:40 +0300

    486. Predict the Winner ADDENDUM * simplify solution

--- src/main/scala/com/leetcode/cosminci/_500/_486_PredictTheWinner.scala
+++ src/main/scala/com/leetcode/cosminci/_500/_486_PredictTheWinner.scala
@@ -3,23 +3,11 @@ package com.leetcode.cosminci._500
 import scala.collection.mutable
 
 object _486_PredictTheWinner:
-  def main(args: Array[String]): Unit =
-    println(predictTheWinner(Array(1, 5, 2)))
-    println(predictTheWinner(Array(1, 5, 233, 7)))
 
   def predictTheWinner(nums: Array[Int]): Boolean =
     val mem = mutable.Map.empty[(Int, Int), Int]
-
-    def dfs(left: Int, right: Int): Int =
-      if left == right then return nums(left)
-      if mem.contains((left, right)) then return mem((left, right))
-
-      val result = math.max(
-        nums(left) - dfs(left + 1, right),
-        nums(right) - dfs(left, right - 1)
-      )
-
-      mem.update((left, right), result)
-      result
-
-    dfs(0, nums.length - 1) >= 0
+    def dfs(l: Int, r: Int): Int = mem.getOrElseUpdate((l, r),
+      if l == r then nums(l)
+      else (nums(l) - dfs(l + 1, r)).max(nums(r) - dfs(l, r - 1))
+    )
+    dfs(l = 0, r = nums.length - 1) >= 0
