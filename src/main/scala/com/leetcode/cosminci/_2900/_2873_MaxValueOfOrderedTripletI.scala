aeb5f279c2a2b8966db743073dd8e84c4355d900
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-10-29 11:27:02 +0200

    2873. Maximum Value of an Ordered Triplet I

---
+++ src/main/scala/com/leetcode/cosminci/_2900/_2873_MaxValueOfOrderedTripletI.scala
@@ -0,0 +1,12 @@
+package com.leetcode.cosminci._2900
+
+object _2873_MaxValueOfOrderedTripletI:
+
+  def maximumTripletValue(nums: Array[Int]): Long =
+    nums
+      .foldLeft(0L, 0L, 0L) { case ((result, maxDiff, maxNum), n) =>
+        val newRes     = result.max(maxDiff * n)
+        val newMaxDiff = maxDiff.max(maxNum - n)
+        val newMaxNum  = maxNum.max(n)
+        (newRes, newMaxDiff, newMaxNum)
+      }._1
