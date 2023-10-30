d6251eb05676a6ef0f2b095a85d7991b8ab10103
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-10-30 08:30:06 +0200

    2903. Find Indices With Index and Value Difference I

---
+++ src/main/scala/com/leetcode/cosminci/_3000/_2903_FindIndicesWithIndexAndValueDiff1.scala
@@ -0,0 +1,12 @@
+package com.leetcode.cosminci._3000
+
+object _2903_FindIndicesWithIndexAndValueDiff1:
+
+  def findIndices(nums: Array[Int], indexDifference: Int, valueDifference: Int): Array[Int] =
+    val maybeResult = for
+      i <- 0 until nums.length - indexDifference
+      j <- i + indexDifference until nums.length
+      if (nums(i) - nums(j)).abs >= valueDifference
+    yield Array(i, j)
+
+    maybeResult.headOption.getOrElse(Array(-1, -1))
