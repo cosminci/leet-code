7ed28b895bdfb484996794a0c7535fcb069cffc2
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-10-30 11:04:21 +0200

    2908. Minimum Sum of Mountain Triplets I

---
+++ src/main/scala/com/leetcode/cosminci/_3000/_2908_MinSumOfMountainTripletsI.scala
@@ -0,0 +1,11 @@
+package com.leetcode.cosminci._3000
+
+object _2908_MinSumOfMountainTripletsI:
+
+  def minimumSum(nums: Array[Int]): Int =
+    val pMin = nums.scanLeft(Int.MaxValue)(_ min _)
+    val sMin = nums.scanRight(Int.MaxValue)(_ min _).tail
+
+    nums.indices
+      .collect { case i if pMin(i) < nums(i) && nums(i) > sMin(i) => pMin(i) + nums(i) + sMin(i) }
+      .minOption.getOrElse(-1)
