58bb1283d0faec1812626403eb540474910eb19a
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-11-26 11:12:58 +0200

    2942. Find Words Containing Character ADDENDUM * minor fix

--- src/main/scala/com/leetcode/cosminci/_3000/_2942_FindWordsContainingChar.scala
+++ src/main/scala/com/leetcode/cosminci/_3000/_2942_FindWordsContainingChar.scala
@@ -3,4 +3,4 @@ package com.leetcode.cosminci._3000
 object _2942_FindWordsContainingChar:
 
   def findWordsContaining(words: Array[String], x: Char): List[Int] =
-    words.indices.filter(i => words(i).contains(x))
+    words.indices.filter(i => words(i).contains(x)).toList
