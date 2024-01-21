c6b2791b7ad7f280b88bcea2dc289d05ac3bcda4
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2024-01-21 15:45:33 +0200

    2998. Minimum Number of Operations to Make X and Y Equal

---
+++ src/main/scala/com/leetcode/cosminci/_1300/_2998_MinOpsToMakeXAndYEqual.scala
@@ -0,0 +1,15 @@
+package com.leetcode.cosminci._1300
+
+object _2998_MinOpsToMakeXAndYEqual:
+
+  def minimumOperationsToMakeEqual(x: Int, y: Int): Int =
+    @annotation.tailrec
+    def dfs(toVisit: Seq[Int], visited: Set[Int], steps: Int): Int =
+      if toVisit.contains(y) then steps
+      else
+        val next = toVisit
+          .flatMap(n => Seq(n + 1, n - 1) ++ Option.when(n % 11 == 0)(n / 11) ++ Option.when(n % 5 == 0)(n / 5))
+          .filterNot(visited.contains)
+        dfs(next, visited ++ next, steps + 1)
+
+    dfs(toVisit = Seq(x), visited = Set(x), steps = 0)
