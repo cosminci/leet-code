6b4b45be34665d71ecbfef3b1aee8614f988a3bd
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2024-02-04 19:59:14 +0200

    3026. Maximum Good Subarray Sum ADDENDUM * fix typo

--- src/main/scala/com/leetcode/cosminci/_3100/_3026_MaxGoodSubarraySum.scala
+++ src/main/scala/com/leetcode/cosminci/_3100/_3026_MaxGoodSubarraySum.scala
@@ -5,10 +5,10 @@ import scala.util.chaining.*
 object _3026_MaxGoodSubarraySum:
 
   def maximumSubarraySum(nums: Array[Int], k: Int): Long =
-    nums.foldLeft(0L, Map.empty[Int, Long], Long.MinValue) { case ((currSum, prevsums, res), n) =>
+    nums.foldLeft(0L, Map.empty[Int, Long], Long.MinValue) { case ((currSum, prevSums, res), n) =>
       val newRes = Seq(n - k, n + k)
-        .flatMap(prevsums.get).map(minValue => currSum + n - minValue)
+        .flatMap(prevSums.get).map(minValue => currSum + n - minValue)
         .maxOption.getOrElse(res).max(res)
-      val newPrevSums = prevsums.updated(n, prevsums.getOrElse(n, Long.MaxValue).min(currSum))
+      val newPrevSums = prevSums.updated(n, prevSums.getOrElse(n, Long.MaxValue).min(currSum))
       (currSum + n, newPrevSums, newRes)
     }.pipe { case (_, _, res) => if res > Long.MinValue then res else 0 }
